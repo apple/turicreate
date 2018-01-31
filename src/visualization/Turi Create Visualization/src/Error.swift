06e00c612c9b0921b09ba741075c18773ce6842d
Zach Nation <znation@apple.com>
2018-01-30 17:20:57 -0800

    Remove unused imports from Swift code in viz

--- src/visualization/Turi Create Visualization/src/Error.swift
+++ src/visualization/Turi Create Visualization/src/Error.swift
@@ -2,7 +2,6 @@
 //
 //  Use of this source code is governed by a BSD-3-clause license that can
 //  be found in the LICENSE.txt file or at https://opensource.org/licenses/BSD-3-Clause
-import Foundation
 
 enum VisualizationError:Error {
     case JavascriptError(String)
