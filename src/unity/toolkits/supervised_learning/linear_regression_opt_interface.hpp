2818159862dbb1470db945b1edb4cf20adf00ac1
nickjong <nickjong@users.noreply.github.com>
2018-02-19 19:44:13 -0800

    Fix validation-set statistics saved to linear/logistic regression models (#290)
    The changes for https://github.com/apple/turicreate/pull/274 were incomplete. They added validation-set fields, but populated them with recomputed training-set stats. The optimizer interface must explicitly support computing validation-set stats.

--- src/unity/toolkits/supervised_learning/linear_regression_opt_interface.hpp
+++ src/unity/toolkits/supervised_learning/linear_regression_opt_interface.hpp
@@ -36,8 +36,7 @@ namespace supervised {
  */
 class linear_regression_opt_interface: public
                                        optimization::second_order_opt_interface
-  {
-
+{
   protected:
 
   ml_data data;
@@ -145,6 +144,7 @@ class linear_regression_opt_interface: public
   void compute_first_order_statistics(const DenseVector &point, DenseVector&
       gradient, double & function_value, const size_t mbStart = 0, const size_t
       mbSize = -1);
+
   /**
    * Compute second order statistics at the given point. (Gradient & Function value)
    *
@@ -157,7 +157,25 @@ class linear_regression_opt_interface: public
   void compute_second_order_statistics(const DenseVector &point, DenseMatrix&
       hessian, DenseVector& gradient, double & function_value);
 
+  /**
+   * Compute second order statistics at the given point with respect to the
+   * validation data. (Gradient & Function value)
+   *
+   * \param[in]  point           Point at which we are computing the stats.
+   * \param[out] hessian         Hessian (Dense)
+   * \param[out] gradient        Dense gradient
+   * \param[out] function_value  Function value
+   *
+   */
+  void compute_validation_second_order_statistics(
+      const DenseVector& point, DenseMatrix& hessian, DenseVector& gradient,
+      double &function_value);
 
+  private:
+
+  void compute_second_order_statistics(
+      const ml_data& data, const DenseVector &point, DenseMatrix& hessian,
+      DenseVector& gradient, double & function_value);
 };
 
 
