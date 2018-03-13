f02d3ae6ed162f19321fef15a8107fa538c72d62
Gustav Larsson <glarsson@apple.com>
2018-03-13 14:21:39 -0700

    Consistent color for draw_bounding_boxes

--- src/unity/python/turicreate/toolkits/object_detector/util/_visualization.py
+++ src/unity/python/turicreate/toolkits/object_detector/util/_visualization.py
@@ -11,6 +11,14 @@ import numpy as _np
 from turicreate.toolkits._internal_utils import _numeric_param_check_range
 
 
+def _string_hash(s):
+    """String hash (djb2) with consistency between py2/py3 and persistency between runs (unlike `hash`)."""
+    h = 5381
+    for c in s:
+        h = h * 33 + ord(c)
+    return h
+
+
 COLOR_NAMES = [
     'AliceBlue', 'Chartreuse', 'Aqua', 'Aquamarine', 'Azure', 'Beige',
     'Bisque', 'BlanchedAlmond', 'BlueViolet', 'BurlyWood', 'CadetBlue',
@@ -50,7 +58,7 @@ def _annotate_image(pil_image, anns, confidence_threshold):
         if 'confidence' in ann and ann['confidence'] < confidence_threshold:
             continue
         if 'label' in ann:
-            color = COLOR_NAMES[hash(ann['label']) % len(COLOR_NAMES)]
+            color = COLOR_NAMES[_string_hash(ann['label']) % len(COLOR_NAMES)]
         else:
             color = 'White'
 
