fd19f677bba99ec3916276f096444c195a81eb0c
Gustav Larsson <glarsson@apple.com>
2018-03-13 21:18:55 -0700

    Add checks for return type in classifier predict

--- src/unity/python/turicreate/test/test_image_classifier.py
+++ src/unity/python/turicreate/test/test_image_classifier.py
@@ -11,7 +11,9 @@ import sys
 import unittest
 import turicreate as tc
 from turicreate.toolkits._main import ToolkitError as _ToolkitError
-from turicreate.toolkits._internal_utils import _mac_ver
+from turicreate.toolkits._internal_utils import (_mac_ver,
+                                                 _raise_error_if_not_sframe,
+                                                 _raise_error_if_not_sarray)
 import tempfile
 from . import util as test_util
 import pytest
@@ -128,6 +130,7 @@ class ImageClassifierTest(unittest.TestCase):
         model = self.model
         for output_type in ['class', 'probability_vector']:
             preds = model.predict(self.sf.head(), output_type=output_type)
+            _raise_error_if_not_sarray(preds)
             self.assertEqual(len(preds), len(self.sf.head()))
 
     def test_single_image(self):
@@ -136,7 +139,7 @@ class ImageClassifierTest(unittest.TestCase):
         prediction = model.predict(single_image)
         self.assertTrue(isinstance(prediction, int))
         prediction = model.predict_topk(single_image)
-        self.assertIsNotNone(prediction)
+        _raise_error_if_not_sframe(prediction)
         prediction = model.classify(single_image)
         self.assertTrue(isinstance(prediction, dict) and 'class' in prediction and 'probability' in prediction)
 
@@ -144,11 +147,11 @@ class ImageClassifierTest(unittest.TestCase):
         model = self.model
         data = self.sf[self.feature]
         predictions = model.predict(data)
-        self.assertIsNotNone(predictions)
+        _raise_error_if_not_sarray(predictions)
         predictions = model.predict_topk(data)
-        self.assertIsNotNone(predictions)
+        _raise_error_if_not_sframe(predictions)
         predictions = model.classify(data)
-        self.assertIsNotNone(predictions)
+        _raise_error_if_not_sframe(predictions)
 
     def test_junk_input(self):
         model = self.model
