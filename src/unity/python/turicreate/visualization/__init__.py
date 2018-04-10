0155be9c2dcde50fb4255fecc8ff3f9edb9fbee9
Shantanu Chhabra <shantanu_chhabra@apple.com>
2018-04-10 04:10:58 -0700

    Unnecessary newline

--- src/unity/python/turicreate/visualization/__init__.py
+++ src/unity/python/turicreate/visualization/__init__.py
@@ -3,7 +3,6 @@
 #
 # Use of this source code is governed by a BSD-3-clause license that can
 # be found in the LICENSE.txt file or at https://opensource.org/licenses/BSD-3-Clause
-
 from __future__ import print_function as _
 from __future__ import division as _
 from __future__ import absolute_import as _
