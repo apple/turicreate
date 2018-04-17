62df5fe60815b73e243a355d1bef01b52fd762f0
Hoyt Koepke <hoytak@apple.com>
2018-04-17 11:00:03 -0700

    Added license information.

--- src/capi/impl/capi_error_handling.hpp
+++ src/capi/impl/capi_error_handling.hpp
@@ -1,3 +1,8 @@
+/* Copyright © 2018 Apple Inc. All rights reserved.
+ *
+ * Use of this source code is governed by a BSD-3-clause license that can
+ * be found in the LICENSE.txt file or at https://opensource.org/licenses/BSD-3-Clause
+ */
 #ifndef TURI_CAPI_ERROR_HANDLING_INTERNAL
 #define TURI_CAPI_ERROR_HANDLING_INTERNAL
 
