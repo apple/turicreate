5031ba38273a7be591a9ab7dac079fda19e7edd1
Zach Nation <znation@apple.com>
2018-05-17 23:01:51 -0700

    Only apply the fix on gcc

--- test/capi/boost_utils.hpp
+++ test/capi/boost_utils.hpp
@@ -8,12 +8,14 @@
 #define CAPI_BOOST_TEST_UTILS
 
 // TODO - not sure why this is necessary, but we get symbol undefined errors
-// unless we define this explicitly. The errors are of the form:
+// on gcc unless we define this explicitly. The errors are of the form:
 //
 // print_helper.hpp:216: undefined reference to `boost::test_tools::tt_detail::print_log_value<decltype(nullptr)>::operator()(std::ostream&, decltype(nullptr))'
 //
+#ifndef __clang__
 void boost::test_tools::tt_detail::print_log_value<decltype(nullptr)>::operator()(std::ostream& os, decltype(nullptr)) {
   os << "nullptr";
 }
+#endif
 
 #endif
