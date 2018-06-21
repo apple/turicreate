bf13dd8381647aef35f1e72a9646a523638be407
Zach Nation <znation@gmail.com>
2018-06-21 12:47:40 -0700

    Fix visualization compatibility on 10.12 & 10.13 (#730)
    Also adds a unit test to sanity check the visualization client, to
    ensure that at least the explore view of an SFrame shows the contents of
    the SFrame.
    
    Fixes #652

--- scripts/install_python_toolchain.sh
+++ scripts/install_python_toolchain.sh
@@ -53,6 +53,10 @@ function download_file {
 
 $python_scripts/$PIP install --upgrade "pip>=8.1"
 $python_scripts/$PIP install -r scripts/requirements.txt
+if [[ $OSTYPE == darwin* ]]; then
+  # macOS only test dependency
+  $python_scripts/$PIP install pyobjc==4.2.2
+fi
 
 mkdir -p deps/local/lib
 mkdir -p deps/local/include
