91bd0eb54e361c101530400538d2dfba80ce26a4
Nick Jong <njong@apple.com>
2018-07-18 23:40:13 -0700

    Automatic regression should attempt multiple models and compare the results (#864)
    The automatic classification feature already trains multiple models, returning the one with the highest validation accuracy. We should do the same thing for regression, using RMSE to choose the final model.
    
    Note that the current automatic regression logic simply uses boosted trees for any dataset with fewer than 200 columns, and linear otherwise. For small datasets, linear is much more likely to be the best bet.
    
    These changes unify the Python (turicreate.regression.create) and C API (create_automatic_regression_model) implementations. The automatic classification implementations both train multiple models but have some subtle differences, so unification there is deferred for now to minimize risk.

--- src/unity/toolkits/supervised_learning/automatic_model_creation.cpp
+++ src/unity/toolkits/supervised_learning/automatic_model_creation.cpp
@@ -1,5 +1,12 @@
-#include <unity/lib/api/unity_sframe_interface.hpp>
 #include <unity/toolkits/supervised_learning/automatic_model_creation.hpp>
+
+#include <algorithm>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include <unity/lib/api/unity_sframe_interface.hpp>
+#include <unity/lib/unity_sframe.hpp>
 #include <unity/toolkits/supervised_learning/boosted_trees.hpp>
 #include <unity/toolkits/supervised_learning/decision_tree.hpp>
 #include <unity/toolkits/supervised_learning/linear_regression.hpp>
@@ -7,10 +14,141 @@
 #include <unity/toolkits/supervised_learning/logistic_regression.hpp>
 #include <unity/toolkits/supervised_learning/random_forest.hpp>
 #include <unity/toolkits/supervised_learning/supervised_learning.hpp>
+#include <unity/toolkits/supervised_learning/supervised_learning_utils-inl.hpp>
 
 
 namespace turi {
 namespace supervised {
+
+namespace {
+
+// Threshold on the number of feature columns, used to determine the
+// applicability of certain models.
+constexpr size_t WIDE_DATA = 200;
+
+/**
+ * Compute the width of the data.
+ *
+ * \param[in] X  Input SFrame
+ * \returns width
+ *
+ * The width is the same as the num_coefficients.
+ *
+ */
+size_t compute_data_width(sframe X){
+  ml_data data;
+  data.fill(X);
+  return get_number_of_coefficients(data.metadata());
+}
+
+
+// If `field_name` is present in `model_fields`, populates `*output` with the
+// value of `model->get_value_from_state(field_name)` and returns
+// true. Otherwise, returns false.
+bool read_model_field(
+    const std::shared_ptr<supervised_learning_model_base>& model,
+    const std::vector<std::string>& model_fields,
+    const char* field_name, variant_type* output) {
+  auto iter = std::find(model_fields.begin(), model_fields.end(), field_name);
+  bool success = iter != model_fields.end();
+  if (success) {
+    *output = model->get_value_from_state(field_name);
+  }
+  return success;
+}
+
+// Returns the regressors worth trying for the given data.
+std::vector<std::shared_ptr<supervised_learning_model_base>>
+get_regression_models(std::shared_ptr<unity_sframe> X) {
+
+  std::vector<std::shared_ptr<supervised_learning_model_base>> models;
+
+  const size_t data_width = compute_data_width(*X->get_underlying_sframe());
+
+  // Always try linear regression.
+  models.push_back(std::make_shared<linear_regression>());
+
+  if (data_width < WIDE_DATA) {
+    models.push_back(std::make_shared<xgboost::boosted_trees_regression>());
+  }
+
+  return models;
+}
+
+// Returns the validation Root Mean Squared Error if it exists, otherwise
+// returns training RMSE. Throws if neither field exists.
+double get_regression_rmse(
+    const std::shared_ptr<supervised_learning_model_base>& model) {
+
+  variant_type rmse;
+  std::vector<std::string> model_fields = model->list_fields();
+  bool success = false;
+  if (!success) {
+    success = read_model_field(model, model_fields, "validation_rmse", &rmse);
+  }
+  if (!success) {
+    success = read_model_field(model, model_fields, "training_rmse", &rmse);
+  }
+  if (!success) {
+    log_and_throw("Model does not have metrics that can be used for model "
+                  "selection.");
+  }
+
+  double result = variant_get_value<flexible_type>(rmse).to<double>();
+  ASSERT_TRUE(result >= 0);
+
+  return result;
+}
+
+}  // anonymous namespace
+
+/**
+ * Rule based better than stupid model selector.
+ */
+std::string _classifier_model_selector(std::shared_ptr<unity_sframe> _X){
+
+  sframe X = *(_X->get_underlying_sframe());
+
+  size_t data_width = compute_data_width(X);
+  if (data_width < WIDE_DATA){
+    return "boosted_trees_classifier";
+  } else {
+    return "classifier_logistic_regression";
+  }
+}
+
+/**
+ * Rule based better than stupid model selector.
+ */
+std::vector<std::string> _classifier_available_models(size_t num_classes,
+                                         std::shared_ptr<unity_sframe> _X){
+  sframe X = *(_X->get_underlying_sframe());
+
+  // Throw error if only one class.
+  // If number of classes more than 2, use boosted trees
+  if (num_classes == 1) {
+    log_and_throw("One-class classification is not currently supported. Please check your target column.");
+  } else if (num_classes > 2) {
+    return {"boosted_trees_classifier",
+            "random_forest_classifier",
+            "decision_tree_classifier",
+            "classifier_logistic_regression"};
+  } else {
+    size_t data_width = compute_data_width(X);
+    if (data_width < WIDE_DATA){
+      return {"boosted_trees_classifier",
+              "random_forest_classifier",
+              "decision_tree_classifier",
+              "classifier_svm",
+              "classifier_logistic_regression"};
+    } else {
+      return {"classifier_logistic_regression",
+              "classifier_svm"};
+    }
+  }
+
+  return std::vector<std::string>();
+}
 
 std::shared_ptr<supervised_learning_model_base> create_classifier(const std::string& model_name) {
   std::shared_ptr<supervised_learning_model_base> result;
@@ -62,10 +200,15 @@ std::shared_ptr<supervised_learning_model_base> create_automatic_classifier_mode
   gl_sframe validation_data;
   std::tie(data, validation_data) = create_validation_data(data, _validation_data);  
 
+  // TODO: The original Python code path only runs the model selector on at most
+  // 100000 rows of data. Should we do the same here?
   size_t num_classes = data[target].unique().size();
   std::vector<std::string> possible_models = _classifier_available_models(num_classes, data);
 
   // If no validation set and enough training data, create a validation set.
+  // TODO: The original Python code path allowed users to specify no validation
+  // set (use all data for training) by providing an empty SFrame or None.
+  // Should these behaviors be unified?
   if(validation_data.empty() && data.size() >= 100) {
     std::pair<gl_sframe, gl_sframe>  split = data.random_split(.95);
     data = split.first;
@@ -97,12 +240,16 @@ std::shared_ptr<supervised_learning_model_base> create_automatic_regression_mode
     gl_sframe data, const std::string target, const variant_type& _validation_data,
     const std::map<std::string, flexible_type>& options) {
 
+  // Perform training/validation split if necessary.
   gl_sframe validation_data;
   std::tie(data, validation_data) = create_validation_data(data, _validation_data);  
 
-  std::string model_name = _regression_model_selector(data);
-
   // If the data is more than 1e5 rows, sample 1e5 rows.
+  // TODO: The original Python implementation only sampled the training data for
+  // the purposes of determining what models to attempt. It used all the
+  // provided data for training. Probably this code path should do the same,
+  // although the running time of this function could increase dramatically for
+  // very large data sets.
   gl_sframe train_sframe;
   if (data.size() > 1e5) {
     float fraction = 1e5 / data.size();
@@ -111,16 +258,21 @@ std::shared_ptr<supervised_learning_model_base> create_automatic_regression_mode
     train_sframe = data;
   }
 
-  std::shared_ptr<supervised_learning_model_base> m;
-  if(model_name == "boosted_trees_regression") {
-    m = std::make_shared<xgboost::boosted_trees_regression>();
-  } else {
-    DASSERT_TRUE(model_name == "regression_linear_regression");
-    m = std::make_shared<linear_regression>();
-  }
-  m->api_train(train_sframe, target, validation_data, options);
+  // Determine what regression models to try.
+  using shared_model_ptr = std::shared_ptr<supervised_learning_model_base>;
+  std::vector<shared_model_ptr> models = get_regression_models(train_sframe);
+  ASSERT_GT(models.size(), 0);
 
-  return m; 
+  // Perform training on each model.
+  for (const shared_model_ptr& model : models) {
+    model->api_train(train_sframe, target, validation_data, options);
+  }
+
+  // Return the model with the lowest validation (or training) RMSE.
+  auto compare_rmse = [](const shared_model_ptr& a, const shared_model_ptr& b) {
+    return get_regression_rmse(a) < get_regression_rmse(b);
+  };
+  return *std::min_element(models.begin(), models.end(), compare_rmse);
 }
 
 std::pair<gl_sframe, gl_sframe> create_validation_data(gl_sframe data, const variant_type& _validation_data) {
