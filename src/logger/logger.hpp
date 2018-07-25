apple/turicreate

commit b5d1d5071aaf260b002d53adccd4a29ec9b70d1b

Clean up error messages on model save (#862)
* Clean up error messages on model save

* Adds a turi::error namespace and io_error class. This can be used to
  distinguish between ios_base::failure coming from the C++ standard
  library, and I/O errors we throw ourselves with a crafted message.
* Use the new io_error from our own log_and_throw macros.
* In some key places related to model saving, avoid handling and
  throwing a new error when we already have a turi::error.

The result is that in a few cases, the previous user-facing error
message about unknown errors has been replaced with meaningful error
messages on model save:

* When the directory path already exists as a file.
* When the directory fails to be created, and does not exist at all.

* Address code review feedback

src/logger/logger.hpp
@@ -49,6 +49,7 @@
 #include <timer/timer.hpp>
 #include <logger/fail_method.hpp>
 #include <logger/backtrace.hpp>
+#include <logger/error.hpp>
 #include <util/code_optimization.hpp> 
 #include <process/process_util.hpp>
 
@@ -345,7 +346,7 @@
   do {                                                                \
     auto throw_error = [&]() GL_COLD_NOINLINE_ERROR {    \
       logstream(LOG_ERROR) << (message) << std::endl;                 \
-      throw(std::ios_base::failure(message, std::error_code()));      \
+      throw(turi::error::io_error(message, std::error_code()));      \
     };                                                                \
     throw_error();                                                    \
   } while(0)
@@ -354,11 +355,19 @@
   do {                                                                \
     auto throw_error = [&]() GL_COLD_NOINLINE_ERROR {    \
       logstream(LOG_ERROR) << (message) << std::endl;                 \
-      throw(std::ios_base::failure(message));                         \
+      throw(turi::error::io_error(message));                         \
     };                                                                \
     throw_error();                                                    \
   } while(0)
 #endif
+
+#define log_and_throw_current_io_failure()                            \
+  do {                                                                \
+    auto error_code = errno;                                          \
+    std::string error_message = std::strerror(error_code);            \
+    errno = 0; // clear errno
+    log_and_throw_io_failure(error_message);                          \
+  } while(0)
 
 #define log_func_entry()                                       \
   do {                                                         \

src/fileio/fs_utils.cpp
@@ -272,6 +272,39 @@ EXPORT bool create_directory(const std::string& path) {
   return false;
 }
 
+EXPORT bool create_directory_or_throw(const std::string& path) {
+  bool result = create_directory(path);
+
+  // this function throws if the directory still doesn't exist
+  // at that location
+  file_status status = get_file_status(path);
+  switch (status) {
+    case file_status::MISSING:
+      log_and_throw_io_failure(
+        "Unable to create directory structure at " +
+        path +
+        ". Ensure that you have write permission to this location, or try again with a different path."
+      );
+      break;
+    case file_status::REGULAR_FILE:
+      log_and_throw_io_failure(
+        "Unable to create directory at " +
+        path +
+        ". A non-directory file already exists there. Delete that file, or try again with a different path."
+      );
+    case file_status::DIRECTORY:
+      // happy path, return below
+      break;
+    default:
+      // not sure what error message to give; fall back to current I/O error
+      log_and_throw_current_io_failure();
+  }
+
+  // result will still indicate whether it was created or not
+  // by this function call
+  return result;
+}
+
 EXPORT bool delete_path(const std::string& path,
                         file_status stat) {
   if (stat == file_status::FS_UNAVAILABLE) stat = get_file_status(path);

src/fileio/fs_utils.hpp
@@ -45,9 +45,20 @@ std::vector<std::pair<std::string, file_status>> get_directory_listing(const std
  * \ingroup fileio
  * Creates a directory and all parent required directories (like mkdir -p).
  * Path can be hdfs, s3, or regular filesystem.
- * Returns true on success, false on failure.
+ * Returns true on creation, false on failure or if the directory already exists.
+ * To get meaningful error messages thrown on failure, use create_directory_or_throw.
  */
 bool create_directory(const std::string& path);
+
+
+/**
+ * \ingroup fileio
+ * Creates a directory and all parent required directories (like mkdir -p).
+ * Path can be hdfs, s3, or regular filesystem.
+ * Returns true on creation, false if the directory already exists.
+ * Throws std::ios_base::failure on failure.
+ */
+bool create_directory_or_throw(const std::string& path);
 
 /**
  * \ingroup fileio

src/fileio/general_fstream.cpp
@@ -65,7 +65,12 @@ std::shared_ptr<std::istream> general_ifstream::get_underlying_stream() {
 /*****************************************************************************/
 
 general_ofstream::general_ofstream(std::string filename)
-    try :general_ofstream_base(filename), opened_filename(filename) { } catch (const std::exception& e) {
+    try :general_ofstream_base(filename), opened_filename(filename) {
+      // this space intentionally left blank
+    } catch (const std::ios_base::failure& e) {
+      // an I/O error is already the exception type - just re-throw
+      throw;
+    } catch (const std::exception& e) {
       log_and_throw_io_failure("Cannot open " + sanitize_url(filename) + " for write. " + e.what());
     } catch (std::string e) {
       log_and_throw_io_failure("Cannot open " + sanitize_url(filename) + " for write. " + e);

src/fileio/union_fstream.cpp
@@ -16,6 +16,10 @@
 #include <fileio/sanitize_url.hpp>
 #include <boost/algorithm/string.hpp>
 #include <fileio/fs_utils.hpp>
+
+#include <cstring>
+#include <cerrno>
+
 namespace turi {
 /**
  * A simple union of std::fstream and turi::hdfs::fstream, and turi::fileio::cache_stream.
@@ -97,8 +101,7 @@ union_fstream::union_fstream(std::string url,
       // Output stream must be a local openable file.
       output_stream.reset(new std::ofstream(url, std::ofstream::binary));
       if (!output_stream->good()) {
-        output_stream.reset();
-        log_and_throw_io_failure("Cannot open " + url + " for writing");
+        log_and_throw_current_io_failure();
       }
     } else {
 #ifdef TC_ENABLE_REMOTEFS

src/logger/CMakeLists.txt
@@ -8,6 +8,7 @@ project(Turi)
 
 make_library(logger
   SOURCES
+    error.cpp
     logger.cpp
     backtrace.cpp
     log_rotate.cpp

src/logger/error.cpp
@@ -1,0 +1,20 @@
+/* Copyright © 2018 Apple Inc. All rights reserved.
+ *
+ * Use of this source code is governed by a BSD-3-clause license that can
+ * be found in the LICENSE.txt file or at https://opensource.org/licenses/BSD-3-Clause
+ */
+
+/*
+ * Error types to distinguish Turi Create errors from arbitrary C++ exceptions
+ */
+
+#include <logger/error.hpp>
+
+using namespace turi::error;
+
+io_error::io_error(const std::string &message, const std::error_code &ec)
+  : std::ios_base::failure(message, ec), m_message(message) {}
+
+const char *io_error::what() const noexcept {
+  return m_message.c_str();
+}

src/logger/error.hpp
@@ -1,0 +1,26 @@
+/* Copyright © 2018 Apple Inc. All rights reserved.
+ *
+ * Use of this source code is governed by a BSD-3-clause license that can
+ * be found in the LICENSE.txt file or at https://opensource.org/licenses/BSD-3-Clause
+ */
+
+/*
+ * Error types to distinguish Turi Create errors from arbitrary C++ exceptions
+ */
+
+#include <ios>
+
+namespace turi {
+namespace error {
+
+class io_error : public std::ios_base::failure {
+  private:
+    std::string m_message;
+
+  public:
+    explicit io_error(const std::string &message, const std::error_code &ec = std::io_errc::stream);
+    virtual const char *what() const noexcept override;
+};
+
+} // namespace error
+} // namespace turi

commit 295668d383b94a91d5004654f0622ac9f1499f16

Fix build break caused by #862
Missed a trailing backslash to continue the macro.

src/logger/logger.hpp
@@ -365,7 +365,7 @@
   do {                                                                \
     auto error_code = errno;                                          \
     std::string error_message = std::strerror(error_code);            \
-    errno = 0; // clear errno
+    errno = 0; /* clear errno */                                      \
     log_and_throw_io_failure(error_message);                          \
   } while(0)
 