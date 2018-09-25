9dc39898a86d60998860b8601d1e3333db89ce3c
fareeha <fareeha@apple.com>
2018-09-25 12:26:15 -0700

    SFrame add_column FR (#1099)
    Feature Request 1072. add_column() in SFrame now handles non-SArray iterables as input and maps constant into SArray when noniterable passed.

--- src/unity/python/turicreate/test/test_sframe.py
+++ src/unity/python/turicreate/test/test_sframe.py
@@ -3444,6 +3444,39 @@ class SFrameTest(unittest.TestCase):
                 {'long': [12], 'unicode': [unicode('foo')]}))
 
         _assert_sframe_equal(sf, sf2)
+    
+    def test_add_column_nonSArray(self):
+        sf = SFrame()
+        sf = sf.add_column([1,2,3,4],'x')
+    
+        sf_test = SFrame()
+        sf_test['x'] = SArray([1,2,3,4])
+        
+        _assert_sframe_equal(sf, sf_test)
+    
+    
+    def test_add_column_noniterable1(self):
+        sf = SFrame()
+        sf = sf.add_column([1,2,3,4],'x')
+        sf = sf.add_column(5,'y')
+    
+        sf_test = SFrame()
+        sf_test['x'] = SArray([1,2,3,4])
+        sf_test['y'] = 5
+        
+        _assert_sframe_equal(sf, sf_test)
+    
+    
+
+    def test_add_column_noniterable2(self):
+        # If SFrame is empty then the passed data should be treated as an SArray of size 1
+        sf = SFrame()
+        sf = sf.add_column(5,'y')
+
+        sf_test = SFrame()
+        sf_test['y'] = SArray([5])
+        
+        _assert_sframe_equal(sf, sf_test)
 
 
 if __name__ == "__main__":
