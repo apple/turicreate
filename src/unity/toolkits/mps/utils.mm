bb8876efeefba2989af4e0334c32979575dacbdc
npratapa <abhishekpratapa@Abhisheks-MacBook-Pro.local>
2018-10-17 11:15:00 -0700

    finished the graph compiler

--- src/unity/toolkits/mps/utils.mm
+++ src/unity/toolkits/mps/utils.mm
@@ -30,7 +30,13 @@ namespace turi {
     namespace mps {
 
         std::shared_ptr<Graph> create_graph(std::map<std::string, std::shared_ptr<Layer>>& layer_dict, std::vector<std::shared_ptr<Layer>> layer_arr) {
-            if (@available(macOS 10.13, *)) {
+            /* TODO: MOVE THIS INTO COMPILE IN THE GRAPH OBJECT */
+
+            if (@available(macOS 10.13.4, *)) {
+                /* TODO: MOVE THIS OUT */
+                id<MTLDevice> dev = MTLCreateSystemDefaultDevice();
+                /* TODO: MOVE THIS OUT */
+
                 NSMutableDictionary *mps_layer_dictionary = [[NSMutableDictionary alloc] init];
                 for(auto const& l: layer_arr) {
                     switch (l->m_type) {
@@ -48,8 +54,8 @@ namespace turi {
                                 NSMutableArray *output_layer_images = [[NSMutableArray alloc]init];
                                 for(auto const& o: node->m_layers){
                                     NSString *layer_key = [NSString stringWithUTF8String:o.m_name.c_str()];
-                                    MPSNNImageNode *layer_images = [[mps_layer_dictionary objectForKey:layer_key] resultImage];
-                                    [output_layer_images addObject:layer_images];
+                                    MPSNNImageNode *layer_image = [[mps_layer_dictionary objectForKey:layer_key] resultImage];
+                                    [output_layer_images addObject:layer_image];
                                 }
                                 NSString *key = [NSString stringWithUTF8String:node->m_name.c_str()];
                                 [mps_layer_dictionary setObject:output_layer_images  forKey:key];
@@ -59,53 +65,132 @@ namespace turi {
                         case layer_type::addition:
                             {
                                 std::shared_ptr<AdditionNode> node = std::dynamic_pointer_cast<AdditionNode>(l);
+                                NSString *left_key = [NSString stringWithUTF8String:((node->m_left)->m_name).c_str()];
+                                NSString *right_key = [NSString stringWithUTF8String:((node->m_right)->m_name).c_str()];
+                                MPSNNImageNode *left_image = [[mps_layer_dictionary objectForKey:left_key] resultImage];
+                                MPSNNImageNode *right_image = [[mps_layer_dictionary objectForKey:right_key] resultImage];
+                                NSString *key = [NSString stringWithUTF8String:node->m_name.c_str()];
+                                AdditionLayer* addition_layer = [[AdditionLayer alloc] initWithParams:key
+                                                                                             leftNode:left_image
+                                                                                            rightNode:right_image];
+                                [mps_layer_dictionary setObject:addition_layer forKey:key];
                             }
                             break;
 
                         case layer_type::convolution:
                             {
-                                std::shared_ptr<ConvolutionNode> node = std::dynamic_pointer_cast<ConvolutionNode>(l); 
+                                std::shared_ptr<ConvolutionNode> node = std::dynamic_pointer_cast<ConvolutionNode>(l);
+                                /* TODO: Take care of the bias being null */
+                                NSString *key = [NSString stringWithUTF8String:node->m_name.c_str()];
+                                NSString *input_key = [NSString stringWithUTF8String:((node->m_input)->m_name).c_str()];
+                                MPSNNImageNode *input_image = [[mps_layer_dictionary objectForKey:input_key] resultImage];
+                                ConvolutionalLayer *convolution_layer = [[ConvolutionalLayer alloc] initWithParameters:key
+                                                                                                           kernelWidth:node->m_kernel_width
+                                                                                                          kernelHeight:node->m_kernel_height
+                                                                                                  inputFeatureChannels:node->m_input_feature_channels
+                                                                                                 outputFeatureChannels:node->m_output_feature_channels
+                                                                                                           strideWidth:node->m_stride_width
+                                                                                                          strideHeight:node->m_stride_height
+                                                                                                          paddingWidth:node->m_padding_width
+                                                                                                         paddingHeight:node->m_padding_height
+                                                                                                               weights:&(node->m_weights[0])
+                                                                                                                biases:NULL
+                                                                                                             inputNode:input_image
+                                                                                                                device:dev];
+                                [mps_layer_dictionary setObject:convolution_layer forKey:key];
                             }
                             break;
 
                         case layer_type::instance_norm:
                             {
                                 std::shared_ptr<InstanceNormNode> node = std::dynamic_pointer_cast<InstanceNormNode>(l);
+                                NSString *key = [NSString stringWithUTF8String:node->m_name.c_str()];
+                                NSString *input_key = [NSString stringWithUTF8String:((node->m_input)->m_name).c_str()];
+                                MPSNNImageNode *input_image = [[mps_layer_dictionary objectForKey:input_key] resultImage];
+                                
+                                std::vector<float*> gamma(node->m_gamma.size());
+                                for (int i = 0; i < node->m_gamma.size(); ++i)
+                                    gamma[i] = &*(node->m_gamma[i]).begin();
+
+                                std::vector<float*> beta(node->m_beta.size());
+                                for (int i = 0; i < node->m_beta.size(); ++i)
+                                    beta[i] = &*(node->m_beta[i]).begin();
+
+                                InstanceNormLayer *instance_norm_layer = [[InstanceNormLayer alloc] initWithParameters:key
+                                                                                                              channels:node->m_channels
+                                                                                                                styles:node->m_styles
+                                                                                                                 gamma:&(gamma[0])
+                                                                                                                  beta:&(beta[0])
+                                                                                                             inputNode:input_image
+                                                                                                                device:dev];
+                                [mps_layer_dictionary setObject:instance_norm_layer forKey:key];
                             }
                             break;
 
                         case layer_type::pooling:
                             {
                                 std::shared_ptr<PoolingNode> node = std::dynamic_pointer_cast<PoolingNode>(l);
+                                NSString *key = [NSString stringWithUTF8String:node->m_name.c_str()];
+                                NSString *input_key = [NSString stringWithUTF8String:((node->m_input)->m_name).c_str()];
+                                MPSNNImageNode *input_image = [[mps_layer_dictionary objectForKey:input_key] resultImage];
+                                AveragePoolingLayer* pooling_layer = [[AveragePoolingLayer alloc] initWithParams:key
+                                                                                                       inputNode:input_image
+                                                                                                     kernelWidth:node->m_kernel_width 
+                                                                                                    kernelHeight:node->m_kernel_height 
+                                                                                                     strideWidth:node->m_stride_in_pixels_x 
+                                                                                                    strideHeight:node->m_stride_in_pixels_y];
+
+                                [mps_layer_dictionary setObject:pooling_layer forKey:key];
                             }
                             break;
 
                         case layer_type::relu:
                             {
                                 std::shared_ptr<ReluNode> node = std::dynamic_pointer_cast<ReluNode>(l);
+                                NSString *key = [NSString stringWithUTF8String:node->m_name.c_str()];
+                                NSString *input_key = [NSString stringWithUTF8String:((node->m_input)->m_name).c_str()];
+                                MPSNNImageNode *input_image = [[mps_layer_dictionary objectForKey:input_key] resultImage];
+                                ReluLayer* relu_layer = [[ReluLayer alloc] initWithParams:key
+                                                                                inputNode:input_image];
+                                [mps_layer_dictionary setObject:relu_layer forKey:key];
                             }
                             break;
 
                         case layer_type::sigmoid:
                             {
                                 std::shared_ptr<SigmoidNode> node = std::dynamic_pointer_cast<SigmoidNode>(l);
+                                NSString *key = [NSString stringWithUTF8String:node->m_name.c_str()];
+                                NSString *input_key = [NSString stringWithUTF8String:((node->m_input)->m_name).c_str()];
+                                MPSNNImageNode *input_image = [[mps_layer_dictionary objectForKey:input_key] resultImage];
+                                SigmoidLayer* sigmoid_layer = [[SigmoidLayer alloc] initWithParams:key
+                                                                                         inputNode:input_image];
+                                [mps_layer_dictionary setObject:sigmoid_layer forKey:key];
                             }
                             break;
 
                         case layer_type::upsampling:
                             {
                                 std::shared_ptr<UpsamplingNode> node = std::dynamic_pointer_cast<UpsamplingNode>(l);
+                                NSString *key = [NSString stringWithUTF8String:node->m_name.c_str()];
+                                NSString *input_key = [NSString stringWithUTF8String:((node->m_input)->m_name).c_str()];
+                                MPSNNImageNode *input_image = [[mps_layer_dictionary objectForKey:input_key] resultImage];
+                                
+                                NearestUpsamplingLayer* upsample_layer = [[NearestUpsamplingLayer alloc] initWithParams:key
+                                                                                                                 scaleX:node->m_scale_x
+                                                                                                                 scaleY:node->m_scale_y
+                                                                                                              inputNode:input_image];
+                                [mps_layer_dictionary setObject:upsample_layer forKey:key];
                             }
                             break;
                         default:
                             {
-                                std::cout << "TMPS - Layer Type not supported. name: " << l->m_name;
+                                std::cout << "TMPS - Layer Type not supported. name: " << l->m_name << std::endl;
                             }
                             break;
                     }
                 }
             } else {
-                // Print Warning, MPSGraph isn't supported
+                std::cout << "TCMPS not supported on this machine." << std::endl;
             }
 
             // TODO: populate graph
