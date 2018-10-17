eb42075597e4a9e985aee7757442d724de100026
Nicholas Jong <njong@apple.com>
2018-10-17 13:50:38 -0700

    Fixup: Resolve linking errors for libRecommender.dylib

--- src/unity/toolkits/neural_net/cnn_module.cpp
+++ src/unity/toolkits/neural_net/cnn_module.cpp
@@ -6,7 +6,7 @@
 
 #include "cnn_module.hpp"
 
-#ifdef HAS_MPS
+#if defined(HAS_MPS) && !defined(TC_BUILD_IOS)
 #include "mps_cnn_module_factory.h"
 #endif
 
@@ -18,7 +18,7 @@ std::unique_ptr<cnn_module> cnn_module::create_object_detector(
     int n, int c_in, int h_in, int w_in, int c_out, int h_out, int w_out,
     const float_array_map& config, const float_array_map& weights) {
 
-#ifdef HAS_MPS
+#if defined(HAS_MPS) && !defined(TC_BUILD_IOS)
   return create_mps_object_detector(n, c_in, h_in, w_in, c_out, h_out, w_out,
                                     config, weights);
 #else
