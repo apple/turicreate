2da957ffd91f64769efc1d32558bbdec5450642c
Toby Roseman <troseman@apple.com>
2018-11-14 09:49:09 -0800

    Fixes Copyright Headers (#1185)

--- src/unity/toolkits/coreml_export/MLModel/src/WordTaggerValidator.cpp
+++ src/unity/toolkits/coreml_export/MLModel/src/WordTaggerValidator.cpp
@@ -1,11 +1,8 @@
-//
-//  WordTaggerValidator.cpp
-//  mlmodel
-//
-//  Created by Tao Jia on 3/21/18.
-//  Copyright © 2018 Apple Inc. All rights reserved.
-//
-
+/* Copyright © 2017 Apple Inc. All rights reserved.
+ *
+ * Use of this source code is governed by a BSD-3-clause license that can
+ * be found in the LICENSE.txt file or at https://opensource.org/licenses/BSD-3-Clause
+ */
 #include "Result.hpp"
 #include "Validators.hpp"
 #include "ValidatorUtils-inl.hpp"
--- src/unity/toolkits/neural_net/mps_lstm_helper.h
+++ src/unity/toolkits/neural_net/mps_lstm_helper.h
@@ -1,11 +1,8 @@
-//
-//  mps_lstm_helper.h
-//  tcmps
-//
-//  Created by ilai giloh on 29/04/2018.
-//  Copyright © 2018 Turi. All rights reserved.
-//
-
+/* Copyright © 2017 Apple Inc. All rights reserved.
+ *
+ * Use of this source code is governed by a BSD-3-clause license that can
+ * be found in the LICENSE.txt file or at https://opensource.org/licenses/BSD-3-Clause
+ */
 #ifndef mps_lstm_helper_h
 #define mps_lstm_helper_h
 
