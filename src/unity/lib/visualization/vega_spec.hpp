acd21fa5203bc0ce6b153ad25f9b5ef3eccf3f2e
Zach Nation <znation@apple.com>
2019-01-13 12:38:29 -0800

    Implement dark mode plot API
    Completed in this change:
    
    * C and C++ API for retrieving and showing dark mode Vega spec for a Plot object
    * macOS support for autodetecting dark mode
    
    Not yet implemented (will come in future changes):
    
    * Python API for retrieving and showing dark mode Vega spec for a Plot object
    * Linux autodetect (TBD: can this be done w.r.t. popular desktop
      environments or via a standard?)
    * Proper UX support for dark mode (i.e. the native app changes
      background color and switches appropriately in real time).
    * Fix automatic detection of dark mode
    * Also fixes title (vs. axis title) styling.

--- src/unity/lib/visualization/vega_spec.hpp
+++ src/unity/lib/visualization/vega_spec.hpp
@@ -23,6 +23,8 @@ namespace turi {
     std::string boxes_and_whiskers_spec(const flexible_type& xlabel, const flexible_type& ylabel, const flexible_type& title);
 
     // Utility for escaping JSON string literals. Not concerned with Vega implications of the contents of those strings.
+    std::string make_format_string(unsigned char *raw_format_str_ptr,
+                                           size_t raw_format_str_len);
     std::string escape_string(const std::string& str, bool include_quotes=true);
     std::string replace_all(std::string str, const std::string& from, const std::string& to);
     std::string extra_label_escape(const std::string& str, bool include_quotes=true);
