59f0e727ac88d987bb04f6e81ed28c51416889f6
Shantanu Chhabra <shantanu_chhabra@apple.com>
2019-02-21 21:20:37 -0800

    Added code to convert stroke-based drawings into bitmaps for non-Mac paths

--- src/unity/toolkits/drawing_recognition/data_preparation.cpp
+++ src/unity/toolkits/drawing_recognition/data_preparation.cpp
@@ -8,12 +8,16 @@
 #include <math.h>
 #include <iomanip>
 #include <logger/assertions.hpp>
+#include <image/image_type.hpp>
 #include "data_preparation.hpp"
 #include "random/random.hpp"
 #include "util/sys_util.hpp"
 
 namespace turi {
 namespace drawing_recognition {
+
+#define MIN(a,b) (((a)<(b))?(a):(b))
+#define MAX(a,b) (((a)>(b))?(a):(b))
 
 struct Point {
     float x;
@@ -30,7 +34,6 @@ struct Line {
 
 void make_point(Point *P, const flex_dict &point_dict) {
     flex_string x ("x");
-    flex_string y ("y");
     for (auto key_value_pair: point_dict) {
         const flex_string &key = key_value_pair.first.get<flex_string>();
         const flex_float &value = key_value_pair.second.get<flex_float>();
@@ -106,7 +109,7 @@ flex_list ramer_douglas_peucker(flex_list stroke, float epsilon) {
     return compressed_stroke;
 }
 
-flex_list simplify_stroke(flex_list raw_drawing) {
+flex_list simplify_drawing(flex_list raw_drawing) {
     size_t num_strokes = raw_drawing.size();
     size_t min_x = INT_MAX;
     size_t max_x = 0;
@@ -161,16 +164,103 @@ flex_list simplify_stroke(flex_list raw_drawing) {
     return simplified_drawing;
 }
 
-flex_list rasterize(flex_list simplified_drawing) {
-#ifdef __APPLE__
-    int MAC_OS_STRIDE = 64;
+void swap_points(Point *P1, Point *P2) {
+    float temp_x, temp_y;
+    if (P1->x > P2->x) {
+        temp_x = P1->x;
+        temp_y = P1->y;
+        P1->x = P2->x;
+        P1->y = P2->y;
+        P2->x = temp_x;
+        P2->y = temp_y;
+    }
+}
+
+bool in_bounds(int x, int y, int dim) {
+    return (x >= 0 && x < dim && y >= 0 && y < dim);
+}
+
+flex_nd_vec paint_stroke(
+    flex_nd_vec bitmap, Point start, Point end, float stroke_width) {
+    swap_points(&start, &end);
+    float slope = (end.y - start.y)/(end.x - start.x);
+    int x1 = (int)(start.x);
+    int y1 = (int)(start.y);
+    int x2 = (int)(end.x);
+    int y2 = (int)(end.y);
+    int pad = (int)(stroke_width/2);
+    size_t dimension = bitmap.shape()[1];
+    if (x1 == x2) {
+        // just paint the y axis
+        int min_y = MIN(y1, y2);
+        int max_y = MAX(y1, y2);
+        for (int y = min_y; y < max_y; y++) {
+            for (int dx = -pad; dx < pad; dx++) {
+                for (int dy = -pad; dy < pad; dy++) {
+                    // figure out how to do this with the flex_list
+                    if (in_bounds(x1+dx, y+dy, dimension)) {
+                        bitmap[(y+dy)*dimension + (x1+dx)] = 1.0;
+                    }
+                }
+            }
+        }
+    } else {
+        for (int x = x1; x < x2; x++) {
+            int y = (int)(slope * (x - x1) + y1);
+            for (int dx = -pad; dx < pad; dx++) {
+                for (int dy = -pad; dy < pad; dy++) {
+                    if (in_bounds(x+dx, y+dy, dimension)) {
+                        bitmap[(y+dy) * dimension + (x+dx)] = 1.0;
+                    }
+                }
+            }
+        }
+    }
+    return bitmap;
+}
+
+flex_image blur_bitmap(flex_nd_vec bitmap, int ksize) {
+    std::vector<size_t> bitmap_shape = bitmap.shape();
+    flex_nd_vec blurred_bitmap(bitmap_shape, 0.0);
+    int dimension = bitmap_shape[1];
+    int pad = ksize/2;
+    for (int row = 0; row < dimension; row++) {
+        for (int col = 0; col < dimension; col++) {
+            int index = row * dimension + col;
+            if (row < pad 
+                || row >= dimension-pad 
+                || col < pad 
+                || col >= dimension-pad) {
+                blurred_bitmap[index] = (int)(255*bitmap[index]);
+                continue;
+            }
+            double sum_for_blur = 0.0;
+            for (int dr = -pad; dr <= pad; dr++) {
+                for (int dc = -pad; dc <= pad; dc++) {
+                    sum_for_blur += bitmap[(row+dr) * dimension + (col+dc)];
+                }
+            }
+            blurred_bitmap[index] = (int)(255*sum_for_blur/(ksize*ksize));
+        }
+    }
+    uint8_t image_data[dimension * dimension];
+    for (int idx=0; idx < dimension * dimension; idx++) {
+        image_data[idx] = ((uint8_t)(blurred_bitmap[idx]));
+    }
+    return flex_image((const char *)image_data,
+        dimension, dimension, 1, dimension*dimension, 1, 2);
+}
+
+flex_image rasterize(flex_list simplified_drawing) {
+    flex_image final_bitmap; // 1 x 28 x 28
+    size_t num_strokes = simplified_drawing.size();
     int INTERMEDIATE_BITMAP_WIDTH = 256;
     int INTERMEDIATE_BITMAP_HEIGHT = 256;
     int FINAL_BITMAP_WIDTH = 28;
     int FINAL_BITMAP_HEIGHT = 28;
     float STROKE_WIDTH = 20.0;
-    size_t num_strokes = simplified_drawing.size();
-    flex_list final_bitmap; // 1 x 28 x 28
+    int MAC_OS_STRIDE = 64;
+#ifdef __APPLE__
     CGColorSpaceRef grayscale = CGColorSpaceCreateDeviceGray();
     CGContextRef bitmap_context = CGBitmapContextCreate(
         NULL, INTERMEDIATE_BITMAP_WIDTH, INTERMEDIATE_BITMAP_HEIGHT, 
@@ -211,30 +301,54 @@ flex_list rasterize(flex_list simplified_drawing) {
     CGRect new_rect = CGRectMake(0.0, 0.0, 
         ((float)(FINAL_BITMAP_WIDTH)), ((float)(FINAL_BITMAP_HEIGHT)));
     CGContextDrawImage(scale_28_context, new_rect, cropped_image);
-    CGImageRef cropped_image_gray = CGBitmapContextCreateImage(scale_28_context);
+    CGImageRef cropped_image_gray = CGBitmapContextCreateImage(
+        scale_28_context);
     CFDataRef pixel_data = CGDataProviderCopyData(
         CGImageGetDataProvider(cropped_image_gray));
     const uint8_t* data_ptr = CFDataGetBytePtr(pixel_data);
-    flex_list outer_layer;
+    uint8_t real_data[FINAL_BITMAP_WIDTH * FINAL_BITMAP_HEIGHT];
     for (size_t row = 0; row < FINAL_BITMAP_WIDTH; row++) {
-        flex_list current_row;
         for (size_t col = 0; col < FINAL_BITMAP_HEIGHT; col++) {
-            // check platform here okay
-            uint32_t val = (uint32_t)(data_ptr[row * MAC_OS_STRIDE + col]);
-            current_row.push_back(val/255.0);
+            // check platform here
+            real_data[row*FINAL_BITMAP_WIDTH+col] = (uint8_t)(
+                data_ptr[row * MAC_OS_STRIDE + col]);
         }
-        outer_layer.push_back(current_row);
     }
-    final_bitmap.push_back(outer_layer);
-    return final_bitmap;
+    return flex_image((const char *)real_data, 
+        FINAL_BITMAP_WIDTH, FINAL_BITMAP_HEIGHT, 1, 
+        FINAL_BITMAP_WIDTH * FINAL_BITMAP_HEIGHT, 1, 2);
 #endif // __APPLE__
-    return simplified_drawing;
+    Point last_point;
+    std::vector<size_t> intermediate_bitmap_shape {1, (size_t)INTERMEDIATE_BITMAP_WIDTH, (size_t)INTERMEDIATE_BITMAP_HEIGHT};
+    flex_nd_vec intermediate_bitmap(intermediate_bitmap_shape, 0.0);
+    for (size_t i = 0; i < num_strokes; i++) {
+        const flex_list &stroke = simplified_drawing[i].get<flex_list>();
+        const flex_dict &start_point_dict = stroke[0].get<flex_dict>();
+        size_t num_points_in_stroke = stroke.size();
+        Point start; make_point(&start, start_point_dict);
+        last_point = start;
+        for (size_t j = 1; j < num_points_in_stroke; j++) {
+            const flex_dict &next_point_dict = stroke[j].get<flex_dict>();
+            Point next; make_point(&next, next_point_dict);
+            intermediate_bitmap = paint_stroke(
+                intermediate_bitmap, last_point, next, STROKE_WIDTH);
+            last_point = next;
+        }
+    }
+    int CHOSEN_KERNEL_SIZE = 7;
+    final_bitmap = blur_bitmap(intermediate_bitmap, CHOSEN_KERNEL_SIZE);
+    final_bitmap = image_util::resize_image(
+        final_bitmap, 
+        FINAL_BITMAP_WIDTH, 
+        FINAL_BITMAP_HEIGHT, 
+        1, true).get<flex_image>();
+    return final_bitmap;
 }
 
-flex_list convert_stroke_based_drawing_to_bitmap(
+flex_image convert_stroke_based_drawing_to_bitmap(
     flex_list stroke_based_drawing) {
-    flex_list normalized_drawing = simplify_stroke(stroke_based_drawing);
-    flex_list bitmap = rasterize(normalized_drawing);
+    flex_list normalized_drawing = simplify_drawing(stroke_based_drawing);
+    flex_image bitmap = rasterize(normalized_drawing);
     return bitmap;
 }
 
@@ -243,20 +357,20 @@ gl_sframe _drawing_recognition_prepare_data(const gl_sframe &data,
                                             const std::string &target) {
     DASSERT_TRUE(data.contains_column(feature));
     DASSERT_TRUE(data.contains_column(target));
-
+    
     gl_sarray bitmaps = data[feature].apply([](const flexible_type &strokes) {
         flex_list current_stroke_based_drawing = strokes.to<flex_list>();
-        flex_list current_bitmap = convert_stroke_based_drawing_to_bitmap(
+        flex_image current_bitmap = convert_stroke_based_drawing_to_bitmap(
             current_stroke_based_drawing);
         return current_bitmap;
-    }, flex_type_enum::LIST);
+    }, flex_type_enum::IMAGE);
 
     gl_sframe converted_sframe = gl_sframe(data);
-    converted_sframe.replace_add_column(bitmaps, "bitmap");
+    converted_sframe.replace_add_column(bitmaps, feature);
     
     converted_sframe.materialize();
     return converted_sframe;
-}    
+}
 
 } //drawing_recognition
 } //turi
