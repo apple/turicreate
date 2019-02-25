100d9f840dac07230ac32c66ab2e11673c23be16
Shantanu Chhabra <shantanu_chhabra@apple.com>
2019-02-25 11:38:27 -0800

    changed copyright 2017 to 2019 and moved the CoreGraphics include

--- src/unity/toolkits/drawing_recognition/data_preparation.hpp
+++ src/unity/toolkits/drawing_recognition/data_preparation.hpp
@@ -1,4 +1,4 @@
-/* Copyright © 2017 Apple Inc. All rights reserved.
+/* Copyright © 2019 Apple Inc. All rights reserved.
  *
  * Use of this source code is governed by a BSD-3-clause license that can
  * be found in the LICENSE.txt file or at https://opensource.org/licenses/BSD-3-Clause
@@ -10,9 +10,6 @@
 #include <unity/lib/gl_sframe.hpp>
 #include <unity/lib/variant.hpp>
 #include <unity/lib/image_util.hpp>
-#ifdef __APPLE__
-#include <CoreGraphics/CoreGraphics.h>
-#endif // __APPLE__
 
 namespace turi {
 namespace drawing_recognition {
