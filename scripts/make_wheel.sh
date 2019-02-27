b7dcbd11075d4ec33ea93c1b1b886f1d50320bf8
Zach Nation <znation@apple.com>
2019-02-26 21:08:29 -0800

    Fix syntax in make_wheel.sh

--- scripts/make_wheel.sh
+++ scripts/make_wheel.sh
@@ -118,7 +118,7 @@ if [[ -n "${USE_DOCKER}" ]]; then
   # Run the make_wheel.sh script inside Docker
   docker run --rm \
     --mount type=bind,source=$WORKSPACE,target=/build,consistency=delegated \
-    -e "VIRTUALENV=virtualenv --python=python${DOCKER_PYTHON}"
+    -e "VIRTUALENV=virtualenv --python=python${DOCKER_PYTHON}" \
     -it turicreate/build-image-10.04:${TC_BUILD_IMAGE_VERSION} \
     /build/scripts/make_wheel.sh \
     $make_wheel_args
