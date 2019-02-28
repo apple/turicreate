cce2016275333eecadd3efa5eba3d3ced8b8d11d
Shantanu Chhabra <shantanu_chhabra@apple.com>
2019-02-27 17:17:46 -0800

    added constructor for Point

--- src/unity/toolkits/drawing_recognition/data_preparation.cpp
+++ src/unity/toolkits/drawing_recognition/data_preparation.cpp
@@ -10,12 +10,13 @@
 #include <iomanip>
 #include <logger/assertions.hpp>
 #include <image/image_type.hpp>
+#include <random/random.hpp>
+#include <util/sys_util.hpp>
 #ifdef __APPLE__
 #include <CoreGraphics/CoreGraphics.h>
 #endif // __APPLE__
+
 #include "data_preparation.hpp"
-#include "random/random.hpp"
-#include "util/sys_util.hpp"
 
 namespace turi {
 namespace drawing_recognition {
@@ -25,6 +26,17 @@ namespace {
     struct Point {
         float x;
         float y;
+        Point(const flex_dict &point_dict) {
+            for (auto key_value_pair: point_dict) {
+                const flex_string &key = key_value_pair.first.get<flex_string>();
+                const flex_float &value = key_value_pair.second.get<flex_float>();
+                if (key == "x") {
+                    x = value;
+                } else {
+                    y = value;
+                }
+            }
+        }
     };
 
     struct Line {
@@ -36,20 +48,8 @@ namespace {
 
 } // namespace
 
-void make_point(Point *P, const flex_dict &point_dict) {
-    for (auto key_value_pair: point_dict) {
-        const flex_string &key = key_value_pair.first.get<flex_string>();
-        const flex_float &value = key_value_pair.second.get<flex_float>();
-        if (key == "x") {
-            P->x = value;
-        } else {
-            P->y = value;
-        }
-    }
-}
-
 float line_to_point_distance(Line *L, const flex_dict &point) {
-    Point P; make_point(&P, point);
+    Point P(point);
     int x = (int)(P.x);
     int y = (int)(P.y);
     float a = L->a; float b = L->b; float c = L->c;
@@ -58,14 +58,14 @@ float line_to_point_distance(Line *L, const flex_dict &point) {
 }
 
 void initialize_line(Line *L, const flex_dict &p1, const flex_dict &p2) {
-    Point P1; make_point(&P1, p1);
-    Point P2; make_point(&P2, p2);
+    Point P1(p1);
+    Point P2(p2);
     float start_x = P1.x;
     float start_y = P1.y;
     float end_x = P2.x;
     float end_y = P2.y;
     if (start_x == end_x) {
-        L->a = INT_MAX;
+        L->a = std::numeric_limits<float>::max();
     } else {
         L->a = ((float)(end_y - start_y))/((float)(end_x - start_x));
     }
@@ -116,24 +116,23 @@ flex_list ramer_douglas_peucker(flex_list stroke, float epsilon) {
 
 flex_list simplify_drawing(flex_list raw_drawing) {
     size_t num_strokes = raw_drawing.size();
-    size_t min_x = INT_MAX;
+    size_t min_x = std::numeric_limits<size_t>::max();
     size_t max_x = 0;
-    size_t min_y = INT_MAX;
+    size_t min_y = std::numeric_limits<size_t>::max();
     size_t max_y = 0;
     flex_list simplified_drawing;
-    Point P;
     // Compute bounds of the drawing
     for (size_t i = 0; i < num_strokes; i++) {
         const flex_list &stroke = raw_drawing[i].get<flex_list>();
         for (size_t j = 0; j < stroke.size(); j++) {
             const flex_dict &point = stroke[j].get<flex_dict>();
-            make_point(&P, point);
+            Point P(point);
             float x = P.x;
             float y = P.y;
-            if (x < min_x) min_x = x;
-            if (x > max_x) max_x = x;
-            if (y < min_y) min_y = y;
-            if (y > max_y) max_y = y;
+            min_x = std::min((size_t)x, min_x);
+            max_x = std::max((size_t)x, max_x);
+            min_y = std::min((size_t)y, min_y);
+            max_y = std::max((size_t)y, max_y);
         }
     }
     // Align the drawing to top-left corner and scale to [0,255]
@@ -144,7 +143,7 @@ flex_list simplify_drawing(flex_list raw_drawing) {
             float new_x;
             float new_y;
             flex_dict &point = stroke[j].mutable_get<flex_dict>();
-            make_point(&P, point);
+            Point P(point);
             if (max_x == min_x) {
                 // vertical straight line
                 new_x = (float)min_x;
@@ -163,22 +162,9 @@ flex_list simplify_drawing(flex_list raw_drawing) {
             new_stroke.push_back(new_point);
         }
         // Apply RDP Line Algorithm
-        flex_list compressed_stroke = ramer_douglas_peucker(new_stroke, 2.0);
-        simplified_drawing.push_back(compressed_stroke);
+        simplified_drawing.push_back(ramer_douglas_peucker(new_stroke, 2.0));
     }
     return simplified_drawing;
-}
-
-void swap_points(Point *P1, Point *P2) {
-    float temp_x, temp_y;
-    if (P1->x > P2->x) {
-        temp_x = P1->x;
-        temp_y = P1->y;
-        P1->x = P2->x;
-        P1->y = P2->y;
-        P2->x = temp_x;
-        P2->y = temp_y;
-    }
 }
 
 bool in_bounds(int x, int y, int dim) {
@@ -187,7 +173,9 @@ bool in_bounds(int x, int y, int dim) {
 
 flex_nd_vec paint_stroke(
     flex_nd_vec bitmap, Point start, Point end, float stroke_width) {
-    swap_points(&start, &end);
+    if (start.x > end.x) {
+        std::swap(start, end);
+    }
     float slope = (end.y - start.y)/(end.x - start.x);
     int x1 = (int)(start.x);
     int y1 = (int)(start.y);
@@ -202,7 +190,6 @@ flex_nd_vec paint_stroke(
         for (int y = min_y; y < max_y; y++) {
             for (int dx = -pad; dx < pad; dx++) {
                 for (int dy = -pad; dy < pad; dy++) {
-                    // figure out how to do this with the flex_list
                     if (in_bounds(x1+dx, y+dy, dimension)) {
                         bitmap[(y+dy)*dimension + (x1+dx)] = 1.0;
                     }
@@ -259,13 +246,13 @@ flex_image blur_bitmap(flex_nd_vec bitmap, int ksize) {
 flex_image rasterize(flex_list simplified_drawing) {
     flex_image final_bitmap; // 1 x 28 x 28
     size_t num_strokes = simplified_drawing.size();
-    int INTERMEDIATE_BITMAP_WIDTH = 256;
-    int INTERMEDIATE_BITMAP_HEIGHT = 256;
-    int FINAL_BITMAP_WIDTH = 28;
-    int FINAL_BITMAP_HEIGHT = 28;
+    size_t INTERMEDIATE_BITMAP_WIDTH = 256;
+    size_t INTERMEDIATE_BITMAP_HEIGHT = 256;
+    size_t FINAL_BITMAP_WIDTH = 28;
+    size_t FINAL_BITMAP_HEIGHT = 28;
     float STROKE_WIDTH = 20.0;
-    int MAC_OS_STRIDE = 64;
 #ifdef __APPLE__
+    int MAC_OS_STRIDE = 64;
     CGColorSpaceRef grayscale = CGColorSpaceCreateDeviceGray();
     CGContextRef intermediate_bitmap_context = CGBitmapContextCreate(
         NULL, INTERMEDIATE_BITMAP_WIDTH, INTERMEDIATE_BITMAP_HEIGHT, 
@@ -277,7 +264,7 @@ flex_image rasterize(flex_list simplified_drawing) {
         const flex_list &stroke = simplified_drawing[i].get<flex_list>();
         const flex_dict &start_point_dict = stroke[0].get<flex_dict>();
         size_t num_points_in_stroke = stroke.size();
-        Point P; make_point(&P, start_point_dict);
+        Point P(start_point_dict);
         // @TODO: When we make a dylib, we'll need some sophisticated 
         // iOS/macOS checking here to figure out whether we need to 
         // subtract 256 or not........
@@ -285,7 +272,7 @@ flex_image rasterize(flex_list simplified_drawing) {
             P.x, ((float)(INTERMEDIATE_BITMAP_WIDTH))-P.y);
         for (size_t j = 1; j < num_points_in_stroke; j++) {
             const flex_dict &point_dict = stroke[j].get<flex_dict>();
-            make_point(&P, point_dict);
+            Point P(point_dict);
             CGPathAddLineToPoint(path, &transform, 
                 P.x, ((float)(INTERMEDIATE_BITMAP_WIDTH))-P.y);
         }
@@ -326,18 +313,16 @@ flex_image rasterize(flex_list simplified_drawing) {
         FINAL_BITMAP_WIDTH, FINAL_BITMAP_HEIGHT, 1, 
         FINAL_BITMAP_WIDTH * FINAL_BITMAP_HEIGHT, 1, 2);
 #endif // __APPLE__
-    Point last_point;
     std::vector<size_t> intermediate_bitmap_shape {1, (size_t)INTERMEDIATE_BITMAP_WIDTH, (size_t)INTERMEDIATE_BITMAP_HEIGHT};
     flex_nd_vec intermediate_bitmap(intermediate_bitmap_shape, 0.0);
     for (size_t i = 0; i < num_strokes; i++) {
         const flex_list &stroke = simplified_drawing[i].get<flex_list>();
         const flex_dict &start_point_dict = stroke[0].get<flex_dict>();
         size_t num_points_in_stroke = stroke.size();
-        Point start; make_point(&start, start_point_dict);
-        last_point = start;
+        Point last_point(start_point_dict);
         for (size_t j = 1; j < num_points_in_stroke; j++) {
             const flex_dict &next_point_dict = stroke[j].get<flex_dict>();
-            Point next; make_point(&next, next_point_dict);
+            Point next(next_point_dict);
             intermediate_bitmap = paint_stroke(
                 intermediate_bitmap, last_point, next, STROKE_WIDTH);
             last_point = next;
