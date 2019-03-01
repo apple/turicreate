7b714aea496be531cad7e29324df3b000a02faeb
Shantanu Chhabra <shantanu_chhabra@apple.com>
2019-02-28 18:04:16 -0800

    Copyright header

--- src/unity/python/turicreate/toolkits/drawing_recognition/util/_visualization.py
+++ src/unity/python/turicreate/toolkits/drawing_recognition/util/_visualization.py
@@ -1,3 +1,8 @@
+# -*- coding: utf-8 -*-
+# Copyright © 2019 Apple Inc. All rights reserved.
+#
+# Use of this source code is governed by a BSD-3-clause license that can
+# be found in the LICENSE.txt file or at https://opensource.org/licenses/BSD-3-Clause
 import turicreate as _tc
 import numpy as _np
 from turicreate import extensions as _extensions
