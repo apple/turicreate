0c7a9cafbb1997453403bdf26d609a6fff5d65f1
Shantanu Chhabra <shantanu_chhabra@apple.com>
2019-03-01 14:10:07 -0800

    Added docstring, along with single input support

--- src/unity/python/turicreate/toolkits/drawing_recognition/util/_visualization.py
+++ src/unity/python/turicreate/toolkits/drawing_recognition/util/_visualization.py
@@ -7,15 +7,45 @@ import turicreate as _tc
 from turicreate import extensions as _extensions
 
 def draw_strokes(stroke_based_drawings):
-    if type(stroke_based_drawings) != _tc.SArray:
+    """
+    Visualizes drawings (ground truth or predictions) by
+    returning images to represent the stroke-based data from 
+    the user.
+
+    Parameters
+    ----------
+    stroke_based_drawings: SArray or list
+        An `SArray` of type `list`. Each element in the SArray 
+        should be a list of strokes, where each stroke is a list
+        of points, and each point is represented as a dictionary
+        with two keys, "x" and "y". A single stroke-based drawing
+        is also supported, in which case, the type of the input
+        would be list
+        
+    Returns
+    -------
+    drawings: SArray or _tc.Image
+        Each stroke-based drawing is converted into a 28x28 
+        grayscale drawing for the user to visualize what their
+        strokes traced.
+
+    """
+    single_input = False
+    if type(stroke_based_drawings) != _tc.SArray and type(stroke_based_drawings) != list:
         raise _ToolkitError("Input to draw_strokes must be of type " 
-            + "turicreate.SArray")
-    is_stroke_input = (stroke_based_drawings.dtype != _tc.Image)
-    if is_stroke_input:
-        sf = _tc.SFrame({"drawings": stroke_based_drawings})
-        sf_with_drawings = _extensions._drawing_recognition_prepare_data(
-            sf, "drawings")
-        return sf_with_drawings["drawings"]
-    else:
-        # User passed in drawings that were already drawings
-        return stroke_based_drawings
+            + "turicreate.SArray or list (for a single stroke-based drawing)")
+    if type(stroke_based_drawings) == _tc.SArray and stroke_based_drawings.dtype != list:
+        raise _ToolkitError("SArray input to draw_strokes must have dtype " 
+            + "list. Each element in the SArray should be a list of strokes, " 
+            + "where each stroke is a list of points, " 
+            + "and each point is represented as a dictionary " 
+            + "with two keys, \"x\" and \"y\".")
+    if type(stroke_based_drawings) == list:
+        single_input = True
+        stroke_based_drawings = _tc.SArray([stroke_based_drawings])
+    sf = _tc.SFrame({"drawings": stroke_based_drawings})
+    sf_with_drawings = _extensions._drawing_recognition_prepare_data(
+        sf, "drawings")
+    if single_input:
+        return sf_with_drawings["drawings"][0]
+    return sf_with_drawings["drawings"]
