d4d7b9dc763c34b2023516be2f897d8dd99cfabf
Shantanu Chhabra <shantanu_chhabra@apple.com>
2019-02-28 18:05:15 -0800

    not using numpy

--- src/unity/python/turicreate/toolkits/drawing_recognition/util/_visualization.py
+++ src/unity/python/turicreate/toolkits/drawing_recognition/util/_visualization.py
@@ -4,7 +4,6 @@
 # Use of this source code is governed by a BSD-3-clause license that can
 # be found in the LICENSE.txt file or at https://opensource.org/licenses/BSD-3-Clause
 import turicreate as _tc
-import numpy as _np
 from turicreate import extensions as _extensions
 
 def draw_strokes(stroke_based_drawings):
