fbea8061b1524de588fb6f7d2a4868927252490b
Hoyt Koepke <hoytak@apple.com>
2019-03-04 16:06:40 -0800

    Fix for bit mask to make corner case more portable.

--- src/util/bitops.hpp
+++ src/util/bitops.hpp
@@ -10,6 +10,7 @@
 #include <cmath>
 #include <type_traits>
 #include <climits>
+#include <util/code_optimization.hpp>
 // #include <bitset>
 // #include <iostream>
 
@@ -122,7 +123,7 @@ static inline void flip_bit(T& x, unsigned int bit, _ENABLE_IF_UINT(T))
  */
 template <typename T>
 static inline T bit_mask(size_t n_bits, _ENABLE_IF_UINT(T)) {
-  return (T(1) << n_bits) - 1;
+    return (UNLIKELY(n_bits >= bitsizeof(T))) ? (~T(0)) : ((T(1) << n_bits) - 1);
 }
 
 /**
