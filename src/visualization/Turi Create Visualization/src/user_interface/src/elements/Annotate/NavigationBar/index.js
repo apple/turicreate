4a2a1cf9bd971f033f6c6c1ad77c4bbbe29cdfc2
Shreya Jain <shreyajain@Shreyas-MacBook-Pro-2.local>
2019-03-26 09:43:17 -0700

    added functionality

--- src/visualization/Turi Create Visualization/src/user_interface/src/elements/Annotate/NavigationBar/index.js
+++ src/visualization/Turi Create Visualization/src/user_interface/src/elements/Annotate/NavigationBar/index.js
@@ -4,27 +4,41 @@ import style from './index.module.css';
 
 class NavigationBar extends Component{
 
+constructor(props){
+  super(props);
+  this.state = {value:''};
+  this.handleChange = this.handleChange.bind(this);
+  this.enterPressJumpRow = this.enterPressJumpRow.bind(this);
+}
+
+handleChange(event){
+  this.setState({value: event.target.value});
+};
+
 enterPressJumpRow(e) {
     if(e.keyCode == 13){
-      if(this.imgNum != null){
-      	if (this.props.infiniteScroll)
+
+      if(this.state.value != null){
+      var image_number = this.state.value;
+      this.setState({value:''});
+        
+      if (this.props.infiniteScroll)
       	{
-      	this.props.toggleInfiniteScroll.bind(this);
+      	this.props.toggleInfiniteScroll(this);
         }
-      var image_number = this.imgNum.value;
-      this.imgNum.value = "";
-      this.props.getData(image_number, image_number+1)
-      this.props.getAnnotation(image_number, image_number+1)
+      this.props.updateIncrementalCurrentIndex(image_number-1);
+      this.props.getData(image_number-1, image_number+1);
+      this.props.getAnnotation(image_number-1, image_number+1);
+      }
     }
   }
 };
  
-	render(){
+render(){
 	return (
 		<div className="BottomBar">
 		<div className="jumpToImageContainer">
-		<input className="imageNumber" id="imgNum" onKeyDown={this.enterPressJumpRow.bind(this)} placeholder="Image #"/>
-		
+		<input className="imageNumber" type="text" value={this.state.value} onChange={this.handleChange} id="imgNum" onKeyDown={this.enterPressJumpRow} placeholder="Image #"/>
 		</div>
 		</div>
 		);
