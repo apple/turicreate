16c35404bc3e982d208a3f3888e61bbca0d102a1
Zach Nation <znation@apple.com>
2019-04-03 23:02:05 -0700

    Add Ubuntu 18.04 Dockerfile

--- scripts/create_docker_images.sh
+++ scripts/create_docker_images.sh
@@ -41,6 +41,7 @@ if [[ -n $PERSIST_TO_REPO_DIR ]]; then
   (test -f .docker_images/image-10.04.gz && docker load -i .docker_images/image-10.04.gz) || true
   (test -f .docker_images/image-12.04.gz && docker load -i .docker_images/image-12.04.gz) || true
   (test -f .docker_images/image-14.04.gz && docker load -i .docker_images/image-14.04.gz) || true
+  (test -f .docker_images/image-18.04.gz && docker load -i .docker_images/image-18.04.gz) || true
 fi
 
 (docker image ls turicreate/build-image-10.04:${TC_BUILD_IMAGE_VERSION} | grep turicreate/build-image) || \
@@ -52,6 +53,9 @@ cat scripts/Dockerfile-Ubuntu-12.04 | docker build -t turicreate/build-image-12.
 (docker image ls turicreate/build-image-14.04:${TC_BUILD_IMAGE_VERSION} | grep turicreate/build-image) || \
 cat scripts/Dockerfile-Ubuntu-14.04 | docker build -t turicreate/build-image-14.04:${TC_BUILD_IMAGE_VERSION} -
 
+(docker image ls turicreate/build-image-18.04:${TC_BUILD_IMAGE_VERSION} | grep turicreate/build-image) || \
+cat scripts/Dockerfile-Ubuntu-18.04 | docker build -t turicreate/build-image-18.04:${TC_BUILD_IMAGE_VERSION} -
+
 # Save images back to repo directory so they can be re-used the next time this script is run.
 # (This assumes the build may run on another machine, but with the same directory contents. This setup is
 #  primarily intended for CI systems that create a new VM for each run, but can cache the filesystem.)
@@ -59,5 +63,6 @@ if [[ -n $PERSIST_TO_REPO_DIR ]]; then
   docker save turicreate/build-image-10.04:${TC_BUILD_IMAGE_VERSION} | gzip -c > .docker_images/image-10.04.gz
   docker save turicreate/build-image-12.04:${TC_BUILD_IMAGE_VERSION} | gzip -c > .docker_images/image-12.04.gz
   docker save turicreate/build-image-14.04:${TC_BUILD_IMAGE_VERSION} | gzip -c > .docker_images/image-14.04.gz
+  docker save turicreate/build-image-18.04:${TC_BUILD_IMAGE_VERSION} | gzip -c > .docker_images/image-18.04.gz
 fi
 
