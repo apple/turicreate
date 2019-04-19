4fc93fb8873b90a1a57da499db4ca710e78f1478
Abhishek <abhishekpratapa@gmail.com>
2019-04-19 15:26:21 -0700

    added no_select CSS to Elements to fix weird selection issue (#1746)
    * added no_select CSS to Elements
    
    * Fixed Formatting

--- src/visualization/Turi Create Visualization/src/user_interface/build/service-worker.js
+++ src/visualization/Turi Create Visualization/src/user_interface/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.f589ba260e4f18b89922b6a0004159f8.js"
+  "./precache-manifest.be17d8f8837cb7462783ed752b20ba72.js"
 );
 
 workbox.clientsClaim();
