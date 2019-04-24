b5dfaf68faf8ac8c7b7f3300f24383aedb239946
Abhishek Pratapa <abhishekpratapa@gmail.com>
2019-04-24 15:08:44 -0700

    updated copyright header

--- src/visualization/Turi Create Visualization/src/CustomWebKitView.swift
+++ src/visualization/Turi Create Visualization/src/CustomWebKitView.swift
@@ -1,10 +1,7 @@
+//  Copyright © 2017 Apple Inc. All rights reserved.
 //
-//  CustomWebKitView.swift
-//  Turi Create Visualization
-//
-//  Created by Abhishek Pratapa on 4/24/19.
-//  Copyright © 2019 Apple. All rights reserved.
-//
+//  Use of this source code is governed by a BSD-3-clause license that can
+//  be found in the LICENSE.txt file or at https://opensource.org/licenses/BSD-3-Clause
 
 import Cocoa
 import WebKit
