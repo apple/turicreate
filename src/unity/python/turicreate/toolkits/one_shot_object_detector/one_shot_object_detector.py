9833b53ecc84a1f1cd3c59040f809a73080ac0f2
Shantanu Chhabra <shantanu_chhabra@apple.com>
2019-04-26 16:09:54 -0700

    Revamped the Python implementation of OneShotObjectDetector based on PR feedback

--- src/unity/python/turicreate/toolkits/one_shot_object_detector/one_shot_object_detector.py
+++ src/unity/python/turicreate/toolkits/one_shot_object_detector/one_shot_object_detector.py
@@ -8,41 +8,57 @@ import turicreate as _tc
 from turicreate import extensions as _extensions
 from turicreate.toolkits._model import CustomModel as _CustomModel
 
-def create(dataset, target, feature=None, batch_size=0, max_iterations=0,
-           seed=None, verbose=True):
+def create(dataset,
+           target,
+           feature=None,
+           batch_size=0,
+           max_iterations=0,
+           seed=None,
+           verbose=True):
     model = _extensions.one_shot_object_detector()
     if seed is None: seed = _random.randint(0, (1<<31)-1)
     # Option arguments to pass in to C++ Object Detector, if we use it:
     # {'mlmodel_path':'darknet.mlmodel', 'max_iterations' : 25}
     augmented_data = model.augment(dataset, target, _tc.SArray(), {"seed":seed})
-    model = _tc.object_detector.create(augmented_data)
-    return OneShotObjectDetector(model)
+    od_model = _tc.object_detector.create(augmented_data)
+    state = {'detector':od_model}
+    return OneShotObjectDetector(state)
+
 
 class OneShotObjectDetector(_CustomModel):
     _PYTHON_ONE_SHOT_OBJECT_DETECTOR_VERSION = 1
+    
+    def __init__(self, state):
+        # We use PythonProxy here so that we get tab completion
+        self.__proxy__ = PythonProxy(state)
 
-    def __init__(self, model):
-        self.__proxy__ = model
+    def predict(self, dataset):
+        return self.__proxy__['detector'].predict(dataset)
 
-    @classmethod
-    def _native_name(cls):
-        return "one_shot_object_detector"
+    def evaluate(self, dataset, metric="auto"):
+        return self.__proxy__['detector'].evaluate(dataset, metric)
 
     def _get_version(self):
         return self._PYTHON_ONE_SHOT_OBJECT_DETECTOR_VERSION
 
     @classmethod
+    def _native_name(cls):
+        return "one_shot_object_detector"
+
+    def _get_native_state(self):
+        # make sure to not accidentally modify the proxy object.
+        # take a copy of it.
+        state = self.__proxy__.get_state()
+
+        # We don't know how to serialize a Python class, hence we need to 
+        # reduce the detector to the proxy object before saving it.
+        state['detector'] = state['detector'].__proxy__
+        return state
+
+    @classmethod
     def _load_version(cls, state, version):
-        pass
-
-    def predict(self, dataset):
-        return self.__proxy__.predict(dataset)
-
-    def evaluate(self, dataset, metric="auto"):
-        return self.__proxy__.evaluate(dataset, metric)
-
-    def export_coreml(self, filename, verbose=False):
-        # TODO: The model exported out of here would look like an 
-        # Object Detector model. We must change the metadata to make it a 
-        # OneShotObjectDetector.
-        self.__proxy__.export_to_coreml(filename)
+        assert(version == _PYTHON_ONE_SHOT_OBJECT_DETECTOR_VERSION)
+        # we need to undo what we did at save and turn the proxy object
+        # back into a Python class
+        state['detector'] = ObjectDetector(state['detector'])
+        return OneShotObjectDetector(state)
