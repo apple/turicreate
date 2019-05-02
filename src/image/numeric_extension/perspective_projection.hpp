6842bd09c1d1c703ce904586e4eb95c5275a3fa3
Shantanu Chhabra <shantanu_chhabra@apple.com>
2019-05-01 20:19:18 -0700

    One Shot Object Detector Code Skeleton (#1790)

---
+++ src/image/numeric_extension/perspective_projection.hpp
@@ -0,0 +1,107 @@
+/* Copyright © 2019 Apple Inc. All rights reserved.
+ *
+ * Use of this source code is governed by a BSD-3-clause license that can
+ * be found in the LICENSE.txt file or at https://opensource.org/licenses/BSD-3-Clause
+ */
+#include <Eigen/Core>
+#include <boost/gil/gil_all.hpp>
+
+/* BEGIN 
+ * TODO: Add the necessary linear algebra to compute the transformation matrix
+ */
+
+namespace warp_perspective {
+
+Eigen::MatrixXf get_2D_to_3D(int width, int height) {
+  Eigen::MatrixXf A1(4,3);
+  A1 << 1, 0, 0, 
+        0, 1, 0, 
+        0, 0, 1, 
+        0, 0, 1;
+  return A1;
+}
+
+Eigen::MatrixXf get_rotation(float theta, float phi, float gamma) {
+  Eigen::Matrix4f Rx, Ry, Rz, R;
+  Rx << 1, 0, 0, 0,
+        0, 1, 0, 0,
+        0, 0, 1, 0,
+        0, 0, 0, 1;
+  Ry << 1, 0, 0, 0,
+        0, 1, 0, 0,
+        0, 0, 1, 0,
+        0, 0, 0, 1;
+  Rz << 1, 0, 0, 0,
+        0, 1, 0, 0,
+        0, 0, 1, 0,
+        0, 0, 0, 1;
+  R = (Rx * Ry) * Rz;
+  return R;
+}
+
+Eigen::MatrixXf get_translation(int dx, int dy, int dz) {
+  Eigen::Matrix4f T;
+  T << 1, 0, 0, 0,
+       0, 1, 0, 0,
+       0, 0, 1, 0,
+       0, 0, 0, 1;
+  return T;
+}
+
+Eigen::MatrixXf get_3D_to_2D(float focal, int width, int height) {
+  Eigen::MatrixXf A2(3,4);
+  A2 << 0, 0, 0, 0,
+        0, 0, 0, 0,
+        0, 0, 1, 0;
+  return A2;
+}
+
+Eigen::Matrix<float, 3, 3> get_transformation_matrix(
+  int width, int height, 
+  float theta, float phi, float gamma, 
+  int dx, int dy, int dz, 
+  float focal) {
+  Eigen::MatrixXf A1  = get_2D_to_3D(width, height);
+  Eigen::Matrix4f R   = get_rotation(theta, phi, gamma);
+  Eigen::Matrix4f T   = get_translation(dx, dy, dz);
+  Eigen::MatrixXf A2  = get_3D_to_2D(focal, width, height);
+  return (A2 * (T * (R * A1)));
+}
+
+/* END 
+ * TODO: Add the necessary linear algebra to compute the transformation matrix
+ */
+
+}
+
+namespace boost {
+namespace gil {
+
+/* Matrix - Vector multiplication */
+template <typename T, typename F> 
+boost::gil::point2<F> operator*(const boost::gil::point2<T>& p, const Eigen::Matrix<F, 3, 3>& m) {
+  float denominator = m(2,0)*p.x + m(2,1)*p.y + m(2,2);
+  if (denominator == 0) {
+    // TODO: Figure out the right failure behavior for when denominator is 0.
+    return boost::gil::point2<F>(0,0);
+  }
+  return boost::gil::point2<F>((m(0,0)*p.x + m(0,1)*p.y + m(0,2))/denominator, 
+                               (m(1,0)*p.x + m(1,1)*p.y + m(1,2))/denominator);
+}
+
+/* Conforming to the MapFn concept required by Boost GIL, for Eigen::Matrix3f
+ */
+template <typename T> struct mapping_traits;
+
+template <typename F, typename F2> 
+boost::gil::point2<F> transform(const Eigen::Matrix<F, 3, 3>& mat, const boost::gil::point2<F2>& src) {
+  return src * mat;
+}
+
+template <typename F>
+struct mapping_traits<Eigen::Matrix<F, 3, 3> >{
+    typedef boost::gil::point2<F> result_type;
+};
+
+} // gil
+} // boost
---
+++ src/unity/toolkits/object_detection/one_shot_object_detection/one_shot_object_detector.cpp
@@ -0,0 +1,169 @@
+/* Copyright © 2019 Apple Inc. All rights reserved.
+ *
+ * Use of this source code is governed by a BSD-3-clause license that can
+ * be found in the LICENSE.txt file or at https://opensource.org/licenses/BSD-3-Clause
+ */
+
+#include <algorithm>
+#include <cmath>
+#include <limits>
+#include <vector>
+#include <random>
+
+#include <boost/gil/gil_all.hpp>
+#include <boost/gil/extension/io/jpeg.hpp>
+#include <boost/gil/extension/numeric/sampler.hpp>
+#include <boost/gil/extension/numeric/resample.hpp>
+
+#include <Eigen/Core>
+#include <Eigen/Dense>
+
+#include <image/numeric_extension/perspective_projection.hpp>
+
+#include <unity/toolkits/object_detection/object_detector.hpp>
+#include <unity/toolkits/object_detection/one_shot_object_detection/one_shot_object_detector.hpp>
+
+namespace turi {
+namespace one_shot_object_detection {
+
+/* A ParameterSampler class to randomly generate different samples of parameters
+ * that can later be used to compute the transformation matrix necessary to 
+ * create image projections.
+ */
+class ParameterSampler {
+public:
+  ParameterSampler(size_t width, size_t height) {
+    width_ = width;
+    height_ = height;
+  }
+
+  double deg_to_rad(double angle) {
+    return angle * M_PI / 180.0;
+  }
+
+  /* Getters for all the parameters:
+   * theta: rotation around the x axis.
+   * phi: rotation around the y axis.
+   * gamma: rotation around the z axis.
+   * dz: distance of the object from the camera.
+   * focal: focal length of the camera used.
+   */
+  double get_theta() {
+    return deg_to_rad(theta_);
+  }
+
+  double get_phi() {
+    return deg_to_rad(phi_);
+  }
+
+  double get_gamma() {
+    return deg_to_rad(gamma_);
+  }
+
+  size_t get_dz() {
+    return dz_;
+  }
+
+  double get_focal() {
+    return focal_;
+  }
+
+  /* */
+  void sample(long seed) {
+    /* Barebones */
+    theta_ = 0.0;
+    phi_ = 0.0;
+    gamma_ = 0.0;
+    std::normal_distribution<double> focal_distribution((double)width_, focal_stdev_);
+    focal_generator_.seed(seed+6);
+    focal_ = focal_distribution(focal_generator_);
+    dz_ = focal_;
+  }
+
+private:
+  size_t width_;
+  size_t height_;
+  double focal_stdev_ = 40.0;
+  std::default_random_engine focal_generator_;
+  double theta_;
+  double phi_;
+  double gamma_;
+  size_t dz_;
+  double focal_;
+
+};
+
+namespace data_augmentation {
+
+gl_sframe augment_data(gl_sframe data,
+                       const std::string& target_column_name,
+                       gl_sarray backgrounds,
+                       long seed) {
+  // TODO: Get input image from the data sframe.
+  // TODO: Use backgrounds from the background SArray.
+  // TODO: Generalize 1024 and 676 to be the width and height of the image 
+  //       passed in.
+  ParameterSampler parameter_sampler = ParameterSampler(2*1024, 2*676);
+  // TODO: Take n as input.
+  size_t n = 1;
+  for (size_t i = 0; i < n; i++) {
+    parameter_sampler.sample(seed+i);
+
+    Eigen::Matrix<float, 3, 3> mat = warp_perspective::get_transformation_matrix(
+      2*1024,
+      2*676,
+      parameter_sampler.get_theta(),
+      parameter_sampler.get_phi(),
+      parameter_sampler.get_gamma(),
+      1024/2,
+      676/2,
+      parameter_sampler.get_dz(),
+      parameter_sampler.get_focal());
+
+    // TODO: Construct annotations that conform to the OD format, from the 
+    // four warped corner points.
+
+    boost::gil::rgb8_image_t starter_image, background;
+    // TODO: Don't hardcode this.
+    boost::gil::read_image("in-affine.jpg", starter_image, boost::gil::jpeg_tag());
+    // TODO: Don't hardcode this. Fetch this from the backgrounds SFrame in a 
+    //       loop.
+    boost::gil::read_image("background.jpg", background, boost::gil::jpeg_tag());
+
+    Eigen::Matrix<float, 3, 3> M = mat.inverse();
+    // TODO: Use a background.
+    // TODO: Use a mask during superposition on a random background.
+    boost::gil::rgb8_image_t transformed(boost::gil::rgb8_image_t::point_t(view(starter_image).dimensions()*2));
+    boost::gil::fill_pixels(view(transformed),boost::gil::rgb8_pixel_t(255, 255, 255));
+    boost::gil::resample_pixels(const_view(starter_image), view(transformed), M, boost::gil::bilinear_sampler());
+
+    // TODO: Write these images into an SFrame that this function can return.
+    std::string output_filename = "out-perspective-" + std::to_string(i) + ".jpg";
+    write_view(output_filename, view(transformed), boost::gil::jpeg_tag());
+  }
+  // TODO: Return the augmented data once the SFrame is written.
+  return data;
+}
+
+}
+
+one_shot_object_detector::one_shot_object_detector() {
+  model_.reset(new turi::object_detection::object_detector());
+}
+
+gl_sframe one_shot_object_detector::augment(gl_sframe data,
+                                            const std::string& target_column_name,
+                                            gl_sarray backgrounds,
+                                            std::map<std::string, flexible_type> options){
+  
+  gl_sframe augmented_data = data_augmentation::augment_data(data,
+                                                             target_column_name,
+                                                             backgrounds,
+                                                             options["seed"]);
+  // TODO: Call object_detector::train from here once we incorporate mxnet into
+  // the C++ Object Detector.
+  return augmented_data;
+}
+
+} // one_shot_object_detection
+} // turi
