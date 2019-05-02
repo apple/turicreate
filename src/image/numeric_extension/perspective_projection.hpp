f4e3db497d65b76deb1940398d33d81c0551e079
Shantanu Chhabra <shantanu_chhabra@apple.com>
2019-05-02 00:53:16 -0700

    Added Transformation Matrix for One Shot OD (#1826)

--- src/image/numeric_extension/perspective_projection.hpp
+++ src/image/numeric_extension/perspective_projection.hpp
@@ -14,45 +14,45 @@ namespace warp_perspective {
 
 Eigen::MatrixXf get_2D_to_3D(int width, int height) {
   Eigen::MatrixXf A1(4,3);
-  A1 << 1, 0, 0, 
-        0, 1, 0, 
-        0, 0, 1, 
-        0, 0, 1;
+  A1 << 1, 0,  -1*(float)width/2, 
+        0, 1,  -1*(float)height/2, 
+        0, 0,         1, 
+        0, 0,         1;
   return A1;
 }
 
 Eigen::MatrixXf get_rotation(float theta, float phi, float gamma) {
   Eigen::Matrix4f Rx, Ry, Rz, R;
-  Rx << 1, 0, 0, 0,
-        0, 1, 0, 0,
-        0, 0, 1, 0,
-        0, 0, 0, 1;
-  Ry << 1, 0, 0, 0,
-        0, 1, 0, 0,
-        0, 0, 1, 0,
-        0, 0, 0, 1;
-  Rz << 1, 0, 0, 0,
-        0, 1, 0, 0,
-        0, 0, 1, 0,
-        0, 0, 0, 1;
+  Rx << 1,               0,                0, 0,
+        0, std::cos(theta), -1*std::sin(theta), 0,
+        0, std::sin(theta),  std::cos(theta), 0,
+        0,               0,                0, 1;
+  Ry << std::cos(phi), 0, -1*std::sin(phi), 0,
+                    0, 1,              0, 0,
+        std::sin(phi), 0,  std::cos(phi), 0,
+                    0, 0,              0, 1;
+  Rz << std::cos(gamma), -1*std::sin(gamma), 0, 0,
+        std::sin(gamma),  std::cos(gamma), 0, 0,
+                      0,                0, 1, 0,
+                      0,                0, 0, 1;
   R = (Rx * Ry) * Rz;
   return R;
 }
 
 Eigen::MatrixXf get_translation(int dx, int dy, int dz) {
   Eigen::Matrix4f T;
-  T << 1, 0, 0, 0,
-       0, 1, 0, 0,
-       0, 0, 1, 0,
+  T << 1, 0, 0, dx,
+       0, 1, 0, dy,
+       0, 0, 1, dz,
        0, 0, 0, 1;
   return T;
 }
 
 Eigen::MatrixXf get_3D_to_2D(float focal, int width, int height) {
   Eigen::MatrixXf A2(3,4);
-  A2 << 0, 0, 0, 0,
-        0, 0, 0, 0,
-        0, 0, 1, 0;
+  A2 << focal,     0,  (float)width/2, 0,
+            0, focal, (float)height/2, 0,
+            0,     0,               1, 0;
   return A2;
 }
 
