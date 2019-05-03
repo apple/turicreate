45d60c120f117bce3c1ce71914f124fc881c8931
Zach Nation <znation@apple.com>
2019-05-02 18:23:18 -0700

    Fix CoreML export for tree classifiers
    Fixes #1831
    
    Reverts the changes in `xgboost_exporter.cpp` from #1127. That appeared
    to cause wrong predictions in CoreML for some exported classifiers.

--- src/unity/python/turicreate/test/test_coreml_export.py
+++ src/unity/python/turicreate/test/test_coreml_export.py
@@ -22,8 +22,12 @@ import coremltools
 import sys
 import platform
 import array
+import os
 
 import pytest
+
+dirname = os.path.dirname(__file__)
+mushroom_dataset = os.path.join(dirname, 'mushroom.csv')
 
 class CoreMLExportTest(unittest.TestCase):
 
@@ -97,7 +101,7 @@ class CoreMLExportTest(unittest.TestCase):
                 tc_prediction = model.predict(row)[0]
 
                 if (is_regression == False) and (type(model.classes[0]) == str):
-                    if not is_has_probability:
+                    if not has_probability:
                         self.assertEqual(coreml_prediction["target"], tc_prediction)
                 else:
                     self.assertAlmostEqual(coreml_prediction["target"], tc_prediction, delta = 1e-5)
@@ -359,7 +363,6 @@ class CoreMLExportTest(unittest.TestCase):
             self._test_coreml_export(model, test, False)
 
 
-    @pytest.mark.xfail()
     def test_random_forest_multiclass_simple_tiny(self):
         for code_string in ["nnnn", "bns", "sss"]:
             train, test = self.generate_data("multiclass", 8, code_string)
@@ -375,4 +378,14 @@ class CoreMLExportTest(unittest.TestCase):
             model = tc.random_forest_classifier.create(train, "target", validation_set = None, max_depth=3, max_iterations = 5)
             model.evaluate(test)  # Previous classifier -- this caused errors.  
             self._test_coreml_export(model, test, False)
+
+    def test_tree_export_issue_1831(self):
+        SEED = 42
+        data = tc.SFrame.read_csv(mushroom_dataset)
+        data['target'] = data['label']
+        train_data, test_data = data.random_split(0.8,seed=SEED)
+        model = tc.boosted_trees_classifier.create(train_data, target='target',
+                                                   max_iterations=2,
+                                                   max_depth = 3)
+        self._test_coreml_export(model, test_data, False)
 
