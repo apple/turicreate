6a9a7ac2a500cd2f58da814b87599b42e9a097ec
Zach Nation <znation@apple.com>
2019-05-14 16:03:50 -0700

    Remove storyboard from TCViz app (#1873)
    We've had some issues with the storyboard file since the existence of
    this project:
    
    * Difficult to merge changes
    * Unclear/conflicting sources of truth (code vs. storyboard)
    * Difficulty compiling on various platforms/versions of Xcode
    
    Given that we have a very simple UI (a light wrapper around WKWebView),
    there isn't really a need for a storyboard. The equivalent can be easily
    created in code, and will be easier to modify and maintain in the long run.

--- src/visualization/Turi Create Visualization/src/AppDelegate.swift
+++ src/visualization/Turi Create Visualization/src/AppDelegate.swift
@@ -4,49 +4,123 @@
 //  be found in the LICENSE.txt file or at https://opensource.org/licenses/BSD-3-Clause
 import Cocoa
 
-@NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
     
-    @IBOutlet weak var save_image_object: NSMenuItem!
-    @IBOutlet weak var save_vega_object: NSMenuItem!
-    @IBOutlet weak var print_vega_image: NSMenuItem!
-    @IBOutlet weak var page_setup_object: NSMenuItem!
+    var newWindow: NSWindow?
+    var controller: ViewController?
     
-    
-    @IBAction func print_vega(_ sender: Any) {
-        SharedData.shared.vegaContainer?.get_image {image in
-            let nsImage = NSImageView(image: image)
-            nsImage.sizeToFit()
-            nsImage.setFrameOrigin(NSPoint(x: 0.0, y: 0.0))
-            nsImage.setFrameSize(NSSize(width: image.size.width, height: image.size.height))
-            let print_info = NSPrintInfo()
-        
-            let print_image = NSPrintOperation(view: nsImage, printInfo: print_info)
-            print_image.canSpawnSeparateThread = true
-            print_image.printPanel.options.insert(NSPrintPanel.Options.showsOrientation)
-            print_image.printPanel.options.insert(NSPrintPanel.Options.showsScaling)
-            print_image.run()
+    @objc func print_vega(_ sender: Any) {
+        SharedData.shared.vegaContainer?.get_image {optionalImage in
+            switch optionalImage {
+            case .some(let image):
+                let nsImage = NSImageView(image: image)
+                nsImage.sizeToFit()
+                nsImage.setFrameOrigin(NSPoint(x: 0.0, y: 0.0))
+                nsImage.setFrameSize(NSSize(width: image.size.width, height: image.size.height))
+                let print_info = NSPrintInfo()
+                
+                let print_image = NSPrintOperation(view: nsImage, printInfo: print_info)
+                print_image.canSpawnSeparateThread = true
+                print_image.printPanel.options.insert(NSPrintPanel.Options.showsOrientation)
+                print_image.printPanel.options.insert(NSPrintPanel.Options.showsScaling)
+                print_image.run()
+                
+            case .none:
+                return
+            }
         }
     }
     
-    @IBAction func save_image(_ sender: Any) {
+    @objc func save_image(_ sender: Any) {
         SharedData.shared.vegaContainer?.save_image()
     }
     
-    @IBAction func save_vega(_ sender: Any) {
+    @objc func save_vega(_ sender: Any) {
         SharedData.shared.vegaContainer?.save_vega()
     }
     
-    @IBAction func save_data(_ sender: Any) {
+    func save_data(_ sender: Any) {
         SharedData.shared.vegaContainer?.save_data()
     }
     
     
     func applicationDidFinishLaunching(_ aNotification: Notification) {
+        let contentFrame = NSMakeRect(0, 0, 820, 630)
+        
+        newWindow = NSWindow(contentRect: contentFrame, styleMask: [.resizable, .titled, .closable, .miniaturizable], backing: .buffered, defer: false)
+        newWindow?.center()
+        newWindow?.minSize = NSMakeSize(600, 400)
+        
+        controller = ViewController()
+        let content = newWindow!.contentView! as NSView
+        let view = controller!.view
+        content.frame = contentFrame
+        view.frame = contentFrame
+        content.addSubview(view)
+        view.autoresizingMask = [.width, .height]
+        
+        let mainMenu = NSMenu()
+        let appMenuItem = mainMenu.addItem(withTitle: "Turi Create Visualization", action: nil, keyEquivalent: "")
+        let appMenu = NSMenu(title: "Turi Create Visualization")
+        appMenuItem.submenu = appMenu
+        
+        let save_image_object = NSMenuItem(title: "Export as PNG", action: #selector(self.save_image), keyEquivalent: "S")
+        save_image_object.keyEquivalentModifierMask = .command
+        
+        let save_vega_object = NSMenuItem(title: "Export as Vega JSON", action: #selector(self.save_vega), keyEquivalent: "S")
+        save_vega_object.keyEquivalentModifierMask = [.command, .shift]
+        
+        let print_vega_image = NSMenuItem(title: "Print...", action: #selector(self.print_vega), keyEquivalent: "P")
+        print_vega_image.keyEquivalentModifierMask = .command
+        
+        let page_setup_object = NSMenuItem(title: "Page Setup...", action: #selector(self.print_vega), keyEquivalent: "P")
+        page_setup_object.keyEquivalentModifierMask = [.command, .shift]
+        
+        let hideItem = appMenu.addItem(withTitle: "Hide Turi Create Visualization", action: #selector(NSApplication.shared.hide), keyEquivalent: "h")
+        hideItem.keyEquivalentModifierMask = .command
+        let hideOthersItem = appMenu.addItem(withTitle: "Hide Others", action: #selector(NSApplication.shared.hideOtherApplications), keyEquivalent: "h")
+        hideOthersItem.keyEquivalentModifierMask = [.command, .option]
+        
+        appMenu.addItem(.separator())
+        
+        let quitItem = appMenu.addItem(withTitle: "Quit Turi Create Visualization", action: #selector(NSApplication.shared.terminate), keyEquivalent: "q")
+        quitItem.keyEquivalentModifierMask = .command
+        
+        let fileMenu = NSMenu(title: "File")
+        let closeItem = fileMenu.addItem(withTitle: "Close", action: #selector(newWindow?.close), keyEquivalent: "w")
+        closeItem.keyEquivalentModifierMask = .command
+        fileMenu.addItem(save_image_object)
+        fileMenu.addItem(save_vega_object)
+        fileMenu.addItem(.separator())
+        fileMenu.addItem(page_setup_object)
+        fileMenu.addItem(print_vega_image)
+        
+        let fileMenuItem = mainMenu.addItem(withTitle: "File", action: nil, keyEquivalent: "")
+        fileMenuItem.submenu = fileMenu
+        
+        let viewMenu = NSMenu(title: "View")
+        let fullScreenItem = viewMenu.addItem(withTitle: "Toggle Full Screen", action: #selector(newWindow?.toggleFullScreen), keyEquivalent: "f")
+        fullScreenItem.keyEquivalentModifierMask = [.command, .control]
+        
+        let viewMenuItem = mainMenu.addItem(withTitle: "View", action: nil, keyEquivalent: "")
+        viewMenuItem.submenu = viewMenu
+        
+        let windowMenu = NSMenu(title: "Window")
+        let minimizeItem = windowMenu.addItem(withTitle: "Minimize", action: #selector(newWindow?.miniaturize), keyEquivalent: "m")
+        minimizeItem.keyEquivalentModifierMask = .command
+        windowMenu.addItem(withTitle: "Zoom", action: #selector(newWindow?.zoom), keyEquivalent: "")
+        
+        let windowMenuItem = mainMenu.addItem(withTitle: "Window", action: nil, keyEquivalent: "")
+        windowMenuItem.submenu = windowMenu
+        
         SharedData.shared.save_image = save_image_object
         SharedData.shared.save_vega = save_vega_object
         SharedData.shared.print_vega = print_vega_image
         SharedData.shared.page_setup = page_setup_object
+        
+        NSApplication.shared.mainMenu = mainMenu
+        
+        newWindow!.makeKeyAndOrderFront(nil)
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
