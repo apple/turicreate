bf38754b88e88cefbefce245436377a029f896f7
Krishna <srikrishna_sridhar@apple.com>
2019-05-29 23:40:38 -0700

    Fixed an issue with license text.

--- src/unity/python/turicreate/toolkits/one_shot_object_detector/one_shot_object_detector.py
+++ src/unity/python/turicreate/toolkits/one_shot_object_detector/one_shot_object_detector.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 # Copyright © 2019 Apple Inc. All rights reserved.
 #
-# be found in the LICENSE.txt file or at https://opensource.org/licenses/BSD-3-Clause
 # Use of this source code is governed by a BSD-3-clause license that can
-
+# be found in the LICENSE.txt file or at https://opensource.org/licenses/BSD-3-Clause
 #
+
 import turicreate as _tc
 from turicreate import extensions as _extensions
 from turicreate.toolkits._model import CustomModel as _CustomModel
