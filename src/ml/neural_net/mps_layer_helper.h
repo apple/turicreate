06f011f21a1a2162218349f593da7032dc569745
Abhishek Pratapa <abhishekpratapa@gmail.com>
2019-06-27 14:22:44 -0700

    Removed Fully Connected Data Loader and Convolutional Data Loader

--- src/ml/neural_net/mps_layer_helper.h
+++ src/ml/neural_net/mps_layer_helper.h
@@ -8,33 +8,33 @@ NS_ASSUME_NONNULL_BEGIN
 API_AVAILABLE(macos(10.14))
 @interface TCMPSLayerHelper: NSObject
 
-+ (MPSCNNFullyConnectedNode * _Nullable) createFullyConnected:(MPSNNImageNode *)inputNode
-                                        inputFeatureChannels:(NSNumber *)inputFeatureChannels
-                                       outputFeatureChannels:(NSNumber *)outputFeatureChannels
-                                                 inputHeight:(NSNumber *)inputHeight
-                                                  inputWidth:(NSNumber *)inputWidth
-                                                     weights:(NSArray *)weights
-                                                      biases:(NSArray *)biases
-                                                       label:(NSString *)label
-                                               updateWeights:(bool)updateWeights
-                                                      device:(id<MTLDevice>)dev
-                                                   cmd_queue:(id<MTLCommandQueue>) cmd_q;
++ (MPSCNNFullyConnectedNode *) createFullyConnected:(MPSNNImageNode *)inputNode
+                               inputFeatureChannels:(NSUInteger)inputFeatureChannels
+                              outputFeatureChannels:(NSUInteger)outputFeatureChannels
+                                        inputHeight:(NSUInteger)inputHeight
+                                         inputWidth:(NSUInteger)inputWidth
+                                            weights:(float *)weights
+                                             biases:(float *)biases
+                                              label:(NSString *)label
+                                      updateWeights:(bool)updateWeights
+                                             device:(id<MTLDevice>)dev
+                                          cmd_queue:(id<MTLCommandQueue>) cmd_q;
 
-+ (MPSCNNConvolutionNode *_Nullable) createConvolutional:(MPSNNImageNode *)inputNode
-                                            kernelWidth:(NSNumber *)kernelWidth
-                                           kernelHeight:(NSNumber *)kernelHeight
-                                   inputFeatureChannels:(NSNumber *)inputFeatureChannels
-                                  outputFeatureChannels:(NSNumber *)outputFeatureChannels
-                                            strideWidth:(NSNumber *)strideWidth
-                                           strideHeight:(NSNumber *)strideHeight
-                                           paddingWidth:(NSNumber *)paddingWidth
-                                          paddingHeight:(NSNumber *)paddingHeight
-                                                weights:(NSArray *)weights
-                                                 biases:(NSArray *)biases
-                                                  label:(NSString *)label
-                                          updateWeights:(bool)updateWeights
-                                                 device:(id<MTLDevice>)dev
-                                              cmd_queue:(id<MTLCommandQueue>) cmd_q;
++ (MPSCNNConvolutionNode *) createConvolutional:(MPSNNImageNode *)inputNode
+                                    kernelWidth:(NSUInteger)kernelWidth
+                                   kernelHeight:(NSUInteger)kernelHeight
+                           inputFeatureChannels:(NSUInteger)inputFeatureChannels
+                          outputFeatureChannels:(NSUInteger)outputFeatureChannels
+                                    strideWidth:(NSUInteger)strideWidth
+                                   strideHeight:(NSUInteger)strideHeight
+                                   paddingWidth:(NSUInteger)paddingWidth
+                                  paddingHeight:(NSUInteger)paddingHeight
+                                        weights:(float *)weights
+                                         biases:(float *)biases
+                                          label:(NSString *)label
+                                  updateWeights:(bool)updateWeights
+                                         device:(id<MTLDevice>)dev
+                                      cmd_queue:(id<MTLCommandQueue>) cmd_q;
 
 @end
 
