88443d5b211c64f32f9fbe701edaf2a005c13252
Shreya Jain <shreyajain@Shreyas-MacBook-Pro-2.local>
2019-06-30 23:36:47 -0700

    made session_id_column_name to session_id in function call

--- src/toolkits/activity_classification/class_registrations.cpp
+++ src/toolkits/activity_classification/class_registrations.cpp
@@ -17,14 +17,14 @@ namespace activity_classification {
 BEGIN_FUNCTION_REGISTRATION
 REGISTER_FUNCTION(_activity_classifier_prepare_data , "data" , "features" , "session_id" , "prediction_window" , "predictions_in_chunk" , "target")
 REGISTER_FUNCTION(_activity_classifier_prepare_data_verbose , "data" , "features" , "session_id" , "prediction_window" , "predictions_in_chunk" , "target")
-REGISTER_NAMED_FUNCTION("_activity_classifier_random_split_by_session", activity_classifier::random_split_by_session, "data","session_id_column_name", "fraction", "seed");
+REGISTER_NAMED_FUNCTION("_activity_classifier_random_split_by_session", activity_classifier::random_split_by_session, "data","session_id", "fraction", "seed");
 REGISTER_DOCSTRING(
     activity_classifier::random_split_by_session,
     "----------\n"
     "data : SFrame\n"
     "    Dataset of new observations. Must include columns with the same\n"
     "    names as the features used for model training.\n"
-    "session_id_column_name : string\n"
+    "session_id : string\n"
     "    Name of the column that contains a unique ID for each session.\n"
     "fraction : float, optional\n"
     "   The dataset is randomly split into two datasets where one contains\n"
