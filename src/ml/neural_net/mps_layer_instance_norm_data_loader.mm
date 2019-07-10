bc43ded8521d36ce3781664bcbf24c5d597392b1
Abhishek Pratapa <abhishekpratapa@gmail.com>
2019-07-10 13:48:26 -0700

    Class wrapper for properties

--- src/ml/neural_net/mps_layer_instance_norm_data_loader.mm
+++ src/ml/neural_net/mps_layer_instance_norm_data_loader.mm
@@ -1,6 +1,28 @@
 #include <ml/neural_net/mps_layer_instance_norm_data_loader.h>
 #include <ml/neural_net/mps_weight.h>
 
+API_AVAILABLE(macos(10.14))
+@interface TCMPSInstanceNormDataLoaderProps : NSObject
+  @property (nonatomic) MPSVector *gammaVector;
+  @property (nonatomic) MPSVector *betaVector;
+  @property (nonatomic) MPSVector *gammaMomentumVector;
+  @property (nonatomic) MPSVector *gammaVelocityVector;
+  @property (nonatomic) MPSVector *betaMomentumVector;
+  @property (nonatomic) MPSVector *betaVelocityVector;
+
+  @property (nonatomic) id<MTLBuffer> movingMeanBuffer;
+  @property (nonatomic) id<MTLBuffer> movingVarianceBuffer;
+  @property (nonatomic) id<MTLBuffer> gammaMomentumBuffer;
+  @property (nonatomic) id<MTLBuffer> gammaVelocityBuffer;
+  @property (nonatomic) id<MTLBuffer> betaMomentumBuffer;
+  @property (nonatomic) id<MTLBuffer> betaVelocityBuffer;
+
+  @property (nonatomic) MPSCNNNormalizationGammaAndBetaState *state;
+@end
+
+@implementation TCMPSInstanceNormDataLoaderProps 
+@end
+
 @interface TCMPSInstanceNormDataLoader () {
   NSMutableData *_gamma_weights;
   NSMutableData *_beta_weights;
@@ -8,28 +30,14 @@
   NSString *_name;
   NSUInteger _styles;
 
-  NSMutableArray<MPSVector *> *_gammaVectorArray;
-  NSMutableArray<MPSVector *> *_betaVectorArray;
-  NSMutableArray<MPSVector *> *_gammaMomentumVectorArray;
-  NSMutableArray<MPSVector *> *_gammaVelocityVectorArray;
-  NSMutableArray<MPSVector *> *_betaMomentumVectorArray;
-  NSMutableArray<MPSVector *> *_betaVelocityVectorArray;
-
-  NSMutableArray<id<MTLBuffer>> *_movingMeanBufferArray;
-  NSMutableArray<id<MTLBuffer>> *_movingVarianceBufferArray;
-  NSMutableArray<id<MTLBuffer>> *_gammaMomentumBufferArray;
-  NSMutableArray<id<MTLBuffer>> *_gammaVelocityBufferArray;
-  NSMutableArray<id<MTLBuffer>> *_betaMomentumBufferArray;
-  NSMutableArray<id<MTLBuffer>> *_betaVelocityBufferArray;
+  NSMutableArray<TCMPSInstanceNormDataLoaderProps *> *style_props;
 
   MPSVectorDescriptor *_vDesc;
-  NSMutableArray<MPSCNNNormalizationGammaAndBetaState *> *stateArray;
 
   id<MTLCommandQueue> _cq;
   MPSNNOptimizerAdam *_adamGamma;
   MPSNNOptimizerAdam *_adamBeta;
 }
-
 @end
 
 @implementation TCMPSInstanceNormDataLoader
@@ -62,9 +70,7 @@
     float *zeros_ptr = (float*) calloc(_numberOfFeatureChannels, sizeof(float));
     float *ones_ptr = (float*) malloc(_numberOfFeatureChannels * sizeof(float));
 
-    for(size_t x = 0; x < _numberOfFeatureChannels; x ++){
-      ones_ptr[x] = 1.0f;
-    }
+    for(NSUInteger x = 0; x < _numberOfFeatureChannels; x ++) ones_ptr[x] = 1.0f;
 
     _adamGamma = [[MPSNNOptimizerAdam alloc] initWithDevice:dev
                                               learningRate:0.001f];
@@ -75,21 +81,11 @@
     _vDesc = [MPSVectorDescriptor vectorDescriptorWithLength:_numberOfFeatureChannels
                                                    dataType:(MPSDataTypeFloat32)];
 
-    _gammaVectorArray = [[NSMutableArray alloc] init];
-    _betaVectorArray = [[NSMutableArray alloc] init];
-    _gammaMomentumVectorArray = [[NSMutableArray alloc] init];
-    _gammaVelocityVectorArray = [[NSMutableArray alloc] init];
-    _betaMomentumVectorArray = [[NSMutableArray alloc] init];
-    _betaVelocityVectorArray = [[NSMutableArray alloc] init];
-
-    _movingMeanBufferArray = [[NSMutableArray alloc] init];
-    _movingVarianceBufferArray = [[NSMutableArray alloc] init];
-    _gammaMomentumBufferArray = [[NSMutableArray alloc] init];
-    _gammaVelocityBufferArray = [[NSMutableArray alloc] init];
-    _betaMomentumBufferArray = [[NSMutableArray alloc] init];
-    _betaVelocityBufferArray = [[NSMutableArray alloc] init];
+    style_props = [[NSMutableArray alloc] init];
 
     for (NSUInteger index = 0; index < styles; index ++) {
+      TCMPSInstanceNormDataLoaderProps *style_property = [[TCMPSInstanceNormDataLoaderProps alloc] init];
+
       id<MTLBuffer> gammaBuffer = [dev newBufferWithBytes:_gamma_weights.mutableBytes
                                                    length:sizeof(float) * _numberOfFeatureChannels
                                                   options:MTLResourceStorageModeManaged];
@@ -98,76 +94,52 @@
                                                   length:sizeof(float) * _numberOfFeatureChannels
                                                  options:MTLResourceStorageModeManaged];
 
-      id<MTLBuffer> gammaMomentumBuffer = [dev newBufferWithBytes:zeros_ptr
-                                                           length:sizeof(float) * _numberOfFeatureChannels
-                                                          options:MTLResourceStorageModeManaged];
-
-      [_gammaMomentumBufferArray addObject:gammaMomentumBuffer];
-
-      id<MTLBuffer> gammaVelocityBuffer = [dev newBufferWithBytes:zeros_ptr
-                                                           length:sizeof(float) * _numberOfFeatureChannels
-                                                          options:MTLResourceStorageModeManaged];
-
-      [_gammaVelocityBufferArray addObject:gammaVelocityBuffer];
-
-      id<MTLBuffer> betaMomentumBuffer = [dev newBufferWithBytes:zeros_ptr
-                                                          length:sizeof(float) * _numberOfFeatureChannels
-                                                         options:MTLResourceStorageModeManaged];
-
-      [_betaMomentumBufferArray addObject:betaMomentumBuffer];
-
-      id<MTLBuffer> betaVelocityBuffer = [dev newBufferWithBytes:zeros_ptr
-                                                          length:sizeof(float) * _numberOfFeatureChannels
-                                                         options:MTLResourceStorageModeManaged];
-
-      [_betaVelocityBufferArray addObject:betaVelocityBuffer];
-
-      id<MTLBuffer> movingMeanBuffer = [dev newBufferWithBytes:zeros_ptr
-                                                        length:sizeof(float) * _numberOfFeatureChannels
-                                                       options:MTLResourceStorageModeManaged];
-
-      [_movingMeanBufferArray addObject:movingMeanBuffer];
-
-      id<MTLBuffer> movingVarianceBuffer = [dev newBufferWithBytes:ones_ptr
+      style_property.gammaMomentumBuffer = [dev newBufferWithBytes:zeros_ptr
                                                             length:sizeof(float) * _numberOfFeatureChannels
                                                            options:MTLResourceStorageModeManaged];
 
-      [_movingVarianceBufferArray addObject:movingVarianceBuffer];
+      style_property.gammaVelocityBuffer = [dev newBufferWithBytes:zeros_ptr
+                                                            length:sizeof(float) * _numberOfFeatureChannels
+                                                           options:MTLResourceStorageModeManaged];
 
-      MPSVector *gammaVector = [[MPSVector alloc] initWithBuffer:gammaBuffer
-                                                      descriptor:_vDesc];
+      style_property.betaMomentumBuffer = [dev newBufferWithBytes:zeros_ptr
+                                                           length:sizeof(float) * _numberOfFeatureChannels
+                                                          options:MTLResourceStorageModeManaged];
 
-      [_gammaVectorArray addObject:gammaVector];
+      style_property.betaVelocityBuffer = [dev newBufferWithBytes:zeros_ptr
+                                                           length:sizeof(float) * _numberOfFeatureChannels
+                                                          options:MTLResourceStorageModeManaged];
 
-      MPSVector *gammaMomentumVector = [[MPSVector alloc] initWithBuffer:gammaMomentumBuffer
-                                                              descriptor:_vDesc];
+      style_property.movingMeanBuffer = [dev newBufferWithBytes:zeros_ptr
+                                                         length:sizeof(float) * _numberOfFeatureChannels
+                                                        options:MTLResourceStorageModeManaged];
 
-      [_gammaMomentumVectorArray addObject:gammaMomentumVector];
+      style_property.movingVarianceBuffer = [dev newBufferWithBytes:ones_ptr
+                                                             length:sizeof(float) * _numberOfFeatureChannels
+                                                            options:MTLResourceStorageModeManaged];
 
-      MPSVector *gammaVelocityVector = [[MPSVector alloc] initWithBuffer:gammaVelocityBuffer
-                                                              descriptor:_vDesc];
+      style_property.gammaVector = [[MPSVector alloc] initWithBuffer:gammaBuffer
+                                                          descriptor:_vDesc];
 
-      [_gammaVelocityVectorArray addObject:gammaVelocityVector];
+      style_property.gammaMomentumVector = [[MPSVector alloc] initWithBuffer:style_property.gammaMomentumBuffer
+                                                                  descriptor:_vDesc];
 
-      MPSVector *betaVector = [[MPSVector alloc] initWithBuffer:betaBuffer
-                                                     descriptor:_vDesc];
+      style_property.gammaVelocityVector = [[MPSVector alloc] initWithBuffer:style_property.gammaVelocityBuffer
+                                                                  descriptor:_vDesc];
 
-      [_betaVectorArray addObject:betaVector];
+      style_property.betaVector = [[MPSVector alloc] initWithBuffer:betaBuffer
+                                                         descriptor:_vDesc];
 
-      MPSVector *betaMomentumVector = [[MPSVector alloc] initWithBuffer:betaMomentumBuffer
-                                                             descriptor:_vDesc];
+      style_property.betaMomentumVector = [[MPSVector alloc] initWithBuffer:style_property.betaMomentumBuffer
+                                                                 descriptor:_vDesc];
 
-      [_betaMomentumVectorArray addObject:betaMomentumVector];
+      style_property.betaVelocityVector = [[MPSVector alloc] initWithBuffer:style_property.betaVelocityBuffer
+                                                                 descriptor:_vDesc];
 
-      MPSVector *betaVelocityVector = [[MPSVector alloc] initWithBuffer:betaVelocityBuffer
-                                                             descriptor:_vDesc];
-      [_betaVelocityVectorArray addObject:betaVelocityVector];
+      style_property.state = [[MPSCNNNormalizationGammaAndBetaState alloc] initWithGamma:gammaBuffer
+                                                                                    beta:betaBuffer];
 
-      MPSCNNNormalizationGammaAndBetaState* state = [[MPSCNNNormalizationGammaAndBetaState alloc] 
-                                                          initWithGamma:gammaBuffer
-                                                                   beta:betaBuffer];
-
-      [stateArray addObject:state];
+      [style_props addObject:style_property];
     }
     
     free(zeros_ptr);
@@ -186,7 +158,7 @@
 
 - (float *) beta {
   [self checkpointWithCommandQueue:_cq];
-  return (float *) [[[_betaVectorArray objectAtIndex: _currentStyle] data] contents];
+  return (float *) [[[[style_props objectAtIndex: _currentStyle] betaVector] data] contents];
 }
 
 - (void) loadGamma:(float *)gamma {
@@ -195,7 +167,7 @@
 
 - (float *) gamma {
   [self checkpointWithCommandQueue:_cq];
-  return (float*) [[[_gammaVectorArray objectAtIndex: _currentStyle] data] contents];
+  return (float*) [[[[style_props objectAtIndex: _currentStyle] gammaVector] data] contents];
 }
 
 - (MPSCNNNormalizationGammaAndBetaState *)updateGammaAndBetaWithCommandBuffer:(id<MTLCommandBuffer>)commandBuffer 
@@ -214,9 +186,9 @@
       [_adamGamma encodeToCommandBuffer:commandBuffer
                     inputGradientVector:gradientWeightsVector
                       inputValuesVector:inputWeightsVector
-                    inputMomentumVector:_gammaMomentumVectorArray[_currentStyle]
-                    inputVelocityVector:_gammaVelocityVectorArray[_currentStyle]
-                     resultValuesVector:_gammaVectorArray[_currentStyle]];
+                    inputMomentumVector:[[style_props objectAtIndex: _currentStyle] gammaMomentumVector]
+                    inputVelocityVector:[[style_props objectAtIndex: _currentStyle] gammaVelocityVector]
+                     resultValuesVector:[[style_props objectAtIndex: _currentStyle] gammaVector]];
 
       MPSVector *gradientBiasesVector = [[MPSVector alloc] initWithBuffer:nonnull_cast(instanceNormalizationState.gradientForBeta)
                                                                descriptor:_vDesc];
@@ -227,30 +199,30 @@
       [_adamBeta encodeToCommandBuffer:commandBuffer
                    inputGradientVector:gradientBiasesVector
                      inputValuesVector:inputBiasesVector
-                   inputMomentumVector:_betaMomentumVectorArray[_currentStyle]
-                   inputVelocityVector:_betaVelocityVectorArray[_currentStyle]
-                    resultValuesVector:_betaVectorArray[_currentStyle]];
+                   inputMomentumVector:[[style_props objectAtIndex: _currentStyle] betaMomentumVector] 
+                   inputVelocityVector:[[style_props objectAtIndex: _currentStyle] betaVelocityVector]
+                    resultValuesVector:[[style_props objectAtIndex: _currentStyle] betaVector]];
 
   }
 
-    return stateArray[_currentStyle];
+    return [[style_props objectAtIndex: _currentStyle] state];
 }
 
 - (void)checkpointWithCommandQueue:(nonnull id<MTLCommandQueue>)commandQueue {
   id<MTLCommandBuffer> commandBuffer = [commandQueue commandBuffer];
   id<MTLBlitCommandEncoder> blit = commandBuffer.blitCommandEncoder;
 
-  for (size_t index = 0; index < _styles; index ++){
-    [blit synchronizeResource:_betaMomentumBufferArray[index]];
-    [blit synchronizeResource:_betaVelocityBufferArray[index]];
-    [blit synchronizeResource:nonnull_cast(stateArray[index].beta)];
+  for (size_t index = 0; index < _styles; index ++) {
+    [blit synchronizeResource:[[style_props objectAtIndex: index] betaMomentumBuffer]];
+    [blit synchronizeResource:[[style_props objectAtIndex: index] betaVelocityBuffer]];
+    [blit synchronizeResource:nonnull_cast([[style_props objectAtIndex: index] state].beta)];
 
-    [blit synchronizeResource:_gammaMomentumBufferArray[index]];
-    [blit synchronizeResource:_gammaVelocityBufferArray[index]];
-    [blit synchronizeResource:nonnull_cast(stateArray[index].gamma)];
+    [blit synchronizeResource:[[style_props objectAtIndex: index] gammaMomentumBuffer]];
+    [blit synchronizeResource:[[style_props objectAtIndex: index] gammaVelocityBuffer]];
+    [blit synchronizeResource:nonnull_cast([[style_props objectAtIndex: index] state].gamma)];
 
-    [blit synchronizeResource:_movingMeanBufferArray[index]];
-    [blit synchronizeResource:_movingVarianceBufferArray[index]];
+    [blit synchronizeResource:[[style_props objectAtIndex: index] movingMeanBuffer]];
+    [blit synchronizeResource:[[style_props objectAtIndex: index] movingVarianceBuffer]];
   }
 
   [blit endEncoding];
