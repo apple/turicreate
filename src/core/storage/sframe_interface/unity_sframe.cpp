a4197f64af970486c15bfd4b1cd5256010c4af3e
Hoyt Koepke <hoytak@graphlab.com>
2019-07-16 13:37:26 -0700

    Quick fix to get compilation to work on linux with GCC 7.4. (#2130)
    * Quick fix to get compilation to allow things to build on linux with gcc 7.4.
    
    * Addressed PR feedback.

--- src/core/storage/sframe_interface/unity_sframe.cpp
+++ src/core/storage/sframe_interface/unity_sframe.cpp
@@ -2014,6 +2014,11 @@ void unity_sframe::explore(const std::string& path_to_client, const std::string&
       enum MethodType {GetRows = 0, GetAccordion = 1};
       auto response = boost::optional<MethodType>();
 
+      // It appears that GCC falsely assumes the payload part of the response 
+      // variable may be uninitialized; this fixes that.
+#     pragma GCC diagnostic push
+#     pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
+      
       auto sa = gl_sarray(std::vector<flexible_type>(1, input)).astype(flex_type_enum::DICT);
       flex_dict dict = sa[0].get<flex_dict>();
       for (const auto& pair : dict) {
@@ -2045,6 +2050,7 @@ void unity_sframe::explore(const std::string& path_to_client, const std::string&
           std::runtime_error, "Unsupported case (should be either GetRows or GetAccordion).");
         ASSERT_UNREACHABLE();
       }
+#     pragma GCC diagnostic pop
     }
   });
 }
