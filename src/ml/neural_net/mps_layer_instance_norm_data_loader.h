df8ccff7650cd2861751a6ca7ab3c7f77d99158d
Abhishek Pratapa <abhishekpratapa@gmail.com>
2019-07-17 10:25:45 -0700

    Updated copyright headers

--- src/ml/neural_net/mps_layer_instance_norm_data_loader.h
+++ src/ml/neural_net/mps_layer_instance_norm_data_loader.h
@@ -1,3 +1,9 @@
+/* Copyright © 2018 Apple Inc. All rights reserved.
+ *
+ * Use of this source code is governed by a BSD-3-clause license that can
+ * be found in the LICENSE.txt file or at https://opensource.org/licenses/BSD-3-Clause
+ */
+
 #import <Accelerate/Accelerate.h>
 #import <Foundation/Foundation.h>
 #import <Metal/Metal.h>
