f533467aea3da62b2b1db6a85331cd3a7be709ab
Shreya Jain <shreyajain@Shreyas-MacBook-Pro-2.local>
2019-07-25 01:15:01 -0700

    build works with pybind

--- src/ml/neural_net/mxnet_compute_context.cpp
+++ src/ml/neural_net/mxnet_compute_context.cpp
@@ -4,135 +4,71 @@
  * be found in the LICENSE.txt file or at https://opensource.org/licenses/BSD-3-Clause
 */
 
-// #include <ml/neural_net/mps_compute_context.hpp>
 
-// #import <ml/neural_net/mps_device_manager.h>
-
-// #include <core/logging/logger.hpp>
-// #include <ml/neural_net/mps_cnnmodule.h>
-// #include <ml/neural_net/mps_graph_cnnmodule.h>
-// #include <ml/neural_net/mps_image_augmentation.hpp>
 #include <include/pybind11/pybind11.h>
+#include <include/pybind11/embed.h>
 #include <iostream>
+#include "mxnet_compute_context.hpp"
+#include <core/export.hpp>
+#include <model_server/lib/toolkit_function_macros.hpp>
+#include <vector>
+#include <include/pybind11/stl.h>
+#include <include/pybind11/numpy.h>
 
-// #include <core/logging/logger.hpp>
-
-
-// namespace turi {
-// namespace neural_net {
-
-// namespace {
-
-// std::unique_ptr<compute_context> create_mxnet_compute_context() {
-//   return std::unique_ptr<compute_context>(new mxnet_compute_context);
-// }
-
-// // At static-init time, register create_mps_compute_context().
-// // TODO: Codify priority levels?
-// static auto* mxnet_registration = new compute_context::registration(
-//     /* priority */ 1, &create_mxnet_compute_context);
-
-// }  // namespace
-
-// mxnet_compute_context::mxnet_compute_context(
-//     std::unique_ptr<mps_command_queue> command_queue)
-//   : command_queue_(std::move(command_queue))
-// {}
-
-// mps_compute_context::mps_compute_context()
-//   : command_queue_(new mps_command_queue)
-// {
-//   @autoreleasepool {
-
-//   id <MTLDevice> dev = [[TCMPSDeviceManager sharedInstance] preferredDevice];
-//   if (dev == nil) {
-//     log_and_throw("No valid Metal device found.");
-//   }
-
-//   command_queue_->impl = [dev newCommandQueue];
-
-//   }  // @autoreleasepool
-// }
-
-// mps_compute_context::~mps_compute_context() = default;
-
-// size_t mps_compute_context::memory_budget() const {
-//   @autoreleasepool {
-
-//   id <MTLDevice> dev = command_queue_->impl.device;
-//   return static_cast<size_t>(dev.recommendedMaxWorkingSetSize);
-
-//   }  // @autoreleasepool
-// }
-
-// std::vector<std::string> mps_compute_context::gpu_names() const {
-//   @autoreleasepool {
-
-//   id <MTLDevice> dev = command_queue_->impl.device;
-//   return { [dev.name cStringUsingEncoding:NSUTF8StringEncoding] };
-
-//   }  // @autoreleasepool
-// }
-
-// std::unique_ptr<image_augmenter> mps_compute_context::create_image_augmenter(
-//     const image_augmenter::options& opts) {
-
-//   return std::unique_ptr<image_augmenter>(new mps_image_augmenter(opts));
-// }
-
-// std::unique_ptr<image_augmenter>
-// mps_compute_context::create_image_augmenter_for_testing(
-//     const image_augmenter::options& opts,
-//     std::function<float(float lower, float upper)> rng) {
-
-//   return std::unique_ptr<image_augmenter>(new mps_image_augmenter(opts, rng));
-// }
-
-// std::unique_ptr<model_backend> mps_compute_context::create_object_detector(
-//     int n, int c_in, int h_in, int w_in, int c_out, int h_out, int w_out,
-//     const float_array_map& config, const float_array_map& weights) {
-
-//   std::unique_ptr<mps_graph_cnn_module> result(
-//       new mps_graph_cnn_module(*command_queue_));
-
-//   result->init(/* network_id */ kODGraphNet, n, c_in, h_in, w_in, c_out, h_out,
-//                w_out, config, weights);
-
-//   return result;
-// }
-
-// std::unique_ptr<model_backend> mps_compute_context::create_activity_classifier(
-//     int n, int c_in, int h_in, int w_in, int c_out, int h_out, int w_out,
-//     const float_array_map& config, const float_array_map& weights) {
-
-//   std::unique_ptr<mps_cnn_module> result(new mps_cnn_module(*command_queue_));
-
-//   result->init(/* network_id */ kActivityClassifierNet, n, c_in, h_in, w_in,
-//                c_out, h_out, w_out, /* updater_id */ 2 /* Adam */,  config);
-//   result->load(weights);
-
-//   return result;
-// }
-
-// }  // namespace neural_net
-// }  // namespace turi
 
 namespace py = pybind11;
 
-bool try_the_code() {
-	py::print(py::str("HELLOWORLD"));
-	auto math = py::module::import("math");
-    double root_two = math.attr("sqrt")(2.0).cast<double>();
+py::array_t<double> add_arrays(py::array_t<double> input1, py::array_t<double> input2) {
+    py::buffer_info buf1 = input1.request(), buf2 = input2.request();
 
-    std::cout << "The square root of 2 is: " << root_two << "\n";
-	return true;
+    if (buf1.ndim != 1 || buf2.ndim != 1)
+        throw std::runtime_error("Number of dimensions must be one");
+
+    if (buf1.size != buf2.size)
+        throw std::runtime_error("Input shapes must match");
+
+    /* No pointer is passed, so NumPy will allocate the buffer */
+    auto result = py::array_t<double>(buf1.size);
+
+    py::buffer_info buf3 = result.request();
+
+    double *ptr1 = (double *) buf1.ptr,
+           *ptr2 = (double *) buf2.ptr,
+           *ptr3 = (double *) buf3.ptr;
+
+    for (size_t idx = 0; idx < 3; idx++)
+        ptr3[idx] = ptr1[idx] + ptr2[idx];
+
+    return result;
 }
 
-PYBIND11_MODULE(mxnet_compute_context, m){
-    m.doc() = "pybind11 example plugin"; // optional module docstring
-
-    m.def("try_the_code", &try_the_code, "A function which does stuff");
+int import_modules() {
+	
+	py::module calc = py::module::import("py_mod.calc");
+	py::object result = calc.attr("add")(1, 2);
+	int n = result.cast<int>();
+	
+	return n;
 }
+
+// int import_obj(){
+// 	std::cout << "hey";
+// 	py::module calc = py::module::import("py_mod.calc");
+// 	py::object res = calc.attr("tran")();
+// 	int n = res.cast<int>();
+// 	return n;
+
+// }
+
+PYBIND11_MODULE(libtcmxnet, m) {
+	
+    m.doc() = "pybind11 example module";
+    m.def("add_arrays", &add_arrays, "Add two NumPy arrays");
+    m.def("import_modules", &import_modules, "Calc");
+    // m.def("import_obj", &import_obj, "ghjjk");
+}
+
+
 
 
 
