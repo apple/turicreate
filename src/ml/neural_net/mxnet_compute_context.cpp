9387606692cd67504b9304deb6bc92f5adfdc92c
Shreya Jain <shreyajain@Shreyas-MacBook-Pro-2.local>
2019-08-01 16:48:45 -0700

    add files to build pybind11 and adding mxnet_compute_context

--- src/ml/neural_net/mxnet_compute_context.cpp
+++ src/ml/neural_net/mxnet_compute_context.cpp
@@ -7,12 +7,59 @@
 #include <iostream>
 #include <vector>
 
-#include <core/export.hpp>
 #include <external/pybind11/pybind11.h>
-#include <external/pybind11/stl.h>
 #include <external/pybind11/numpy.h>
 #include <ml/neural_net/mxnet_compute_context.hpp>
-#include <model_server/lib/toolkit_function_macros.hpp>
 
 
+
+namespace turi {
+namespace neural_net {
 namespace py = pybind11;
+namespace {
+
+std::unique_ptr<compute_context> create_mxnet_compute_context() {
+  return std::unique_ptr<compute_context>(new mxnet_compute_context);
+}
+
+// At static-init time, register create_mxnet_compute_context().
+// TODO: Codify priority levels?
+static auto* mxnet_registration = new compute_context::registration(
+    /* priority */ 1, &create_mxnet_compute_context);
+
+}  // namespace
+
+mxnet_compute_context::mxnet_compute_context()
+{}
+
+
+mxnet_compute_context::~mxnet_compute_context() = default;
+
+size_t mxnet_compute_context::memory_budget() const {
+  return 0;
+}
+
+std::vector<std::string> mxnet_compute_context::gpu_names() const {
+  return std::vector<std::string>();
+}
+
+std::unique_ptr<image_augmenter> mxnet_compute_context::create_image_augmenter(
+    const image_augmenter::options& opts) {
+  return std::unique_ptr<image_augmenter>();
+}
+
+
+std::unique_ptr<model_backend> mxnet_compute_context::create_object_detector(
+    int n, int c_in, int h_in, int w_in, int c_out, int h_out, int w_out,
+    const float_array_map& config, const float_array_map& weights) {
+  return std::unique_ptr<model_backend>();
+}
+
+std::unique_ptr<model_backend> mxnet_compute_context::create_activity_classifier(
+    int n, int c_in, int h_in, int w_in, int c_out, int h_out, int w_out,
+    const float_array_map& config, const float_array_map& weights) {
+  return std::unique_ptr<model_backend>();
+}
+
+}  // namespace neural_net
+}  // namespace turi
