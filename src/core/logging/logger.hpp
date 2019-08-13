3505c05282c405adf8717ff6829432a636fd1db3
Guihao Liang <guihao_liang@apple.com>
2019-08-12 18:33:26 -0700

    missing right parenthesis for non-debug mode

--- src/core/logging/logger.hpp
+++ src/core/logging/logger.hpp
@@ -335,7 +335,7 @@
   do {                                                \
     auto throw_error = [&]() GL_COLD_NOINLINE_ERROR { \
       logstream(LOG_ERROR) << (message) << std::endl; \
-      throw(std::runtime_error(message);              \
+      throw(std::runtime_error(message));             \
     };                                                \
     throw_error();                                    \
   } while (0)
