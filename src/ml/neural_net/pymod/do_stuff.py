6ec867b2be430613985b8eca261d8fbd50317abd
Shreya Jain <shreya_jain3@Shreyas-MacBook-Pro.local>
2019-08-27 16:41:30 -0700

    remove pymod

--- src/ml/neural_net/pymod/do_stuff.py
+++
@@ -1,9 +0,0 @@
-from __future__ import absolute_import
-import numpy as np
-
-class NumpyObject:
-	def __init__(self):
-		self.arr1 = np.arange(5.0).astype(np.float32)
-
-	def add(self):
-		return self.arr1 
