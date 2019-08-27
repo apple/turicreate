6116b9cad1b2b32570aedf82ef0a5ab29ff8de94
Shreya Jain <shreya_jain3@Shreyas-MacBook-Pro.local>
2019-08-27 16:48:44 -0700

    removing some work around code

--- src/python/turicreate/toolkits/activity_classifier/_activity_classifier.py
+++ src/python/turicreate/toolkits/activity_classifier/_activity_classifier.py
@@ -170,7 +170,6 @@ def create(dataset, session_id, target, features=None, prediction_window=100,
 
     params = {
         'use_tensorflow': False,
-        'mps' : False,
         'show_deprecated_warnings' : False
     }
 
@@ -212,7 +211,7 @@ def create(dataset, session_id, target, features=None, prediction_window=100,
 
     # Decide whether to use MPS GPU, MXnet GPU or CPU
     num_mxnet_gpus = _mxnet_utils.get_num_gpus_in_use(max_devices=num_sessions)
-    use_mps = _use_mps() and num_mxnet_gpus == 0 and not params['use_tensorflow']
+    use_mps = _use_mps() and num_mxnet_gpus == 0 
 
     if verbose:
         if use_mps:
