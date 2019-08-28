5dd1033658b06a96bc6b12143a3f14896efaa800
Abhishek Pratapa <abhishekpratapa@gmail.com>
2019-08-28 16:25:21 -0700

    Initial Commit for TF Style Transfer

---
+++ src/python/turicreate/toolkits/style_transfer/_tf_model_architecture.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# Copyright © 2019 Apple Inc. All rights reserved.
+#
+# Use of this source code is governed by a BSD-3-clause license that can
+# be found in the LICENSE.txt file or at https://opensource.org/licenses/BSD-3-Clause
+
+from __future__ import print_function as _
+from __future__ import division as _
+from __future__ import absolute_import as _
+
+import tensorflow as _tf
+
+# TODO: Extend from TensorFlowModel
+class StyleTransferTensorFlowModel():
+    def __init__(self, net_params, batch_size, num_styles):
+        # TODO: populate
+        pass
+    def train(self, feed_dict):
+        # TODO: populate
+        pass
+    def predict(self, feed_dict):
+        # TODO: populate
+        pass
+    def export_weights(self):
+        # TODO: populate
+        pass
+    def set_learning_rate(self, lr):
+        # TODO: populate
+        pass
