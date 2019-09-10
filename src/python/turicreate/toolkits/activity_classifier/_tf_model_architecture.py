f4ca2dad3b03e20a300396fae01e3b330db4f509
Shreya Jain <shreya_jain3@Shreyas-MacBook-Pro.local>
2019-09-10 15:19:57 -0700

    making tf model working with c++ implementation

--- src/python/turicreate/toolkits/activity_classifier/_tf_model_architecture.py
+++ src/python/turicreate/toolkits/activity_classifier/_tf_model_architecture.py
@@ -6,41 +6,48 @@
 from __future__ import print_function as _
 from __future__ import division as _
 from __future__ import absolute_import as _
+
 import tensorflow as _tf 
-import numpy as _np
 from .._tf_model import TensorFlowModel
-
-
-_net_params = {
-    'conv_h': 64,
-    'lstm_h': 200,
-    'dense_h': 128
-}
+import numpy as _np
 
 
 class ActivityTensorFlowModel(TensorFlowModel):
     
     def __init__(self, net_params, batch_size, num_features, num_classes, prediction_window, seq_len):
 
+        # Supresses verbosity to only errors
+        _tf.compat.v1.logging.set_verbosity(_tf.compat.v1.logging.ERROR)
+
+
+        _net_params = {
+        'conv_h': 64,
+        'lstm_h': 200,
+        'dense_h': 128
+        }
+
+        for key in net_params.keys():
+            net_params[key] = _np.array(net_params[key], copy=False)
+
         _tf.reset_default_graph()
 
         self.num_classes = num_classes
         self.batch_size = batch_size
         self.seq_len = seq_len
-
+        
         # Vars
         self.data = _tf.compat.v1.placeholder(_tf.float32, [None, prediction_window*seq_len, num_features])
         self.weight = _tf.compat.v1.placeholder(_tf.float32, [None, seq_len, 1])
         self.target = _tf.compat.v1.placeholder(_tf.int32, [None, seq_len, 1])
         self.is_training = _tf.compat.v1.placeholder(_tf.bool)
-
+        
         # Reshaping weights 
         reshaped_weight = _tf.reshape(self.weight, [self.batch_size, seq_len])
 
         # One hot encoding target
         reshaped_target = _tf.reshape(self.target, [self.batch_size, seq_len])
         one_hot_target = _tf.one_hot(reshaped_target, depth=self.num_classes, axis=-1)
-
+        
         # Weights 
         weights = {
         'conv_weight' : _tf.Variable(_tf.zeros([prediction_window, num_features, _net_params['conv_h']]), name='conv_weight'),
@@ -62,8 +69,14 @@ class ActivityTensorFlowModel(TensorFlowModel):
         dropout = _tf.layers.dropout(conv, rate=0.2, training=self.is_training)
 
         # Long Stem Term Memory
-        i2h_i, i2h_f, i2h_c, i2h_o = _np.split(net_params['lstm_i2h_weight'].asnumpy(), 4)
-        h2h_i, h2h_f, h2h_c, h2h_o = _np.split(net_params['lstm_h2h_weight'].asnumpy(), 4)
+        i2h_i = net_params['lstm_i2h_i_weight']
+        i2h_f = net_params['lstm_i2h_f_weight']
+        i2h_c = net_params['lstm_i2h_c_weight']
+        i2h_o = net_params['lstm_i2h_o_weight']
+        h2h_i = net_params['lstm_h2h_i_weight']
+        h2h_f = net_params['lstm_h2h_f_weight']
+        h2h_c = net_params['lstm_h2h_c_weight']
+        h2h_o = net_params['lstm_h2h_o_weight']
         i2h = _np.concatenate((i2h_i, i2h_c, i2h_f, i2h_o), axis=0)
         h2h = _np.concatenate((h2h_i, h2h_c, h2h_f, h2h_o), axis=0)
         lstm = _np.concatenate((i2h, h2h), axis=1)
@@ -72,14 +85,15 @@ class ActivityTensorFlowModel(TensorFlowModel):
         init_state = cells.zero_state(batch_size, _tf.float32)
         rnn_outputs, final_state = _tf.nn.dynamic_rnn(cells, dropout, initial_state=init_state)
 
+        
         # Dense
         dense = _tf.reshape(rnn_outputs, (-1, _net_params['lstm_h']))
         dense = _tf.add(_tf.matmul(dense, weights['dense0_weight']), biases['dense0_bias'])
         dense = _tf.layers.batch_normalization(inputs=dense, 
-            beta_initializer=_tf.initializers.constant(net_params['bn_beta'].asnumpy(), verify_shape=True), 
-            gamma_initializer=_tf.initializers.constant(net_params['bn_gamma'].asnumpy(), verify_shape=True),
-            moving_mean_initializer=_tf.initializers.constant(net_params['bn_moving_mean'].asnumpy(), verify_shape=True), 
-            moving_variance_initializer=_tf.initializers.constant(net_params['bn_moving_var'].asnumpy(), verify_shape=True), training=self.is_training )
+            beta_initializer=_tf.initializers.constant(net_params['bn_beta'], verify_shape=True), 
+            gamma_initializer=_tf.initializers.constant(net_params['bn_gamma'], verify_shape=True),
+            moving_mean_initializer=_tf.initializers.constant(net_params['bn_running_mean'], verify_shape=True), 
+            moving_variance_initializer=_tf.initializers.constant(net_params['bn_running_var'], verify_shape=True), training=self.is_training )
         dense = _tf.nn.relu(dense)
         dense = _tf.layers.dropout(dense, rate=0.5, training=self.is_training)
         
@@ -112,46 +126,75 @@ class ActivityTensorFlowModel(TensorFlowModel):
         # Initialize all variables
         init = _tf.compat.v1.global_variables_initializer()
         self.sess.run(init)
-
+        
         # Assign the initialised weights from MXNet to tensorflow 
         for key in net_params.keys():
             if key in weights.keys():
                 if key.startswith('conv'):
-                    self.sess.run(_tf.assign(_tf.get_default_graph().get_tensor_by_name(key+":0"), _np.transpose(net_params[key].asnumpy(), (2, 1, 0))))
+                    net_params[key] = _np.reshape(_np.transpose(net_params[key], (3, 1, 0, 2)), (prediction_window, self.num_features, _net_params['conv_h']))
+                    self.sess.run(_tf.assign(_tf.get_default_graph().get_tensor_by_name(key+":0"), net_params[key]))
                 elif key.startswith('dense'):
-                    self.sess.run(_tf.assign(_tf.get_default_graph().get_tensor_by_name(key+":0"), _np.transpose(net_params[key].asnumpy()) ))
+                    net_params[key] =  _np.reshape(_np.transpose(net_params[key], (1, 0, 2, 3)), (net_params[key].shape[1], net_params[key].shape[0]))
+                    self.sess.run(_tf.assign(_tf.get_default_graph().get_tensor_by_name(key+":0"), net_params[key] ))
             if key in biases.keys():
-                self.sess.run(_tf.assign(_tf.get_default_graph().get_tensor_by_name(key+":0"), net_params[key].asnumpy()))
+                self.sess.run(_tf.assign(_tf.get_default_graph().get_tensor_by_name(key+":0"), net_params[key]))
 
 
     def train(self, feed_dict):
 
-        _, loss, acc, probs = self.sess.run([self.train_op, self.loss_op, self.acc, self.probs], 
+        for key in feed_dict.keys():
+            feed_dict[key] = _np.array(feed_dict[key], copy=False)
+            feed_dict[key] =  _np.reshape(feed_dict[key], (feed_dict[key].shape[0], feed_dict[key].shape[2], feed_dict[key].shape[3]))
+        _, loss, probs = self.sess.run([self.train_op, self.loss_op, self.probs], 
             feed_dict={self.data : feed_dict['input'], self.target : feed_dict['labels'], self.weight : feed_dict['weights'], self.is_training : True})
-        result = {'loss' : loss, 'acc': acc, 'probs': probs }
+        
+        prob = _np.array(probs)
+        probabilities = _np.reshape(prob, (prob.shape[0], prob.shape[1]*prob.shape[2]))
+        result = {'loss' : _np.array(loss), 'output': probabilities }
+
         return result
 
 
     def predict(self, feed_dict):
 
-        loss, acc, probs= self.sess.run([self.loss_op, self.acc, self.probs], 
-            feed_dict={self.data : feed_dict['input'], self.target : feed_dict['labels'], self.weight : feed_dict['weights'], self.is_training: False})
-        result = {'loss' : loss, 'acc': acc, 'probs': probs }
+
+
+        for key in feed_dict.keys():
+            feed_dict[key] = _np.array(feed_dict[key], copy=False)
+            feed_dict[key] =  _np.reshape(feed_dict[key], (feed_dict[key].shape[0], feed_dict[key].shape[2], feed_dict[key].shape[3]))
+
+        if len(feed_dict.keys()) == 1 :
+            probs = self.sess.run(self.probs, 
+                feed_dict={self.data : feed_dict['input'], self.is_training: False})
+            prob = _np.array(probs)
+            probabilities = _np.reshape(prob, (prob.shape[0], prob.shape[1]*prob.shape[2]))
+            result  = { 'output' :  probabilities}
+            
+        else:
+            loss,  probs= self.sess.run([self.loss_op,  self.probs], 
+                feed_dict={self.data : feed_dict['input'], self.target : feed_dict['labels'], self.weight : feed_dict['weights'], self.is_training: False})
+            prob = _np.array(probs)
+            probabilities = _np.reshape(prob, (prob.shape[0], prob.shape[1]*prob.shape[2]))
+            result = {'loss' : _np.array(loss),  'output': probabilities }
+        
         return result
 
 
     def export_weights(self):
 
-        # Export weights in Core ML Spec
+        # Export weights in Core ML Spec 
         tf_export_params = {}
         tvars = _tf.trainable_variables()
         tvars_vals = self.sess.run(tvars)
+
         for var, val in zip(tvars, tvars_vals):
+            
             if 'weight' in var.name:
                 if var.name.startswith('conv'):
-                    tf_export_params[var.name.split(':')[0]] = _np.transpose(val, (2,1,0))
+                    tf_export_params[var.name.split(':')[0]] = _np.reshape(_np.transpose(val, (2,1,0)), (val.shape[2], val.shape[1], 1, val.shape[0]))
                 elif var.name.startswith('dense'):
-                    tf_export_params[var.name.split(':')[0]] = _np.transpose(val)
+                    tf_export_params[var.name.split(':')[0]] =  _np.reshape(_np.transpose(val), (val.shape[1], val.shape[0], 1, 1))
+
             elif var.name.startswith('rnn/lstm_cell/kernel'):
                 lstm_i2h , lstm_h2h = _np.split(val, [64])
                 i2h_i, i2h_c, i2h_f, i2h_o = _np.split(lstm_i2h, 4, axis=1)
@@ -172,132 +215,21 @@ class ActivityTensorFlowModel(TensorFlowModel):
                 tf_export_params['lstm_h2h_f_bias'] = _np.transpose(h2h_f)
                 tf_export_params['lstm_h2h_o_bias'] = _np.transpose(h2h_o)
             elif var.name.startswith('batch_normalization'):
-                tf_export_params['bn_'+var.name.split('/')[-1][0:-2]] = val
+                tf_export_params['bn_'+var.name.split('/')[-1][0:-2]] = _np.array(val)
             else:
-                tf_export_params[var.name.split(':')[0]] = val
+                tf_export_params[var.name.split(':')[0]] = _np.array(val)
+            
         tvars = _tf.all_variables()
         tvars_vals = self.sess.run(tvars)
         for var, val in zip(tvars, tvars_vals):
             if 'moving_mean' in var.name:
-                tf_export_params['bn_running_mean'] = val
+                tf_export_params['bn_running_mean'] = _np.array(val)
             if 'moving_variance' in var.name:
-                tf_export_params['bn_running_var'] = val
+                tf_export_params['bn_running_var'] = _np.array(val)
         return tf_export_params
 
     def set_learning_rate(self, lr):
+
+        # Set the learning rate
         self.optimizer = _tf.train.AdamOptimizer(learning_rate=lr)
 
-    def get_weights(self):
-
-        import mxnet as _mx 
-
-        arg_params = {}
-        tvars = _tf.trainable_variables()
-        tvars_vals = self.sess.run(tvars)
-        for var, val in zip(tvars, tvars_vals):
-            if 'weight' in var.name:
-                if var.name.startswith('conv'):
-                    arg_params.update(
-                        {var.name.replace(":0", ""): _mx.nd.array(_np.transpose(val, (2,1,0)))})
-                elif var.name.startswith('dense'):
-                    arg_params.update(
-                        {var.name.replace(":0", ""): _mx.nd.array(_np.transpose(val))})
-            elif var.name.startswith('rnn/lstm_cell/kernel'):
-                lstm_i2h , lstm_h2h = _np.split(val, [64])
-                i2h_i, i2h_c, i2h_f, i2h_o = _np.split(lstm_i2h, 4, axis=1)
-                h2h_i, h2h_c, h2h_f, h2h_o = _np.split(lstm_h2h, 4, axis=1)
-                lstm_i2h = _np.concatenate((i2h_i, i2h_f, i2h_c, i2h_o), axis=1)
-                lstm_h2h = _np.concatenate((h2h_i, h2h_f, h2h_c, h2h_o), axis=1)
-                arg_params['lstm_i2h_weight'] =  _mx.nd.array(_np.transpose(lstm_i2h))
-                arg_params['lstm_h2h_weight'] =  _mx.nd.array(_np.transpose(lstm_h2h))
-            elif var.name.startswith('rnn/lstm_cell/bias'):
-                h2h_i, h2h_c, h2h_f, h2h_o = _np.split(val, 4)
-                val = _np.concatenate((h2h_i, h2h_f, h2h_c, h2h_o))
-                arg_params.update({var.name.replace('rnn/lstm_cell/bias:0','lstm_h2h_bias'): _mx.nd.array(val)})
-                arg_params.update({var.name.replace('rnn/lstm_cell/bias:0','lstm_i2h_bias'): _mx.nd.array(_np.zeros(val.shape))})
-            elif var.name.startswith('batch_normalization'):
-                arg_params['bn_'+var.name.split('/')[-1][0:-2]] = _mx.nd.array(val)
-            else:
-                arg_params.update({var.name.replace(":0", ""): _mx.nd.array(val)})
-
-        aux_params = {}
-        tvars = _tf.all_variables()
-        tvars_vals = self.sess.run(tvars)
-        for var, val in zip(tvars, tvars_vals):
-            if 'moving_mean' in var.name:
-                aux_params['bn_moving_mean'] = _mx.nd.array(val)
-            if 'moving_variance' in var.name:
-                aux_params['bn_moving_var'] = _mx.nd.array(val)
-        return arg_params, aux_params
-
-
-def _fit_model_tf(model, net_params, data_iter, valid_iter, max_iterations, verbose, lr):
-    
-    from time import time as _time
-
-    if verbose:
-         # Print progress table header
-        column_names = ['Iteration', 'Train Accuracy', 'Train Loss']
-        if valid_iter:
-            column_names += ['Validation Accuracy', 'Validation Loss']
-        column_names.append('Elapsed Time')
-        num_columns = len(column_names)
-        column_width = max(map(lambda x: len(x), column_names)) + 2
-        hr = '+' + '+'.join(['-' * column_width] * num_columns) + '+'
-        print(hr)
-        print(('| {:<{width}}' * num_columns + '|').format(*column_names, width=column_width-1))
-        print(hr)
-
-    begin = _time()
-    for iteration in range(max_iterations):
-        log = {
-            'train_loss': 0.,
-            'train_acc': 0.
-        }
-
-        # Training iteration
-        data_iter.reset()
-        
-        train_batches = float(data_iter.num_batches) 
-        for batch in data_iter:
-            train_feed_dict = {"input": batch.data, "labels": batch.labels, "weights": batch.weights }
-            result = model.train(train_feed_dict)
-            log['train_loss'] += result['loss'] /train_batches
-            log['train_acc'] += result['acc'] /train_batches
-
-        # Validation iteration
-        if valid_iter is not None:
-            log['valid_acc'] = 0
-            log['valid_loss'] = 0
-            
-            valid_iter.reset()
-            valid_batches = float(valid_iter.num_batches) 
-            for val in valid_iter:
-                val_feed_dict = {"input": val.data, "labels": val.labels, "weights": val.weights }
-                val_results = model.predict(val_feed_dict)
-                log['valid_loss'] += val_results['loss']  / valid_batches
-                log['valid_acc'] += val_results['acc']  / valid_batches
-
-        if verbose:
-            elapsed_time = _time() - begin
-            if valid_iter is None:
-                # print progress row without validation info
-                print("| {cur_iter:<{width}}| {train_acc:<{width}.3f}| {train_loss:<{width}.3f}| {time:<{width}.1f}|".format(
-                          cur_iter = iteration + 1, train_acc = log['train_acc'], train_loss = log['train_loss'],
-                          time = elapsed_time, width = column_width-1))
-            else:
-                # print progress row with validation info
-                print("| {cur_iter:<{width}}| {train_acc:<{width}.3f}| {train_loss:<{width}.3f}"
-                      "| {valid_acc:<{width}.3f}| {valid_loss:<{width}.3f}| {time:<{width}.1f}| ".format(
-                          cur_iter = iteration + 1, train_acc = log['train_acc'], train_loss = log['train_loss'],
-                          valid_acc = log['valid_acc'], valid_loss = log['valid_loss'], time = elapsed_time,
-                          width = column_width-1))
-
-    if verbose:
-        print(hr)
-        print('Training complete')
-        end = _time()
-        print('Total Time Spent: %gs' % (end - begin))
-
-    return log
-        
