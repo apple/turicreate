3bc16530d6f7c03e2e9735f0eca65243f7fa5547
Shreya Jain <47830614+shreyajain17@users.noreply.github.com>
2019-09-13 15:33:42 -0700

    Pybind11 code to call TensorFlow (Python) from C++ implementation (#2300)

---
+++ src/ml/neural_net/tf_model_backend.cpp
@@ -0,0 +1,159 @@
+/* Copyright © 2019 Apple Inc. All rights reserved.
+ *
+ * Use of this source code is governed by a BSD-3-clause license that can
+ * be found in the LICENSE.txt file or at
+ * https://opensource.org/licenses/BSD-3-Clause
+ */
+
+#include <ml/neural_net/tf_model_backend.hpp>
+
+#include <iostream>
+#include <vector>
+
+#include <core/util/try_finally.hpp>
+#include <ml/neural_net/float_array.hpp>
+#include <ml/neural_net/tf_compute_context.hpp>
+
+#include <pybind11/eval.h>
+#include <pybind11/numpy.h>
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
+namespace turi {
+namespace neural_net {
+
+using turi::neural_net::float_array;
+using turi::neural_net::float_array_map;
+using turi::neural_net::shared_float_array;
+
+
+static std::vector<size_t> get_shape(const float_array& num) {
+  return std::vector<size_t>(num.shape(), num.shape() + num.dim());
+}
+
+static std::vector<size_t> get_strides(const float_array& num) {
+  if (num.dim() == 0) {
+    return {};
+  }
+  std::vector<size_t> result(num.dim());
+  const size_t* shape = num.shape();
+  result[num.dim() - 1] = sizeof(float);
+  for (size_t i = num.dim() - 1; i > 0; --i) {
+    result[i - 1] = result[i] * shape[i];
+  }
+  return result;
+}
+
+PYBIND11_MODULE(libtctensorflow, m) {
+  pybind11::class_<float_array>(m, "FloatArray", pybind11::buffer_protocol())
+      .def_buffer([](float_array& m) -> pybind11::buffer_info {
+        return pybind11::buffer_info(
+            const_cast<float*>(m.data()), /* Pointer to buffer */
+            sizeof(float),                /* Size of one scalar */
+            pybind11::format_descriptor<
+                float>::format(), /* Python struct-style format descriptor */
+            m.dim(),              /* Number of dimensions */
+            get_shape(m),         /* Buffer dimensions */
+            get_strides(m)        /* Strides (in bytes) for each index */
+
+        );
+      });
+  pybind11::class_<shared_float_array>(m, "SharedFloatArray",
+                                       pybind11::buffer_protocol())
+      .def_buffer([](shared_float_array& m) -> pybind11::buffer_info {
+        return pybind11::buffer_info(
+            const_cast<float*>(m.data()), /* Pointer to buffer */
+            sizeof(float),                /* Size of one scalar */
+            pybind11::format_descriptor<
+                float>::format(), /* Python struct-style format descriptor */
+            m.dim(),              /* Number of dimensions */
+            get_shape(m),         /* Buffer dimensions */
+            get_strides(m)        /* Strides (in bytes) for each index */
+
+        );
+      });
+}
+
+tf_model_backend::tf_model_backend(pybind11::object model): model_(model) {}
+
+float_array_map tf_model_backend::train(const float_array_map& inputs) {
+
+  // Call train method on the TensorflowModel
+  float_array_map result;
+
+  call_pybind_function([&]() {
+    pybind11::object output = model_.attr("train")(inputs);
+
+    std::map<std::string, pybind11::buffer> buf_output =
+        output.cast<std::map<std::string, pybind11::buffer>>();
+
+    for (auto& kv : buf_output) {
+      pybind11::buffer_info buf = kv.second.request();
+      turi::neural_net::shared_float_array value =
+          turi::neural_net::shared_float_array::copy(
+              static_cast<float*>(buf.ptr),
+              std::vector<size_t>(buf.shape.begin(), buf.shape.end()));
+      result[kv.first] = value;
+    }
+  });
+
+  return result;
+}
+
+float_array_map tf_model_backend::predict(const float_array_map& inputs) const {
+  float_array_map result;
+
+  // Call predict method on the TensorFlowModel 
+  call_pybind_function([&]() {
+    pybind11::object output = model_.attr("predict")(inputs);
+    std::map<std::string, pybind11::buffer> buf_output =
+        output.cast<std::map<std::string, pybind11::buffer>>();
+
+    for (auto& kv : buf_output) {
+      pybind11::buffer_info buf = kv.second.request();
+      turi::neural_net::shared_float_array value =
+          turi::neural_net::shared_float_array::copy(
+              static_cast<float*>(buf.ptr),
+              std::vector<size_t>(buf.shape.begin(), buf.shape.end()));
+      result[kv.first] = value;
+    }
+  });
+
+  return result;
+}
+
+float_array_map tf_model_backend::export_weights() const {
+  float_array_map result;
+  call_pybind_function([&]() {
+
+    // Call export_weights method on the TensorFLowModel
+    pybind11::object exported_weights = model_.attr("export_weights")();
+    std::map<std::string, pybind11::buffer> buf_output =
+        exported_weights.cast<std::map<std::string, pybind11::buffer>>();
+
+    for (auto& kv : buf_output) {
+      pybind11::buffer_info buf = kv.second.request();
+      turi::neural_net::shared_float_array value =
+          turi::neural_net::shared_float_array::copy(
+              static_cast<float*>(buf.ptr),
+              std::vector<size_t>(buf.shape.begin(), buf.shape.end()));
+      result[kv.first] = value;
+    }
+  });
+
+  return result;
+}
+
+void tf_model_backend::set_learning_rate(float lr) {
+  float_array_map result;
+
+  // Call set_learning_rate method on the TensorFLowModel
+  call_pybind_function([&]() { model_.attr("set_learning_rate")(lr); });
+}
+
+tf_model_backend::~tf_model_backend() {
+  call_pybind_function([&]() { model_ = pybind11::object(); });
+}
+
+}  // namespace neural_net
+}  // namespace turi
