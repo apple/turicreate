dda2dfcd6ee73259c6264d85bff361c26d0e979d
Shantanu Chhabra <shantanu_chhabra@apple.com>
2019-09-16 16:21:01 -0700

    Biased angle distributions to zero

--- src/toolkits/object_detection/one_shot_object_detection/util/parameter_sampler.hpp
+++ src/toolkits/object_detection/one_shot_object_detection/util/parameter_sampler.hpp
@@ -62,9 +62,9 @@ class ParameterSampler {
   size_t max_depth_ = 13000;
   double angle_stdev_ = 20.0;
   double focal_stdev_ = 40.0;
-  std::vector<double> theta_means_ = {-180.0, 0.0, 180.0};
-  std::vector<double> phi_means_ = {-180.0, 0.0, 180.0};
-  std::vector<double> gamma_means_ = {-180.0, -90.0, 0.0, 90.0, 180.0};
+  std::vector<double> theta_means_ = {0.0};
+  std::vector<double> phi_means_ = {0.0};
+  std::vector<double> gamma_means_ = {0.0};
   std::default_random_engine theta_generator_;
   std::default_random_engine phi_generator_;
   std::default_random_engine gamma_generator_;
