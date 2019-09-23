08e0241400340af5e91ad3e8c39422bcb87e12de
Abhishek Pratapa <abhishekpratapa@gmail.com>
2019-09-23 15:55:00 -0700

    Clang-Format Style=Google

--- test/neural_net/style_transfer.cxx
+++ test/neural_net/style_transfer.cxx
@@ -1,7 +1,8 @@
 /* Copyright © 2019 Apple Inc. All rights reserved.
  *
  * Use of this source code is governed by a BSD-3-clause license that can
- * be found in the LICENSE.txt file or at https://opensource.org/licenses/BSD-3-Clause
+ * be found in the LICENSE.txt file or at
+ * https://opensource.org/licenses/BSD-3-Clause
  */
 
 #define BOOST_TEST_MODULE
@@ -13,98 +14,98 @@
 #ifdef HAS_MACOS_10_15
 
 struct style_transfer_test {
-public:
+ public:
   /**
     Test Encoding
-    
-    This correctness test checks whether the Style Transfer Encoding layer 
+
+    This correctness test checks whether the Style Transfer Encoding layer
     present in TCMPS is performing as intended by comparing it to the golden
     set.
    */
   void test_encoding() {
-    TS_ASSERT(true); // TODO
+    TS_ASSERT(true);  // TODO
   }
   /**
     Test Residual
-    
+
     This correctness test checks whether the Style Transfer Residual layer
     present in TCMPS is performing as intended by comparing it to the golden
     set.
    */
   void test_residual() {
-    TS_ASSERT(true); // TODO
+    TS_ASSERT(true);  // TODO
   }
 
   /**
     Test Decoding
-    
+
     This correctness test checks whether the Style Transfer Decoding layer
     present in TCMPS is performing as intended by comparing it to the golden
     set.
    */
   void test_decode() {
-    TS_ASSERT(true); // TODO
+    TS_ASSERT(true);  // TODO
   }
 
   /**
     Test Resnet
-    
+
     This correctness test checks whether the Style Transfer Transformer network
     present in TCMPS is performing as intended by comparing it to the golden
     set.
    */
   void test_resnet() {
-    TS_ASSERT(true); // TODO
+    TS_ASSERT(true);  // TODO
   }
 
   /**
     Test Block 1
-    
+
     This correctness test checks whether the VGG Block 1 Layer present in TCMPS
     is performing as intended by comparing it to the golden set.
    */
   void test_block_1() {
-    TS_ASSERT(true); // TODO
+    TS_ASSERT(true);  // TODO
   }
 
   /**
     Test Block 2
-    
+
     This correctness test checks whether the VGG Block 2 Layer present in TCMPS
     is performing as intended by comparing it to the golden set.
    */
   void test_block_2() {
-    TS_ASSERT(true); // TODO
+    TS_ASSERT(true);  // TODO
   }
 
   /**
     VGG 16
-    
+
     This correctness test checks whether the VGG Model present in TCMPS is
     performing as intended by comparing it to the golden set.
    */
   void test_vgg16() {
-    TS_ASSERT(true); // TODO
+    TS_ASSERT(true);  // TODO
   }
 
   /**
     Loss
-    
+
     This test checks whether the Loss out of the total graph defined in TCMPS is
     performing as intended by comparing it to the golden set.
    */
   void test_loss() {
-    TS_ASSERT(true); // TODO
+    TS_ASSERT(true);  // TODO
   }
 
   /**
     Weight Update
-    
-    This test checks whether the Weight Update in the total graph defined in 
+
+    This test checks whether the Weight Update in the total graph defined in
     TCMPS is performing as intended by comparing it to the golden set.
    */
   void test_weight_update() {
-    TS_ASSERT(true); // TODO
+    TS_ASSERT(true);  // TODO
   }
 };
 
@@ -114,37 +115,21 @@ BOOST_FIXTURE_TEST_SUITE(_style_transfer_test, style_transfer_test)
 
 #ifdef HAS_MACOS_10_15
 
-BOOST_AUTO_TEST_CASE(test_encoding) {
-  style_transfer_test::test_encoding();
-}
+BOOST_AUTO_TEST_CASE(test_encoding) { style_transfer_test::test_encoding(); }
 
-BOOST_AUTO_TEST_CASE(test_residual) {
-  style_transfer_test::test_residual();
-}
+BOOST_AUTO_TEST_CASE(test_residual) { style_transfer_test::test_residual(); }
 
-BOOST_AUTO_TEST_CASE(test_decode) {
-  style_transfer_test::test_decode();
-}
+BOOST_AUTO_TEST_CASE(test_decode) { style_transfer_test::test_decode(); }
 
-BOOST_AUTO_TEST_CASE(test_resnet) {
-  style_transfer_test::test_resnet();
-}
+BOOST_AUTO_TEST_CASE(test_resnet) { style_transfer_test::test_resnet(); }
 
-BOOST_AUTO_TEST_CASE(test_block_1) {
-  style_transfer_test::test_block_1();
-}
+BOOST_AUTO_TEST_CASE(test_block_1) { style_transfer_test::test_block_1(); }
 
-BOOST_AUTO_TEST_CASE(test_block_2) {
-  style_transfer_test::test_block_2();
-}
+BOOST_AUTO_TEST_CASE(test_block_2) { style_transfer_test::test_block_2(); }
 
-BOOST_AUTO_TEST_CASE(test_vgg16) {
-  style_transfer_test::test_vgg16();
-}
+BOOST_AUTO_TEST_CASE(test_vgg16) { style_transfer_test::test_vgg16(); }
 
-BOOST_AUTO_TEST_CASE(test_loss) {
-  style_transfer_test::test_loss();
-}
+BOOST_AUTO_TEST_CASE(test_loss) { style_transfer_test::test_loss(); }
 
 BOOST_AUTO_TEST_CASE(test_weight_update) {
   style_transfer_test::test_weight_update();
