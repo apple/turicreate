ceb2946a42c7508780b4e2e9a2abcc9e70c7f2b6
Guihao Liang <guihao_liang@apple.com>
2019-10-08 10:17:45 -0700

    commit

--- src/toolkits/feature_engineering/word_trimmer.cpp
+++ src/toolkits/feature_engineering/word_trimmer.cpp
@@ -202,8 +202,8 @@ void word_trimmer_topk_index_mapping(const gl_sarray& src,
 
         case flex_type_enum::UNDEFINED:
           logstream(LOG_WARNING)
-              << "Skip undefined value. please consider calling "
-              << "dropna on feature columns" << std::endl;
+              << "Skip undefined value. Please consider calling "
+              << "'dropna' on feature columns" << std::endl;
           break;
 
         // Should not be here.
