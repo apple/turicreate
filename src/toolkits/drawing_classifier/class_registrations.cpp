3d3d2b98c688c82fe20b9a8700995e1226987199
Shantanu Chhabra <shantanu_chhabra@apple.com>
2019-10-08 16:31:02 -0700

    Updated with formatting and include feedback

--- src/toolkits/drawing_classifier/class_registrations.cpp
+++ src/toolkits/drawing_classifier/class_registrations.cpp
@@ -3,9 +3,9 @@
  * Use of this source code is governed by a BSD-3-clause license that can
  * be found in the LICENSE.txt file or at https://opensource.org/licenses/BSD-3-Clause
  */
-#include "class_registrations.hpp"
-#include "data_preparation.hpp"
-#include "drawing_classifier.hpp"
+#include <toolkits/drawing_classifier/class_registrations.hpp>
+#include <toolkits/drawing_classifier/data_preparation.hpp>
+#include <toolkits/drawing_classifier/drawing_classifier.hpp>
 
 #include <model_server/lib/toolkit_function_macros.hpp>
 
