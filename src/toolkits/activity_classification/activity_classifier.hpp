84fe55c34f70542b0df861f5a481bbbf40b0ec4e
Henry <henry@Henrys-MacBook-Pro-2.local>
2019-10-10 13:41:49 -0700

    AC c++ classify function, fix a output format bug in predict_window as well.

--- src/toolkits/activity_classification/activity_classifier.hpp
+++ src/toolkits/activity_classification/activity_classifier.hpp
@@ -21,7 +21,6 @@ namespace activity_classification {
 class EXPORT activity_classifier: public ml_model_base {
 
  public:
-  
   static std::tuple<gl_sframe, gl_sframe> random_split_by_session(
       gl_sframe data, std::string session_id_column_name, float fraction,
       size_t seed);
@@ -40,6 +39,7 @@ class EXPORT activity_classifier: public ml_model_base {
              std::map<std::string, flexible_type> opts);
   gl_sarray predict(gl_sframe data, std::string output_type);
   gl_sframe predict_per_window(gl_sframe data, std::string output_type);
+  gl_sframe classify(gl_sframe data, std::string output_frequency);
   variant_map_type evaluate(gl_sframe data, std::string metric);
   std::shared_ptr<coreml::MLModelWrapper> export_to_coreml(
       std::string filename);
@@ -155,6 +155,10 @@ class EXPORT activity_classifier: public ml_model_base {
       "      A probability_vector is given per prediction_window. \n"
       "    - 'class': Class prediction. This returns the class with maximum\n"
       "      probability per prediction_window.\n");
+
+  REGISTER_CLASS_MEMBER_FUNCTION(activity_classifier::classify, "data",
+                                 "output_frequency");
+  register_defaults("classify", {{"output_frequency", std::string("")}});
 
   REGISTER_CLASS_MEMBER_FUNCTION(activity_classifier::evaluate, "data",
                                  "metric");
