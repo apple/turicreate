e7f0def9d5b1e876c79448f88e169f082ae53f21
Hoyt Koepke <hoytak@apple.com>
2019-10-15 17:10:13 -0700

    Updated C++ tests to reflect remotefs support.

--- test/annotation/image_classifier_tests.cxx
+++ test/annotation/image_classifier_tests.cxx
@@ -364,7 +364,6 @@ BOOST_FIXTURE_TEST_SUITE(_image_classification_test, image_classification_test)
 BOOST_AUTO_TEST_CASE(test_pass_through) {
   image_classification_test::test_pass_through();
 }
-#ifndef TC_DISABLE_REMOTEFS
 BOOST_AUTO_TEST_CASE(test_get_items_out_of_index) {
   image_classification_test::test_get_items_out_of_index();
 }
@@ -392,6 +391,5 @@ BOOST_AUTO_TEST_CASE(test_return_annotations_drop_na) {
 BOOST_AUTO_TEST_CASE(test_annotation_registry) {
   image_classification_test::test_annotation_registry();
 }
-#endif
 BOOST_AUTO_TEST_SUITE_END()
 
