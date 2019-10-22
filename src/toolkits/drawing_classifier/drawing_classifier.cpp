5611889255d439ea8cad727edbb016b04c593789
Guihao Liang <guihao_liang@apple.com>
2019-10-22 09:35:46 -0700

    init/export model spec for DC (#2439)
    * initial work
    
    * minor change
    
    * fix Input output order
    
    * anonymous
    
    * add random init
    
    * test for init model
    
    * put vega back
    
    * address some doc issues
    
    * export_coreml + unittest
    
    * use default spec if model is not trained
    
    * put things back
    
    * clean some code

--- src/toolkits/drawing_classifier/drawing_classifier.cpp
+++ src/toolkits/drawing_classifier/drawing_classifier.cpp
@@ -4,39 +4,171 @@
  * be found in the LICENSE.txt file or at https://opensource.org/licenses/BSD-3-Clause
  */
 
-#include <toolkits/drawing_classifier/drawing_classifier.hpp>
-
 #include <iostream>
+#include <memory>
+#include <random>
+#include <sstream>
 
 #include <core/logging/assertions.hpp>
 #include <core/logging/logger.hpp>
+#include <core/util/string_util.hpp>
+#include <ml/neural_net/compute_context.hpp>
+#include <ml/neural_net/model_backend.hpp>
+#include <ml/neural_net/model_spec.hpp>
 #include <model_server/lib/variant_deep_serialize.hpp>
 #include <toolkits/evaluation/metrics.hpp>
 
+#include <toolkits/drawing_classifier/drawing_classifier.hpp>
 
 namespace turi {
 namespace drawing_classifier {
 
 namespace {
 
+using coreml::MLModelWrapper;
 using neural_net::compute_context;
 using neural_net::float_array_map;
 using neural_net::model_backend;
 using neural_net::model_spec;
 using neural_net::shared_float_array;
+using neural_net::weight_initializer;
+using neural_net::xavier_weight_initializer;
+using neural_net::zero_weight_initializer;
+
+using padding_type = model_spec::padding_type;
+// anonymous helper sections
 
 }  // namespace
 
 std::unique_ptr<model_spec> drawing_classifier::init_model() const {
   std::unique_ptr<model_spec> result(new model_spec);
+
+  // state is updated through init_train
+  flex_string target = read_state<flex_string>("target");
+  size_t num_classes = read_state<flex_int>("num_classes");
+
+  // feature columns names
+  const flex_list &features_list = read_state<flex_list>("features");
+
+  result->add_channel_concat(
+      "features",
+      std::vector<std::string>(features_list.begin(), features_list.end()));
+
+  std::mt19937 random_engine;
+  try {
+    std::seed_seq seed_seq{read_state<int>("random_seed")};
+    random_engine = std::mt19937(seed_seq);
+  } catch (const std::out_of_range &e) {
+  }
+
+  weight_initializer initializer = zero_weight_initializer();
+
+  const std::string prefix{"drawing"};
+  const std::string _suffix{"_fwd"};
+  std::string input_name{"features"};
+  std::string output_name;
+
+  {
+    size_t channels_filter = 16;
+    size_t channels_kernel = 1;
+    std::stringstream ss;
+
+    for (size_t ii = 0; ii < 3; ii++) {
+      if (ii) {
+        input_name = std::move(output_name);
+      }
+
+      ss.str("");
+      ss << prefix << "_conv" << ii << "_fwd";
+      output_name = ss.str();
+
+      initializer = xavier_weight_initializer(
+          /* #input_neurons   */ channels_kernel * 3 * 3,
+          /* #output_neurons  */ channels_filter * 3 * 3, &random_engine);
+
+      result->add_convolution(
+          /* name                */ output_name,
+          /* input               */ input_name,
+          /* num_output_channels */ channels_filter,
+          /* num_kernel_channels */ channels_kernel,
+          /* kernel_height       */ 3,
+          /* kernel_width        */ 3,
+          /* stride_height       */ 1,
+          /* stride_width        */ 1,
+          /* padding             */ padding_type::SAME,
+          /* weight_init_fn      */ initializer,
+          /* bias_init_fn        */ zero_weight_initializer());
+
+      channels_kernel = channels_filter;
+      channels_filter *= 2;
+
+      input_name = std::move(output_name);
+      ss.str("");
+      ss << prefix << "_relu" << ii << _suffix;
+      output_name = ss.str();
+
+      result->add_relu(output_name, input_name);
+
+      input_name = std::move(output_name);
+      ss.str("");
+      ss << prefix << "_pool" << ii << _suffix;
+      output_name = ss.str();
+      result->add_pooling(
+          /* name                 */ output_name,
+          /* input                */ input_name,
+          /* kernel_height        */ 2,
+          /* kernel_width         */ 2,
+          /* stride_height        */ 2,
+          /* stride_width         */ 2,
+          /* padding              */ padding_type::VALID,
+          /* avg excluded padding */ false);
+    }
+  }
+
+  input_name = std::move(output_name);
+  output_name = prefix + "_flatten0" + _suffix;
+
+  result->add_flatten(output_name, input_name);
+
+  input_name = std::move(output_name);
+  output_name = prefix + "_dense0" + _suffix;
+
+  initializer = xavier_weight_initializer(
+      /* fan_in    */ 64 * 3 * 3,
+      /* fan_out   */ 128, &random_engine);
+
+  result->add_inner_product(
+      /* name                */ output_name,
+      /* input               */ input_name,
+      /* num_output_channels */ 128,
+      /* num_input_channels  */ 64 * 3 * 3,
+      /* weight_init_fn      */ initializer);
+
+  input_name = std::move(output_name);
+  output_name = prefix + "_dense1" + _suffix;
+
+  initializer = xavier_weight_initializer(
+      /* fan_in    */ 128,
+      /* fan_out   */ num_classes, &random_engine);
+
+  result->add_inner_product(
+      /* name                */ output_name,
+      /* input               */ input_name,
+      /* num_output_channels */ num_classes,
+      /* num_input_channels  */ 128,
+      /* weight_init_fn      */ initializer);
+
+  input_name = std::move(output_name);
+
+  result->add_softmax(target + "Probability", input_name);
+
   return result;
 }
 
-void drawing_classifier::train(gl_sframe data,
-    std::string target_column_name,
-    std::string feature_column_name,
-    variant_type validation_data,
-    std::map<std::string, flexible_type> opts) {
+void drawing_classifier::train(gl_sframe data, std::string target_column_name,
+                               std::string feature_column_name,
+                               variant_type validation_data,
+                               std::map<std::string, flexible_type> opts) {
   nn_spec_ = init_model();
   /* TODO: Add code to train! */
 }
@@ -46,26 +178,63 @@ gl_sarray drawing_classifier::predict(gl_sframe data, std::string output_type) {
   return gl_sarray();
 }
 
-gl_sframe drawing_classifier::predict_topk(gl_sframe data, 
-  std::string output_type, size_t k) {
+gl_sframe drawing_classifier::predict_topk(gl_sframe data,
+                                           std::string output_type, size_t k) {
   /* TODO: Add code to predict_topk! */
   return gl_sframe();
 }
 
-variant_map_type drawing_classifier::evaluate(gl_sframe data, std::string metric) {
+variant_map_type drawing_classifier::evaluate(gl_sframe data,
+                                              std::string metric) {
   // Perform prediction.
   gl_sarray predictions = predict(data, "probability_vector");
 
   /* TODO: This is just for the skeleton. Rewrite. */
-  return evaluation::compute_classifier_metrics(
-      data, "label", metric, predictions,
-      {{"classes", 2}});
+  return evaluation::compute_classifier_metrics(data, "label", metric,
+                                                predictions, {{"classes", 2}});
 }
 
+
 std::shared_ptr<coreml::MLModelWrapper> drawing_classifier::export_to_coreml(
-    std::string filename) {
+    std::string filename, bool use_default_spec) {
   /* Add code for export_to_coreml */
-  return nullptr;
+  if (!nn_spec_) {
+    // use empty nn spec if not initalized;
+    // avoid test bad memory access
+    if (use_default_spec) {
+      nn_spec_ = std::unique_ptr<model_spec>(new model_spec);
+    } else {
+      log_and_throw("model is not initialized; please call train before export_coreml");
+    }
+  }
+
+  std::shared_ptr<MLModelWrapper> model_wrapper =
+      export_drawing_classifier_model(
+          *nn_spec_, read_state<flex_list>("features"),
+          read_state<flex_list>("classes"), read_state<flex_string>("target"));
+
+  const flex_list &features_list = read_state<flex_list>("features");
+  const flex_string features_string =
+      join(std::vector<std::string>(features_list.begin(), features_list.end()),
+           ",");
+
+  flex_dict user_defined_metadata = {
+      {"target", read_state<flex_string>("target")},
+      {"features", features_string},
+      {"max_iterations", read_state<flex_int>("max_iterations")},
+      {"warm_start", read_state<flex_int>("warm_start")},
+      {"type", "drawing_classifier"},
+      {"version", 2},
+  };
+
+  model_wrapper->add_metadata(
+      {{"user_defined", std::move(user_defined_metadata)}});
+
+  if (!filename.empty()) {
+    model_wrapper->save(filename);
+  }
+
+  return model_wrapper;
 }
 
 }  // namespace drawing_classifier
--- src/toolkits/drawing_classifier/drawing_classifier.hpp
+++ src/toolkits/drawing_classifier/drawing_classifier.hpp
@@ -6,23 +6,22 @@
 #ifndef TURI_DRAWING_CLASSIFIER_H_
 #define TURI_DRAWING_CLASSIFIER_H_
 
-#include <core/logging/table_printer/table_printer.hpp>
-#include <model_server/lib/extensions/ml_model.hpp>
-#include <model_server/lib/variant.hpp>
 #include <core/data/sframe/gl_sframe.hpp>
-#include <toolkits/coreml_export/mlmodel_wrapper.hpp>
+#include <core/logging/table_printer/table_printer.hpp>
 #include <ml/neural_net/compute_context.hpp>
 #include <ml/neural_net/model_backend.hpp>
 #include <ml/neural_net/model_spec.hpp>
+#include <model_server/lib/extensions/ml_model.hpp>
+#include <model_server/lib/variant.hpp>
+#include <toolkits/coreml_export/mlmodel_wrapper.hpp>
+#include <toolkits/coreml_export/neural_net_models_exporter.hpp>
 
 namespace turi {
 namespace drawing_classifier {
 
-class EXPORT drawing_classifier: public ml_model_base {
-
+class EXPORT drawing_classifier : public ml_model_base {
  public:
-  
-  // ml_model_base interface
+  /* ml_model_base interface */
 
   /* Commented out for the purpose of a skeleton. */
   // void init_options(const std::map<std::string, flexible_type>& opts) override;
@@ -30,18 +29,21 @@ class EXPORT drawing_classifier: public ml_model_base {
   // void save_impl(oarchive& oarc) const override;
   // void load_version(iarchive& iarc, size_t version) override;
 
-  // Interface exposed via Unity server
+  /* Interface exposed via Unity server */
 
-  void train(gl_sframe data,
-             std::string target_column_name, std::string feature_column_name,
-             variant_type validation_data,
+  void train(gl_sframe data, std::string target_column_name,
+             std::string feature_column_name, variant_type validation_data,
              std::map<std::string, flexible_type> opts);
+
   gl_sarray predict(gl_sframe data, std::string output_type);
+
   gl_sframe predict_topk(gl_sframe data, std::string output_type, size_t k);
+
   variant_map_type evaluate(gl_sframe data, std::string metric);
+
   std::shared_ptr<coreml::MLModelWrapper> export_to_coreml(
-      std::string filename);
-  
+      std::string filename, bool use_default_spec = false);
+
   BEGIN_CLASS_MEMBER_REGISTRATION("drawing_classifier")
 
   IMPORT_BASE_CLASS_REGISTRATION(ml_model_base);
@@ -49,10 +51,12 @@ class EXPORT drawing_classifier: public ml_model_base {
   REGISTER_CLASS_MEMBER_FUNCTION(drawing_classifier::train, "data",
                                  "target_column_name", "feature_column_name",
                                  "validation_data", "options");
+
   register_defaults("train",
                     {{"validation_data", to_variant(std::string("auto"))},
                      {"options",
                       to_variant(std::map<std::string, flexible_type>())}});
+
   REGISTER_CLASS_MEMBER_DOCSTRING(
       drawing_classifier::train,
       "----------\n"
@@ -102,7 +106,9 @@ class EXPORT drawing_classifier: public ml_model_base {
 
   REGISTER_CLASS_MEMBER_FUNCTION(drawing_classifier::predict, "data",
                                  "output_type");
+
   register_defaults("predict", {{"output_type", std::string("class")}});
+
   REGISTER_CLASS_MEMBER_DOCSTRING(
       drawing_classifier::predict,
       "----------\n"
@@ -111,8 +117,8 @@ class EXPORT drawing_classifier: public ml_model_base {
       "    If dataset is an SFrame, it must have a column with the same name\n"
       "    as the feature column during training. Additional columns are\n"
       "    ignored.\n"
-      "    If the data is a single drawing, it can be either of type tc.Image,\n"
-      "    in which case it is a bitmap-based drawing input,\n"
+      "    If the data is a single drawing, it can be either of type\n"
+      "    tc.Image, in which case it is a bitmap-based drawing input,\n"
       "    or of type list, in which case it is a stroke-based drawing input.\n"
       "output_type : {\"class\", \"probability_vector\"}, optional\n"
       "    Form of each prediction which is one of:\n"
@@ -121,12 +127,14 @@ class EXPORT drawing_classifier: public ml_model_base {
       "      alphanumerically by name of the class in the training set) is in\n"
       "      position 0 of the vector, the second in position 1 and so on.\n"
       "    - \"class\": Class prediction. This returns the class with maximum\n"
-      "      probability.\n"
-  );
+      "      probability.\n");
 
-  REGISTER_CLASS_MEMBER_FUNCTION(drawing_classifier::predict_topk,
-                                 "data", "output_type", "k");
-  register_defaults("predict_topk", {{"output_type", std::string("probability")}});
+  REGISTER_CLASS_MEMBER_FUNCTION(drawing_classifier::predict_topk, "data",
+                                 "output_type", "k");
+
+  register_defaults("predict_topk",
+                    {{"output_type", std::string("probability")}});
+
   REGISTER_CLASS_MEMBER_DOCSTRING(
       drawing_classifier::predict_topk,
       "----------\n"
@@ -145,14 +153,16 @@ class EXPORT drawing_classifier: public ml_model_base {
 
   REGISTER_CLASS_MEMBER_FUNCTION(drawing_classifier::evaluate, "data",
                                  "metric");
+
   register_defaults("evaluate", {{"metric", std::string("auto")}});
+
   REGISTER_CLASS_MEMBER_DOCSTRING(
       drawing_classifier::evaluate,
       "----------\n"
       "data : SFrame\n"
       "    Dataset of new observations. Must include columns with the same\n"
-      "    names as the features used for model training, but does not require\n"
-      "    a target column. Additional columns are ignored.\n"
+      "    names as the features used for model training, but does not\n"
+      "    require a target column. Additional columns are ignored.\n"
       "metric : str, optional\n"
       "    Name of the evaluation metric.  Possible values are:\n"
       "    - 'auto'             : Returns all available metrics\n"
@@ -164,29 +174,30 @@ class EXPORT drawing_classifier: public ml_model_base {
       "    - 'log_loss'         : Log loss\n"
       "    - 'confusion_matrix' : An SFrame with counts of possible\n"
       "                           prediction/true label combinations.\n"
-      "    - 'roc_curve'        : An SFrame containing information needed for an\n"
-      "                           ROC curve\n"
-  );
+      "    - 'roc_curve'        : An SFrame containing information needed for\n"
+      "                           an ROC curve\n");
   REGISTER_CLASS_MEMBER_FUNCTION(drawing_classifier::export_to_coreml,
                                  "filename");
 
   END_CLASS_MEMBER_REGISTRATION
 
  protected:
-
   // Returns the initial neural network to train
   virtual std::unique_ptr<neural_net::model_spec> init_model() const;
 
+  template <typename T>
+  T read_state(const std::string& key) const {
+    return variant_get_value<T>(get_state().at(key));
+  }
 
  private:
   // Primary representation for the trained model.
   std::unique_ptr<neural_net::model_spec> nn_spec_;
   std::unique_ptr<neural_net::model_backend> training_model_;
   std::unique_ptr<neural_net::compute_context> training_compute_context_;
-
 };
 
 }  // namespace drawing_classifier
 }  // namespace turi
 
-#endif //TURI_DRAWING_CLASSIFIER_H_
+#endif  // TURI_DRAWING_CLASSIFIER_H_
