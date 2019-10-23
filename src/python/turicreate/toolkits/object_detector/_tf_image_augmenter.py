77db6c9a12b34f9b24242f9775238129ddf4ce9f
Shreya Jain <shreya_jain3@Shreyas-MacBook-Pro.local>
2019-10-23 10:52:27 -0700

    changes in _tf_image_augmenter

--- src/python/turicreate/toolkits/object_detector/_tf_image_augmenter.py
+++ src/python/turicreate/toolkits/object_detector/_tf_image_augmenter.py
@@ -20,42 +20,20 @@ from tensorflow.python.ops import random_ops
 from tensorflow.python.ops import logging_ops
 from tensorflow.python.ops import variables
 
-
-def get_augmented_data(images, annotations, resize_only=False):
-    input_shape = (416, 416)
+def get_augmented_data(images, annotations, output_height, output_width, resize_only):
     session = tf.Session()
-    aug_images = []
-    augmented_annotations = []
-    # TODO : Get resize_only from C++ and call get_resized_image()
-    for i in range(len(images)):
-        image = images[i] 
-        image = _utils.convert_shared_float_array_to_numpy(image)
-        height, width = image.shape[0], image.shape[1]
-        image, transformation  = augment_each_image(image)
-        img, trans = session.run([image, transformation])
-        aug_images.append(img)
-        ann = annotations[i]
-        annotation = _utils.convert_shared_float_array_to_numpy(ann)
-        if annotations:
-            identifier = np.expand_dims(annotation[:, 0], axis=1)
-            box = np.zeros(annotation[:, 1:5].shape)
-            for j in range(len(annotation)):
-                box[j][0] = annotation[j][2]*float(height)
-                box[j][1] = annotation[j][1]*float(width)
-                box[j][2] = (annotation[j][4]+annotation[j][2])*float(height)
-                box[j][3] = (annotation[j][3]+annotation[j][1])*float(width) 
-            confidence = np.expand_dims(annotation[:, 5], axis=1)
-            bbox_out = apply_bounding_box_transformation(box, trans, input_shape)
-            bbox = np.zeros(bbox_out.shape)
-            for k in range(len(bbox_out)):
-              bbox[k][0] = bbox_out[k][1]/input_shape[1]
-              bbox[k][1] = bbox_out[k][0]/input_shape[0]
-              bbox[k][2] = (bbox_out[k][3] - bbox_out[k][1])/input_shape[1]
-              bbox[k][3] = (bbox_out[k][2] - bbox_out[k][0])/input_shape[0]
-            an = np.hstack((np.hstack((identifier, bbox)), confidence))
-        augmented_annotations.append(an)
-    augmented_images = np.array(aug_images)
-    return tuple((augmented_images, augmented_annotations))
+    output_shape = (output_height, output_width)
+    if resize_only:
+        images = get_resized_images(images, output_shape)
+        images = session.run(images)
+        resized = np.ascontiguousarray(np.array(images, dtype=np.float32))
+        return tuple((resized, batch_size*[np.zeros(6)]))
+    else:
+        imgs, transformations = get_augmented_images(images, output_shape)
+        augmented_images, trans = session.run([imgs, transformations])
+        augmented_annotations = apply_bounding_box_transformation(images, annotations, trans, output_shape)
+        augmented_images = np.array(augmented_images, dtype=np.float32)
+        return tuple((augmented_images, augmented_annotations))
 
 def is_tensor(x):
     # Checks if `x` is a symbolic tensor-like object.
@@ -115,13 +93,6 @@ def check_atlease_three_dim_image(image, require_static=True):
         return []
 
 def pad_to_ensure_size(image, target_height, target_width, random=True):
-    """
-    Pads if below target size, but does nothing if above.
-
-    If `width` or `height` is smaller than the specified `target_width` or
-    `target_height` respectively, this op centrally pads with 0 along that
-    dimension.
-    """
     image = ops.convert_to_tensor(image, name='image')
 
     assert_ops = []
@@ -160,6 +131,7 @@ def pad_to_ensure_size(image, target_height, target_width, random=True):
     resized = pad_to_bounding_box(image, offset_pad_height, offset_pad_width,
                                            max_(target_height, height), max_(target_width, width))
 
+    # In theory all the checks below are redundant.
     if resized.get_shape().ndims is None:
         raise ValueError('resized contains no shape.')
 
@@ -169,15 +141,6 @@ def pad_to_ensure_size(image, target_height, target_width, random=True):
 
 def pad_to_bounding_box(image, offset_height, offset_width, target_height,
                                       target_width):
-    """
-    Pad `image` with zeros to the specified `height` and `width`.
-    Adds `offset_height` rows of zeros on top, `offset_width` columns of
-    zeros on the left, and then pads the image on the bottom and right
-    with zeros until it has dimensions `target_height`, `target_width`.
-    This op does nothing if `offset_*` is zero and the image already has size
-    `target_height` by `target_width`
-    """
-
     image = ops.convert_to_tensor(image, name='image')
 
     is_batch = True
@@ -226,26 +189,55 @@ def pad_to_bounding_box(image, offset_height, offset_width, target_height,
 
     return padded
 
-def apply_bounding_box_transformation(bbox, transformation, clip_to_shape=None):
+def apply_bounding_box_transformation(images, annotations, transformations, clip_to_shape=None):
 
-  # The bounding box is [n, 4] reshaped and ones added to multiply to tranformation matrix
-  v = np.concatenate([bbox.reshape(-1, 2), np.ones((bbox.shape[0]*2, 1), dtype=np.float32)], axis=1)
-  # Transform
-  v = np.dot(v, np.transpose(transformation))
-  # Reverse shape
-  bbox_out = v[:, :2].reshape(-1, 4)
-  # Make points correctly ordered (lower < upper)
-  for i in range(len(bbox_out)):
-      if bbox_out[i][0] > bbox_out[i][2]:
-          bbox_out[i][0], bbox_out[i][2] = bbox_out[i][2], bbox_out[i][0]
-      if bbox_out[i][1] > bbox_out[i][3]:
-          bbox_out[i][1], bbox_out[i][3] = bbox_out[i][3], bbox_out[i][1]
+  aug_anns = []
+  for i in range(len(annotations)):
+      image = _utils.convert_shared_float_array_to_numpy(images[i])
+      height = image.shape[0]
+      width = image.shape[0]
+      ann = annotations[i]
+      annotation = _utils.convert_shared_float_array_to_numpy(ann)
+      identifier = np.expand_dims(annotation[:, 0], axis=1)
+      box = np.zeros(annotation[:, 1:5].shape)
+      for j in range(len(annotation)):
+          box[j][0] = annotation[j][2]*float(height)
+          box[j][1] = annotation[j][1]*float(width)
+          box[j][2] = (annotation[j][4]+annotation[j][2])*float(height)
+          box[j][3] = (annotation[j][3]+annotation[j][1])*float(width) 
+      
+      confidence = np.expand_dims(annotation[:, 5], axis=1)
 
-      if clip_to_shape is not None:
-          bbox_out[:, 0::2] = np.clip(bbox_out[:, 0::2], 0, clip_to_shape[0])
-          bbox_out[:, 1::2] = np.clip(bbox_out[:, 1::2], 0, clip_to_shape[1])
+      # The bounding box is [n, 4] reshaped and ones added to multiply to tranformation matrix
+      v = np.concatenate([box.reshape(-1, 2), np.ones((box.shape[0]*2, 1), dtype=np.float32)], axis=1)
+      # Transform
+      v = np.dot(v, np.transpose(transformations[i]))
+      # Reverse shape
+      bbox_out = v[:, :2].reshape(-1, 4)
+      
+      # Make points correctly ordered (lower < upper)
+      # Can probably be made much nicer (numpy-ified?)
+      for i in range(len(bbox_out)):
+          if bbox_out[i][0] > bbox_out[i][2]:
+              bbox_out[i][0], bbox_out[i][2] = bbox_out[i][2], bbox_out[i][0]
+          if bbox_out[i][1] > bbox_out[i][3]:
+              bbox_out[i][1], bbox_out[i][3] = bbox_out[i][3], bbox_out[i][1]
 
-  return bbox_out
+          if clip_to_shape is not None:
+              bbox_out[:, 0::2] = np.clip(bbox_out[:, 0::2], 0, clip_to_shape[0])
+              bbox_out[:, 1::2] = np.clip(bbox_out[:, 1::2], 0, clip_to_shape[1])
+
+      bbox = np.zeros(bbox_out.shape)
+      for k in range(len(bbox_out)):
+          bbox[k][0] = bbox_out[k][1]/float(clip_to_shape[0])
+          bbox[k][1] = bbox_out[k][0]/float(clip_to_shape[1])
+          bbox[k][2] = (bbox_out[k][3] - bbox_out[k][1])/float(clip_to_shape[0])
+          bbox[k][3] = (bbox_out[k][2] - bbox_out[k][0])/float(clip_to_shape[1])
+      
+      an = np.hstack((np.hstack((identifier, bbox)), confidence))
+      an = np.ascontiguousarray(an, dtype=np.float32)
+      aug_anns.append(an)
+  return aug_anns
 
 def _assert(cond, ex_type, msg):
     # A polymorphic assert, works with tensors and boolean expressions.
@@ -261,95 +253,108 @@ def _assert(cond, ex_type, msg):
         else:
             return []
 
-def augment_each_image(image):
-    # Applies augmentation on the given image based on the options
+def get_augmented_images(images, output_shape):
+
+    transformations = []
+    augmented_images = []
 
     # Augmentation option
-    grid_shape = [13, 13]
-    min_scale=1/1.5 
-    max_scale=1.5 
+    min_scale = 1/1.5 
+    max_scale = 1.5 
     max_aspect_ratio=1.5 
     max_hue=0.05 
     max_brightness=0.05 
     max_saturation=1.25 
     max_contrast=1.25 
     horizontal_flip=True 
-    input_shape = (416, 416)
 
-    height, width, _ = tf.unstack(tf.shape(image))
-    scale_h = tf.random_uniform([], minval=min_scale, maxval=max_scale)
-    scale_w = scale_h * tf.exp(tf.random_uniform([], minval=-np.log(max_aspect_ratio), maxval=np.log(max_aspect_ratio)))
-    new_height = tf.to_int32(tf.to_float(height) * scale_h)
-    new_width = tf.to_int32(tf.to_float(width) * scale_w)
+    for i in range(len(images)):
 
-    image_scaled = tf.squeeze(tf.image.resize_bilinear(tf.expand_dims(image, 0), [new_height, new_width]), [0])
-    # Image padding
-    pad_image, pad_offset = pad_to_ensure_size(image_scaled, input_shape[0], input_shape[1])
+        image = images[i]
+        image = _utils.convert_shared_float_array_to_numpy(image)
+        
+        height, width, _ = tf.unstack(tf.shape(image))
+        scale_h = tf.random_uniform([], minval=min_scale, maxval=max_scale)
+        scale_w = scale_h * tf.exp(tf.random_uniform([], minval=-np.log(max_aspect_ratio), maxval=np.log(max_aspect_ratio)))
+        new_height = tf.to_int32(tf.to_float(height) * scale_h)
+        new_width = tf.to_int32(tf.to_float(width) * scale_w)
 
-    new_height = tf.maximum(input_shape[0], new_height)
-    new_width = tf.maximum(input_shape[1], new_width)
+        image_scaled = tf.squeeze(tf.image.resize_bilinear(tf.expand_dims(image, 0), [new_height, new_width]), [0])
+        # Image padding
+        pad_image, pad_offset = pad_to_ensure_size(image_scaled, output_shape[0], output_shape[1])
 
-    slice_offset = (tf.random_uniform([], minval=0, maxval=new_height - input_shape[0] + 1, dtype=tf.int32),
-                    tf.random_uniform([], minval=0, maxval=new_width - input_shape[1] + 1, dtype=tf.int32))
-    augmented_image = array_ops.slice(pad_image, [slice_offset[0], slice_offset[1], 0], [input_shape[0], input_shape[1], 3])
+        new_height = tf.maximum(output_shape[0], new_height)
+        new_width = tf.maximum(output_shape[1], new_width)
 
-    if horizontal_flip:
-        uniform_random = random_ops.random_uniform([], 0, 1.0)
-        did_horiz_flip = math_ops.less(uniform_random, .5)
-        augmented_image = control_flow_ops.cond(did_horiz_flip,
-                                         lambda: array_ops.reverse(augmented_image, [1]),
-                                         lambda: augmented_image)
-        flip_sign = 1 - tf.to_float(did_horiz_flip) * 2
-    else:
-        flip_sign = 1
-        did_horiz_flip = tf.constant(False)
+        slice_offset = (tf.random_uniform([], minval=0, maxval=new_height - output_shape[0] + 1, dtype=tf.int32),
+                        tf.random_uniform([], minval=0, maxval=new_width - output_shape[1] + 1, dtype=tf.int32))
+        augmented_image = array_ops.slice(pad_image, [slice_offset[0], slice_offset[1], 0], [output_shape[0], output_shape[1], 3])
 
-    ty = tf.to_float(pad_offset[0] - slice_offset[0])
-    tx = flip_sign * tf.to_float(pad_offset[1] - slice_offset[1]) + tf.to_float(did_horiz_flip) * input_shape[1]
-    transformation = tf.reshape(tf.stack([
-        scale_h, 0.0,                  ty,
-        0.0,     flip_sign * scale_w,  tx,
-        0.0,     0.0,                 1.0]
-        ), (3, 3))
+        if horizontal_flip:
+            uniform_random = random_ops.random_uniform([], 0, 1.0)
+            did_horiz_flip = math_ops.less(uniform_random, .5)
+            augmented_image = control_flow_ops.cond(did_horiz_flip,
+                                             lambda: array_ops.reverse(augmented_image, [1]),
+                                             lambda: augmented_image)
+            flip_sign = 1 - tf.to_float(did_horiz_flip) * 2
+        else:
+            flip_sign = 1
+            did_horiz_flip = tf.constant(False)
 
-    if max_hue is not None and max_hue > 0:
-        image = tf.image.random_hue(augmented_image, max_delta=max_hue)
+        ty = tf.to_float(pad_offset[0] - slice_offset[0] ) 
+        tx = flip_sign * tf.to_float(pad_offset[1] - slice_offset[1] ) + tf.to_float(did_horiz_flip) * output_shape[1]
+        transformation = tf.reshape(tf.stack([
+            scale_h, 0.0,                  ty,
+            0.0,     flip_sign * scale_w,   tx,
+            0.0,     0.0,                 1.0]
+            ), (3, 3))
 
-    if max_brightness is not None and max_brightness > 0:
-        image = tf.image.random_brightness(augmented_image, max_delta=max_brightness)
+        if max_hue is not None and max_hue > 0:
+            image = tf.image.random_hue(augmented_image, max_delta=max_hue)
 
-    if max_saturation is not None and max_saturation > 1.0:
-        log_sat = np.log(max_saturation)
-        image = tf.image.random_saturation(augmented_image, lower=np.exp(-log_sat), upper=np.exp(log_sat))
+        if max_brightness is not None and max_brightness > 0:
+            image = tf.image.random_brightness(augmented_image, max_delta=max_brightness)
 
-    if max_contrast is not None and max_contrast > 1.0:
-        log_con = np.log(max_contrast)
-        image = tf.image.random_contrast(augmented_image, lower=np.exp(-log_con), upper=np.exp(log_con))
+        if max_saturation is not None and max_saturation > 1.0:
+            log_sat = np.log(max_saturation)
+            image = tf.image.random_saturation(augmented_image, lower=np.exp(-log_sat), upper=np.exp(log_sat))
 
-    augmented_image = tf.clip_by_value(augmented_image, 0, 1)
-    return augmented_image, transformation
+        if max_contrast is not None and max_contrast > 1.0:
+            log_con = np.log(max_contrast)
+            image = tf.image.random_contrast(augmented_image, lower=np.exp(-log_con), upper=np.exp(log_con))
 
-def get_resized_image(image):
+        augmented_image = tf.clip_by_value(augmented_image, 0, 1)
+        augmented_images.append(augmented_image)
+        transformations.append(transformation)
+        
+    return augmented_images, transformations
 
-    # Resizes the input image to fixes size
-    input_shape = (416, 416)
-    height, width, _ = tf.unstack(tf.shape(image))
-    orig_shape = (height, width)
-    scale_h = tf.constant(input_shape[0], dtype=tf.float32) / tf.to_float(height)
-    scale_w = tf.constant(input_shape[1], dtype=tf.float32) / tf.to_float(width)
-    new_height = tf.to_int32(tf.to_float(height) * scale_h)
-    new_width = tf.to_int32(tf.to_float(width) * scale_w)
+def get_resized_images(images, output_shape):
+    
+    resized_images = []
+    for i in range(len(images)):
+        
+        image = images[i] 
+        image = _utils.convert_shared_float_array_to_numpy(image)
+        height, width, _ = tf.unstack(tf.shape(image))
+        orig_shape = (height, width)
+        scale_h = tf.constant(output_shape[0], dtype=tf.float32) / tf.to_float(height)
+        scale_w = tf.constant(output_shape[1], dtype=tf.float32) / tf.to_float(width)
+        new_height = tf.to_int32(tf.to_float(height) * scale_h)
+        new_width = tf.to_int32(tf.to_float(width) * scale_w)
 
-    image_scaled = tf.squeeze(tf.image.resize_bilinear(tf.expand_dims(image, 0), [new_height, new_width]), [0])
+        image_scaled = tf.squeeze(tf.image.resize_bilinear(tf.expand_dims(image, 0), [new_height, new_width]), [0])
 
-    pad_image, pad_offset = pad_to_ensure_size(image_scaled, input_shape[0], input_shape[1],
+        pad_image, pad_offset = pad_to_ensure_size(image_scaled, output_shape[0], output_shape[1],
               random=False)
 
-    new_height = tf.maximum(input_shape[0], new_height)
-    new_width = tf.maximum(input_shape[1], new_width)
+        new_height = tf.maximum(output_shape[0], new_height)
+        new_width = tf.maximum(output_shape[1], new_width)
 
-    slice_offset = (tf.random_uniform([], minval=0, maxval=new_height - input_shape[0] + 1, dtype=tf.int32),
-                      tf.random_uniform([], minval=0, maxval=new_width - input_shape[1] + 1, dtype=tf.int32))
-    image = array_ops.slice(pad_image, [slice_offset[0], slice_offset[1], 0], [input_shape[0], input_shape[1], 3])
-    image = tf.clip_by_value(image, 0, 1)
-    return image
+        slice_offset = (tf.random_uniform([], minval=0, maxval=new_height - output_shape[0] + 1, dtype=tf.int32),
+                      tf.random_uniform([], minval=0, maxval=new_width - output_shape[1] + 1, dtype=tf.int32))
+        image = array_ops.slice(pad_image, [slice_offset[0], slice_offset[1], 0], [output_shape[0], output_shape[1], 3])
+        image = tf.clip_by_value(image, 0, 1)
+        resized_images.append(image)
+
+    return resized_images
