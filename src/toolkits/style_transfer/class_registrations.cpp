34daa95727162072dba71a62563d94271d9acf57
Shreya Jain <shreya_jain3@Shreyas-MacBook-Pro.local>
2019-10-24 15:42:57 -0700

    add registration

---
+++ src/toolkits/style_transfer/class_registrations.cpp
@@ -0,0 +1,18 @@
+/* Copyright © 2019 Apple Inc. All rights reserved.
+ *
+ * Use of this source code is governed by a BSD-3-clause license that can
+ * be found in the LICENSE.txt file or at
+ * https://opensource.org/licenses/BSD-3-Clause
+ */
+#include <toolkits/style_transfer/class_registrations.hpp>
+#include <toolkits/style_transfer/style_transfer.hpp>
+
+namespace turi {
+namespace style_transfer {
+
+BEGIN_CLASS_REGISTRATION
+REGISTER_CLASS(style_transfer)
+END_CLASS_REGISTRATION
+
+}  // namespace style_transfer
+}  // namespace turi
