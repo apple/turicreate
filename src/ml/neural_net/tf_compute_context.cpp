3b3bf42b197aa4c503d27c1b758d30e672563a0f
Shreya Jain <47830614+shreyajain17@users.noreply.github.com>
2019-10-25 17:47:47 -0700

    OD data augmentation done using TensorFlow (#2474)

--- src/ml/neural_net/tf_compute_context.cpp
+++ src/ml/neural_net/tf_compute_context.cpp
@@ -194,15 +194,14 @@ class tf_image_augmenter : public float_array_image_augmenter {
 
   float_array_result prepare_augmented_images(
       labeled_float_image data_to_augment) override;
-
- private:
-  options opts_;
 };
 
 tf_image_augmenter::tf_image_augmenter(const options& opts) : float_array_image_augmenter(opts) {}
 
-float_array_image_augmenter::float_array_result tf_image_augmenter::prepare_augmented_images(
+float_array_image_augmenter::float_array_result
+tf_image_augmenter::prepare_augmented_images(
     float_array_image_augmenter::labeled_float_image data_to_augment) {
+  options opts = get_options();
   float_array_image_augmenter::float_array_result image_annotations;
 
   call_pybind_function([&]() {
@@ -210,9 +209,19 @@ float_array_image_augmenter::float_array_result tf_image_augmenter::prepare_augm
     pybind11::module tf_aug = pybind11::module::import(
         "turicreate.toolkits.object_detector._tf_image_augmenter");
 
+    const size_t output_height = opts.output_height;
+    const size_t output_width = opts.output_width;
+
+    // TODO: Remove resize_only by passing all the augmentation options
+    bool resize_only = false;
+    if (opts.crop_prob == 0.f) {
+      resize_only = true;
+    }
+
     // Get augmented images and annotations from tensorflow
     pybind11::object augmented_data = tf_aug.attr("get_augmented_data")(
-        data_to_augment.images, data_to_augment.annotations);
+        data_to_augment.images, data_to_augment.annotations, output_height,
+        output_width, resize_only);
     std::pair<pybind11::buffer, std::vector<pybind11::buffer>> aug_data =
         augmented_data
             .cast<std::pair<pybind11::buffer, std::vector<pybind11::buffer>>>();
