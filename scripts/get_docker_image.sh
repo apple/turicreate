fffe05eba0e3e46cf1a0b4116acaa5dedff55bfa
Zach Nation <znation@apple.com>
2019-11-04 17:29:06 -0800

    Some progress: OpenSSL error.

--- scripts/get_docker_image.sh
+++ scripts/get_docker_image.sh
@@ -12,7 +12,8 @@ print_help() {
   echo
   echo "Usage: ./get_docker_image.sh --ubuntu=[version]"
   echo
-  echo "  --ubuntu=[version]       The base Ubuntu version (defaults to 10.04)"
+  echo "  --ubuntu=[version]       The base Ubuntu version, OR:"
+  echo "  --centos=[version]       The base CentOS version (defaults to 6)"
   echo
   echo "  --help                   Prints this help text."
   echo
@@ -24,20 +25,25 @@ print_help() {
 while [ $# -gt 0 ]
   do case $1 in
     --ubuntu=*)             UBUNTU_VERSION=${1##--ubuntu=} ;;
+    --centos=*)             CENTOS_VERSION=${1##--centos=} ;;
     --help)                 print_help ;;
     *) unknown_option $1 ;;
   esac
   shift
 done
 
-if [[ -z "${UBUNTU_VERSION}" ]]; then
-  UBUNTU_VERSION="10.04"
+if [[ -z "${UBUNTU_VERSION}" && -z "${CENTOS_VERSION}" ]]; then
+  CENTOS_VERSION="6"
 fi
 
 # The build image version that will be used for building
-TC_BUILD_IMAGE_VERSION="1.0.1"
+TC_BUILD_IMAGE_VERSION="1.0.2"
 
-# The base image name - using Gitlab CI registry
-TC_BUILD_IMAGE="registry.gitlab.com/zach_nation/turicreate/build-image-${UBUNTU_VERSION}:${TC_BUILD_IMAGE_VERSION}"
+if [[ -z "${CENTOS_VERSION}" ]]; then
+  # The base image name - using Gitlab CI registry
+  TC_BUILD_IMAGE="registry.gitlab.com/zach_nation/turicreate/build-image-${UBUNTU_VERSION}:${TC_BUILD_IMAGE_VERSION}"
+else
+  TC_BUILD_IMAGE="registry.gitlab.com/zach_nation/turicreate/build-image-centos-${CENTOS_VERSION}:${TC_BUILD_IMAGE_VERSION}"
+fi
 
 echo ${TC_BUILD_IMAGE}
