a747c3e7a4328b6c00556d414bfda5c3150f2f66
Nick Jong <njong@apple.com>
2019-11-06 08:05:28 -0800

    Provide TF alternative to MXNet data iterator during SC training (#2565)

--- src/python/turicreate/toolkits/sound_classifier/_mx_sound_classifier.py
+++ src/python/turicreate/toolkits/sound_classifier/_mx_sound_classifier.py
@@ -67,7 +67,7 @@ class MultiLayerPerceptronMXNetModel():
         data = mx.gluon.utils.split_and_load(data, ctx_list=self.ctx, batch_axis=0, even_split=False)
         outputs = [self.custom_NN(x).asnumpy() for x in data]
         soft_outputs = mx.nd.softmax(mx.nd.array(outputs[0]))
-        return soft_outputs
+        return soft_outputs.asnumpy()
 
     @staticmethod
     def _build_custom_neural_network(num_inputs, num_labels, layer_sizes):
