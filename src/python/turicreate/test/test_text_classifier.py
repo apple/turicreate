cb28dc2d53cb4a28b9dad30b3e5dbf7ed61a5c2a
Krishna <srikrishna_sridhar@apple.com>
2019-11-16 15:18:25 -0800

    Fixed text classifier.

--- src/python/turicreate/test/test_text_classifier.py
+++ src/python/turicreate/test/test_text_classifier.py
@@ -94,6 +94,13 @@ class TextClassifierTest(unittest.TestCase):
         filename = tempfile.mkstemp('bingo.mlmodel')[1]
         self.model.export_coreml(filename)
 
+        coreml_model = coremltools.models.MLModel(filename)
+        self.assertDictEqual({
+            'com.github.apple.turicreate.version': tc.__version__
+            }, dict(coreml_model.user_defined_metadata)
+        )
+        self.assertTrue('Text classifier created by Turi Create' in coreml_model.short_description)
+
     @unittest.skipIf(_mac_ver() < (10, 13), 'Only supported on macOS 10.13+')
     def test_export_coreml_with_predict(self):
         filename = tempfile.mkstemp('bingo.mlmodel')[1]
