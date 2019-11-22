apple/turicreate

commit e7ed04f119f8ccdefbf248f380769d3b4ef22461

Fix IC/DC annotation on Linux (#2715)
* Bump node-sass version for compat with Node 12.x

* Fix IC annotation on Linux

src/visualization/annotation/image_classification.cpp
@@ -284,8 +284,11 @@ void ImageClassification::_addAnnotationToSFrame(size_t index,
                                                  std::string label) {
   /* Assert that the column type is indeed of type flex_enum::STRING */
   size_t annotation_column_index = m_data->column_index(m_annotation_column);
-  DASSERT_EQ(m_data->dtype().at(annotation_column_index),
-             flex_type_enum::STRING);
+  flex_type_enum annotation_column_type = m_data->dtype().at(annotation_column_index);
+  if (m_data->select_column(annotation_column_index)->any()) {
+    DASSERT_EQ(annotation_column_type,
+              flex_type_enum::STRING);
+  }
 
   std::shared_ptr<unity_sarray> data_sarray =
       std::static_pointer_cast<unity_sarray>(

.gitattributes
@@ -1,3 +1,4 @@
 # Treat these paths as external for the purposes of GitHub's Linguist
 deps/* linguist-vendored
 src/external/* linguist-vendored
+src/visualization/client/Turi[[:space:]]Create[[:space:]]Visualization/src/user_interface/build/* linguist-generated

src/python/turicreate/toolkits/image_classifier/_annotate.py
@@ -119,10 +119,6 @@ def annotate(data, image_column=None, annotation_column='annotations'):
                             [image_column],
                             annotation_column
                         )
-    if __platform == "linux" or __platform == "linux2":
-        model = _image_feature_extractor._create_feature_extractor("squeezenet_v1.1")
-        feature_sframe = model.extract_features(data, image_column, verbose=False, batch_size=64)
-        annotation_window.add_image_features(feature_sframe)
 
     with _QuietProgress(False):
         annotation_window.annotate(_get_client_app_path())

src/visualization/client/Turi Create Visualization/src/user_interface/build/index.html
@@ -1,1 +1,1 @@
-<!doctype html><html lang="en"><head><meta charset="utf-8"><link rel="stylesheet" href="index.css"><meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no"><title>Turi Create Visualization</title><link href="./static/css/main.cd67978c.chunk.css" rel="stylesheet"></head><body><div id="progress_bar"></div><div id="vega_vis"></div><div id="hidden_cont"></div><div id="table_vis"></div><div id="annotate_viz"></div><div id="evaluation_vis"></div><div id="loading_container"><div class="preprocessing_data_key">Loading</div><div id="loading_screen"><div class="loader"></div></div></div><script type="text/javascript" src="index.js"></script><script>!function(l){function e(e){for(var r,t,n=e[0],o=e[1],u=e[2],f=0,i=[];f<n.length;f++)t=n[f],p[t]&&i.push(p[t][0]),p[t]=0;for(r in o)Object.prototype.hasOwnProperty.call(o,r)&&(l[r]=o[r]);for(s&&s(e);i.length;)i.shift()();return c.push.apply(c,u||[]),a()}function a(){for(var e,r=0;r<c.length;r++){for(var t=c[r],n=!0,o=1;o<t.length;o++){var u=t[o];0!==p[u]&&(n=!1)}n&&(c.splice(r--,1),e=f(f.s=t[0]))}return e}var t={},p={2:0},c=[];function f(e){if(t[e])return t[e].exports;var r=t[e]={i:e,l:!1,exports:{}};return l[e].call(r.exports,r,r.exports,f),r.l=!0,r.exports}f.m=l,f.c=t,f.d=function(e,r,t){f.o(e,r)||Object.defineProperty(e,r,{enumerable:!0,get:t})},f.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},f.t=function(r,e){if(1&e&&(r=f(r)),8&e)return r;if(4&e&&"object"==typeof r&&r&&r.__esModule)return r;var t=Object.create(null);if(f.r(t),Object.defineProperty(t,"default",{enumerable:!0,value:r}),2&e&&"string"!=typeof r)for(var n in r)f.d(t,n,function(e){return r[e]}.bind(null,n));return t},f.n=function(e){var r=e&&e.__esModule?function(){return e.default}:function(){return e};return f.d(r,"a",r),r},f.o=function(e,r){return Object.prototype.hasOwnProperty.call(e,r)},f.p="./";var r=window.webpackJsonp=window.webpackJsonp||[],n=r.push.bind(r);r.push=e,r=r.slice();for(var o=0;o<r.length;o++)e(r[o]);var s=n;a()}([])</script><script src="./static/js/1.7de6fc65.chunk.js"></script><script src="./static/js/main.017072fa.chunk.js"></script></body></html>
+<!doctype html><html lang="en"><head><meta charset="utf-8"><link rel="stylesheet" href="index.css"><meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no"><title>Turi Create Visualization</title><link href="./static/css/main.a7482690.chunk.css" rel="stylesheet"></head><body><div id="progress_bar"></div><div id="vega_vis"></div><div id="hidden_cont"></div><div id="table_vis"></div><div id="annotate_viz"></div><div id="evaluation_vis"></div><div id="loading_container"><div class="preprocessing_data_key">Loading</div><div id="loading_screen"><div class="loader"></div></div></div><script type="text/javascript" src="index.js"></script><script>!function(l){function e(e){for(var r,t,n=e[0],o=e[1],u=e[2],f=0,i=[];f<n.length;f++)t=n[f],p[t]&&i.push(p[t][0]),p[t]=0;for(r in o)Object.prototype.hasOwnProperty.call(o,r)&&(l[r]=o[r]);for(s&&s(e);i.length;)i.shift()();return c.push.apply(c,u||[]),a()}function a(){for(var e,r=0;r<c.length;r++){for(var t=c[r],n=!0,o=1;o<t.length;o++){var u=t[o];0!==p[u]&&(n=!1)}n&&(c.splice(r--,1),e=f(f.s=t[0]))}return e}var t={},p={2:0},c=[];function f(e){if(t[e])return t[e].exports;var r=t[e]={i:e,l:!1,exports:{}};return l[e].call(r.exports,r,r.exports,f),r.l=!0,r.exports}f.m=l,f.c=t,f.d=function(e,r,t){f.o(e,r)||Object.defineProperty(e,r,{enumerable:!0,get:t})},f.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},f.t=function(r,e){if(1&e&&(r=f(r)),8&e)return r;if(4&e&&"object"==typeof r&&r&&r.__esModule)return r;var t=Object.create(null);if(f.r(t),Object.defineProperty(t,"default",{enumerable:!0,value:r}),2&e&&"string"!=typeof r)for(var n in r)f.d(t,n,function(e){return r[e]}.bind(null,n));return t},f.n=function(e){var r=e&&e.__esModule?function(){return e.default}:function(){return e};return f.d(r,"a",r),r},f.o=function(e,r){return Object.prototype.hasOwnProperty.call(e,r)},f.p="./";var r=window.webpackJsonp=window.webpackJsonp||[],n=r.push.bind(r);r.push=e,r=r.slice();for(var o=0;o<r.length;o++)e(r[o]);var s=n;a()}([])</script><script src="./static/js/1.59710461.chunk.js"></script><script src="./static/js/main.7f054075.chunk.js"></script></body></html>

src/visualization/client/Turi Create Visualization/src/user_interface/build/precache-manifest.c66015a7f5bf66b84f33da94524faacd.js
@@ -1,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f054075d1d3ebc12e86",
+    "url": "./static/css/main.a7482690.chunk.css"
+  },
+  {
+    "revision": "7f054075d1d3ebc12e86",
+    "url": "./static/js/main.7f054075.chunk.js"
+  },
+  {
+    "revision": "59710461e66cff8c7760",
+    "url": "./static/js/1.59710461.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "ed2cbc77f143936ad0e849324c7fa709",
+    "url": "./static/media/caret-down.ed2cbc77.svg"
+  },
+  {
+    "revision": "b73df5f85c10ac87fb30563f2ccd915a",
+    "url": "./static/media/cancel.b73df5f8.svg"
+  },
+  {
+    "revision": "6be96c8ea9fb5e5b767969677211c34f",
+    "url": "./static/media/filter.6be96c8e.svg"
+  },
+  {
+    "revision": "d006a3aebac9dcb832cf0bb0e45e99e5",
+    "url": "./static/media/carret.d006a3ae.svg"
+  },
+  {
+    "revision": "eb907d772ed07ec0384ea2a5eec0c888",
+    "url": "./static/media/info.eb907d77.svg"
+  },
+  {
+    "revision": "1a77a1d255c565ab88edfe16c338a813",
+    "url": "./static/media/warning.1a77a1d2.svg"
+  },
+  {
+    "revision": "02e4572e260edda6eb7268bab3598722",
+    "url": "./static/media/down.02e4572e.svg"
+  },
+  {
+    "revision": "2104bcc98c328514318953ecdc662d85",
+    "url": "./static/media/down.2104bcc9.svg"
+  },
+  {
+    "revision": "c28fad4b6aa93a7a3a584d2fb57a674b",
+    "url": "./static/media/swap.c28fad4b.svg"
+  },
+  {
+    "revision": "fb9494c38741e9748395277818c8c58b",
+    "url": "./static/media/left_arrow.fb9494c3.svg"
+  },
+  {
+    "revision": "ab264aa77df9fea4c345d32085af3d61",
+    "url": "./static/media/right_arrow.ab264aa7.svg"
+  },
+  {
+    "revision": "78f3f4cdc3136d2b9a46043f8f621b8f",
+    "url": "./static/media/error.78f3f4cd.svg"
+  },
+  {
+    "revision": "d0a6cd866a92357556a64b92a9e82c2a",
+    "url": "./index.html"
+  }
+];

src/visualization/client/Turi Create Visualization/src/user_interface/build/precache-manifest.ef2041777a534687641af8211747bdd4.js
@@ -1,70 +1,0 @@
-self.__precacheManifest = [
-  {
-    "revision": "eb907d772ed07ec0384ea2a5eec0c888",
-    "url": "./static/media/info.eb907d77.svg"
-  },
-  {
-    "revision": "017072fa82e6596ddcfe",
-    "url": "./static/css/main.cd67978c.chunk.css"
-  },
-  {
-    "revision": "7de6fc658db969b75dfd",
-    "url": "./static/js/1.7de6fc65.chunk.js"
-  },
-  {
-    "revision": "4a686d48d5a089750c49",
-    "url": "./static/js/runtime~main.4a686d48.js"
-  },
-  {
-    "revision": "ed2cbc77f143936ad0e849324c7fa709",
-    "url": "./static/media/caret-down.ed2cbc77.svg"
-  },
-  {
-    "revision": "b73df5f85c10ac87fb30563f2ccd915a",
-    "url": "./static/media/cancel.b73df5f8.svg"
-  },
-  {
-    "revision": "6be96c8ea9fb5e5b767969677211c34f",
-    "url": "./static/media/filter.6be96c8e.svg"
-  },
-  {
-    "revision": "d006a3aebac9dcb832cf0bb0e45e99e5",
-    "url": "./static/media/carret.d006a3ae.svg"
-  },
-  {
-    "revision": "017072fa82e6596ddcfe",
-    "url": "./static/js/main.017072fa.chunk.js"
-  },
-  {
-    "revision": "1a77a1d255c565ab88edfe16c338a813",
-    "url": "./static/media/warning.1a77a1d2.svg"
-  },
-  {
-    "revision": "02e4572e260edda6eb7268bab3598722",
-    "url": "./static/media/down.02e4572e.svg"
-  },
-  {
-    "revision": "2104bcc98c328514318953ecdc662d85",
-    "url": "./static/media/down.2104bcc9.svg"
-  },
-  {
-    "revision": "c28fad4b6aa93a7a3a584d2fb57a674b",
-    "url": "./static/media/swap.c28fad4b.svg"
-  },
-  {
-    "revision": "fb9494c38741e9748395277818c8c58b",
-    "url": "./static/media/left_arrow.fb9494c3.svg"
-  },
-  {
-    "revision": "ab264aa77df9fea4c345d32085af3d61",
-    "url": "./static/media/right_arrow.ab264aa7.svg"
-  },
-  {
-    "revision": "78f3f4cdc3136d2b9a46043f8f621b8f",
-    "url": "./static/media/error.78f3f4cd.svg"
-  },
-  {
-    "revision": "e6173020ce028331ae2ff41db5a0983d",
-    "url": "./index.html"
-  }
-];

src/visualization/client/Turi Create Visualization/src/user_interface/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.ef2041777a534687641af8211747bdd4.js"
+  "./precache-manifest.c66015a7f5bf66b84f33da94524faacd.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("./index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });

commit 51a49adddae4f39074231063fbf79b4c5dc6b6ea

Fix build in release mode
Break caused by #2715 -- in release mode, the unused variable causes a
warning-as-error.

src/visualization/annotation/image_classification.cpp
@@ -284,9 +284,8 @@ void ImageClassification::_addAnnotationToSFrame(size_t index,
                                                  std::string label) {
   /* Assert that the column type is indeed of type flex_enum::STRING */
   size_t annotation_column_index = m_data->column_index(m_annotation_column);
-  flex_type_enum annotation_column_type = m_data->dtype().at(annotation_column_index);
   if (m_data->select_column(annotation_column_index)->any()) {
-    DASSERT_EQ(annotation_column_type,
+    DASSERT_EQ(m_data->dtype().at(annotation_column_index),
               flex_type_enum::STRING);
   }
 