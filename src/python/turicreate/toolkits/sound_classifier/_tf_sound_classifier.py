4ffa5e456e21559506cdc20672d5bd7e9d5164dc
Fareeha Irfan <fareeha@apple.com>
2019-11-22 15:22:33 -0800

    removed unneeded imports

--- src/python/turicreate/toolkits/sound_classifier/_tf_sound_classifier.py
+++ src/python/turicreate/toolkits/sound_classifier/_tf_sound_classifier.py
@@ -6,12 +6,7 @@
 from __future__ import print_function as _
 from __future__ import division as _
 from __future__ import absolute_import as _
-from turicreate.util import _ProgressTablePrinter
-import tensorflow as _tf
-import numpy as _np
-import time as _time
 from .._tf_model import TensorFlowModel
-import turicreate.toolkits._tf_utils as _utils
 
 import tensorflow.compat.v1 as _tf
 _tf.disable_v2_behavior()
