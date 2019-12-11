7d065c54b9f55eeb1ecf5c57953c358de9a4e3b7
Henry <henry@Henrys-MacBook-Pro-2.local>
2019-12-10 17:07:17 -0800

    od missing annotation label

--- src/toolkits/object_detection/od_data_iterator.cpp
+++ src/toolkits/object_detection/od_data_iterator.cpp
@@ -104,7 +104,7 @@ std::vector<image_annotation> parse_annotations(
       if (key == "label") {
         // If the label is invalid (not in class_to_index_map) then ignore it.
         if (kv.second.get_type() == flex_type_enum::UNDEFINED) {
-          log_and_throw("Annotation label is missing.");
+          log_and_throw("All annotations must contain label information.");
         }
 
         const flex_string& label = kv.second.get<flex_string>();
