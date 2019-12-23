b82e329f099a577b68ddcc86d3905dd6cae50ef7
Henry Tao <55294647+jakesabathia2@users.noreply.github.com>
2019-12-23 10:10:07 -0800

    Invalid summary input (#2872)

--- src/python/turicreate/test/test_image_similarity.py
+++ src/python/turicreate/test/test_image_similarity.py
@@ -174,6 +174,25 @@ class ImageSimilarityTest(unittest.TestCase):
         model = self.model
         model.summary()
 
+    def test_summary_str(self):
+        model = self.model
+        self.assertTrue(isinstance(model.summary('str'), str))
+
+    def test_summary_dict(self):
+        model = self.model
+        self.assertTrue(isinstance(model.summary('dict'), dict))
+
+    def test_summary_invalid_input(self):
+        model = self.model
+        with self.assertRaises(_ToolkitError):
+            model.summary(model.summary('invalid'))
+
+        with self.assertRaises(_ToolkitError):
+            model.summary(model.summary(0))
+
+        with self.assertRaises(_ToolkitError):
+            model.summary(model.summary({}))
+
     def test_repr(self):
         # Repr after fit
         model = self.model
