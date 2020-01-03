1d03d768dba8450b3be04c68e7391f2d4a49ec70
Toby Roseman <troseman@apple.com>
2020-01-02 16:12:16 -0800

    Refactor Style Transfer Unit Test (#2851)

--- src/python/turicreate/test/test_style_transfer.py
+++ src/python/turicreate/test/test_style_transfer.py
@@ -334,9 +334,8 @@ class StyleTransferTest(unittest.TestCase):
 class StyleTransferGPUTest(unittest.TestCase):
     @classmethod
     def setUpClass(self):
-        self.style_feature = self.content_feature = 'image'
-        self.style_sf = _get_data(feature=self.style_feature)
-        self.content_sf = _get_data(feature=self.content_feature)
+        self.style_sf = _get_data('image')
+        self.content_sf = _get_data('image')
 
 
     def test_gpu_save_load_export(self):
@@ -344,12 +343,14 @@ class StyleTransferGPUTest(unittest.TestCase):
         gpu_options = set([old_num_gpus, 0, 1])
         for in_gpus in gpu_options:
             tc.config.set_num_gpus(in_gpus)
-            model = tc.style_transfer.create(self.style_sf, self.content_sf, max_iterations=1)
+            original_model = tc.style_transfer.create(self.style_sf, self.content_sf, max_iterations=1)
             for out_gpus in gpu_options:
                 with test_util.TempDirectory() as path:
-                    model.save(path)
+                    original_model.save(path)
                     tc.config.set_num_gpus(out_gpus)
                     model = tc.load_model(path)
-                    model.export_coreml(os.path.join(path, 'model.mlmodel'))
+
+                    with test_util.TempDirectory() as export_path:
+                        model.export_coreml(os.path.join(export_path, 'model.mlmodel'))
 
         tc.config.set_num_gpus(old_num_gpus)
