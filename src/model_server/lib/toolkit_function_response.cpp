7ac0651be60c295a913558981c9979611c330fa6
Hoyt Koepke <hoytak@apple.com>
2020-01-15 15:20:53 -0800

    Addressed feedback for comments.

--- src/model_server/lib/toolkit_function_response.cpp
+++ src/model_server/lib/toolkit_function_response.cpp
@@ -3,13 +3,17 @@
 
 namespace turi {
 
-toolkit_function_response_future::toolkit_function_response_future(std::function<toolkit_function_response_type()> exec_function) :
-  m_info(new response_info) {
+toolkit_function_response_future::toolkit_function_response_future(
+    std::function<toolkit_function_response_type()> exec_function) 
+  : m_info(new response_info) {
 
-    auto captured_info = this->m_info; 
+  // Capture the m_info by value.  Capturing `this` could cause segfaults,
+  // as it may not be valid after a copy.  Capturing a shared pointer to
+  // the info by value, 
+  auto captured_info = this->m_info; 
 
-m_info->response_future = std::async(std::launch::async, 
-  [=]() {
+  m_info->response_future = std::async(std::launch::async, 
+   [=]() {
      captured_info->response = exec_function();
      captured_info->is_completed = true;
      return true;
@@ -21,6 +25,7 @@ m_info->response_future = std::async(std::launch::async,
 const toolkit_function_response_type& toolkit_function_response_future::wait() const {
   ASSERT_TRUE(m_info != nullptr); 
 
+  // The response future is invalid 
   if(!m_info->is_completed) { 
      ASSERT_TRUE(m_info->response_future.valid()); 
      m_info->response_future.wait(); 
