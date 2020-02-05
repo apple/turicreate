9524fe3cf6cd3c70851fc0733561728739a630d3
guihao_liang <guihao_liang@apple.com>
2020-02-05 14:25:46 -0800

    prepare for merge

--- scripts/run_python_test.sh
+++ scripts/run_python_test.sh
@@ -33,11 +33,14 @@ push_ld_library_path() {
 pop_ld_library_path() {
   export LD_LIBRARY_PATH=$OLD_LIBRARY_PATH
 }
+
 push_ld_library_path
 cd "$TURI_BUILD_ROOT"/src/python
 make -j4
 pop_ld_library_path
 
+# back to WORKSPACE to use default .coveragerc
+cd "$WORKSPACE"
 find . -name "*.xml" -delete
 if ! type "parallel" 2> /dev/null; then
         cmd=""
