5baeb2db06b7887b42a1e29b765ac37b1ffc77bc
guihao_liang <guihao_liang@apple.com>
2020-02-11 14:34:38 -0800

    backwards compat

--- src/core/parallel/lambda_omp.cpp
+++ src/core/parallel/lambda_omp.cpp
@@ -4,6 +4,7 @@
  * be found in the LICENSE.txt file or at
  * https://opensource.org/licenses/BSD-3-Clause
  */
+
 #if defined(_OPENMP) && !defined(DISABLE_OPENMP)
 #include <omp.h>
 #endif
@@ -13,14 +14,35 @@ namespace turi {
 
 void in_parallel(
     const std::function<void(size_t thread_id, size_t num_threads)>& fn) {
+#if defined(_OPENMP) && !defined(DISABLE_OPENMP)
+
 #pragma omp parallel
   {
     size_t nworkers = omp_get_num_threads();
+
 #pragma omp parallel for
     for (size_t ii = 0; ii < nworkers; ii++) {
       fn(ii, nworkers);
     }
   }
+
+#else
+
+  size_t nworkers = thread_pool::get_instance().size();
+
+  if (thread::get_tls_data().is_in_thread() || nworkers <= 1) {
+    fn(0, 1);
+    return;
+
+  } else {
+    parallel_task_queue threads(thread_pool::get_instance());
+
+    for (unsigned int i = 0; i < nworkers; ++i) {
+      threads.launch([&fn, i, nworkers]() { fn(i, nworkers); }, i);
+    }
+    threads.join();
+  }
+#endif
 }
 
 };  // namespace turi
