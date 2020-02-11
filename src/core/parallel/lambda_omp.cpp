f29264995cb3b5718358f0305ebb8afbf71bd7d2
guihao_liang <guihao_liang@apple.com>
2020-02-11 14:21:06 -0800

    make it happen

--- src/core/parallel/lambda_omp.cpp
+++ src/core/parallel/lambda_omp.cpp
@@ -1,4 +1,12 @@
+/* Copyright © 2017 Apple Inc. All rights reserved.
+ *
+ * Use of this source code is governed by a BSD-3-clause license that can
+ * be found in the LICENSE.txt file or at
+ * https://opensource.org/licenses/BSD-3-Clause
+ */
+#if defined(_OPENMP) && !defined(DISABLE_OPENMP)
 #include <omp.h>
+#endif
 
 #include <core/parallel/lambda_omp.hpp>
 namespace turi {
