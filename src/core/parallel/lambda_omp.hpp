be858d5ff30c72a46e3ec635fb5948b921a18681
guihao_liang <guihao_liang@apple.com>
2020-02-14 15:17:34 -0800

    not do that for lambda, too much work

--- src/core/parallel/lambda_omp.hpp
+++ src/core/parallel/lambda_omp.hpp
@@ -1,18 +1,17 @@
 /* Copyright © 2017 Apple Inc. All rights reserved.
  *
  * Use of this source code is governed by a BSD-3-clause license that can
- * be found in the LICENSE.txt file or at
- * https://opensource.org/licenses/BSD-3-Clause
+ * be found in the LICENSE.txt file or at https://opensource.org/licenses/BSD-3-Clause
  */
 #ifndef TURI_PARALLEL_LAMBDA_OMP_HPP
 #define TURI_PARALLEL_LAMBDA_OMP_HPP
-
-#include <core/parallel/thread_pool.hpp>
-#include <core/util/basic_types.hpp>
-#include <functional>
 #include <iterator>
-#include <type_traits>
 #include <utility>
+#include <functional>
+#include <type_traits>
+
+#include <core/util/basic_types.hpp>
+#include <core/parallel/thread_pool.hpp>
 
 namespace turi {
 
@@ -33,8 +32,25 @@ namespace turi {
  * \param fn The function to run. The function must take two size_t arguments:
  *           the thread ID and the number of threads.
  */
-void in_parallel(
-    const std::function<void(size_t thread_id, size_t num_threads)>& fn);
+inline void in_parallel(const std::function<void (size_t thread_id,
+                                                  size_t num_threads)>& fn) {
+  size_t nworkers = thread_pool::get_instance().size();
+
+  if (thread::get_tls_data().is_in_thread() || nworkers <= 1) {
+
+    fn(0, 1);
+    return;
+
+  } else {
+
+    parallel_task_queue threads(thread_pool::get_instance());
+
+    for (unsigned int i = 0;i < nworkers; ++i) {
+      threads.launch([&fn, i, nworkers]() { fn(i, nworkers); }, i);
+    }
+    threads.join();
+  }
+}
 
 /**
  * Returns the thread pool dedicated for running parallel for jobs.
@@ -74,38 +90,37 @@ thread_pool& get_parfor_thread_pool();
  *           argument which is a current index.
  */
 template <typename FunctionType>
-void parallel_for(size_t begin, size_t end, const FunctionType& fn) {
+void parallel_for(size_t begin,
+                  size_t end,
+                  const FunctionType& fn) {
+
   size_t nworkers = thread_pool::get_instance().size();
 
   if (thread::get_tls_data().is_in_thread() || nworkers <= 1) {
     // we do not support recursive calls to in parallel yet.
-    for (size_t i = begin; i < end; ++i) {
+    for(size_t i = begin; i < end; ++i) {
       fn(i);
     }
   } else {
     parallel_task_queue threads(thread_pool::get_instance());
-    size_t nlen = end - begin;  // total range
-    double split_size =
-        (double)nlen / nworkers;  // size of range each worker gets
+    size_t nlen = end - begin; // total range
+    double split_size = (double)nlen / nworkers; // size of range each worker gets
     for (size_t i = 0; i < nworkers; ++i) {
-      size_t worker_begin =
-          begin + split_size * i;  // beginning of this worker's range
-      size_t worker_end =
-          begin + split_size * (i + 1);  // end of this worker's range
+      size_t worker_begin = begin + split_size * i; // beginning of this worker's range
+      size_t worker_end = begin + split_size * (i + 1); // end of this worker's range
       if (i == nworkers - 1) worker_end = end;
-      threads.launch(
-          [&fn, worker_begin, worker_end]() {
-            size_t worker_iter = worker_begin;
-            while (worker_iter < worker_end) {
-              fn(worker_iter);
-              ++worker_iter;
-            }
-          },
-          i);
+      threads.launch([&fn, worker_begin, worker_end]() {
+        size_t worker_iter = worker_begin;
+        while (worker_iter < worker_end) {
+          fn(worker_iter);
+          ++worker_iter;
+        }
+      }, i);
     }
     threads.join();
   }
 }
+
 
 /**
  * Runs a map reduce operation for ranging from the integers 'begin' to 'end'.
@@ -140,40 +155,37 @@ void parallel_for(size_t begin, size_t end, const FunctionType& fn) {
  *           argument which is a current index.
  */
 template <typename FunctionType, typename ReduceType>
-ReduceType fold_reduce(size_t begin, size_t end, const FunctionType& fn,
-                       ReduceType base = ReduceType()) {
+ReduceType fold_reduce (size_t begin,
+                  size_t end,
+                  const FunctionType& fn,
+                  ReduceType base = ReduceType()) {
   size_t nworkers = thread_pool::get_instance().size();
 
   if (thread::get_tls_data().is_in_thread() || nworkers <= 1) {
     // we do not support recursive calls to in parallel yet.
     ReduceType acc = base;
-    for (size_t i = begin; i < end; ++i) {
+    for(size_t i = begin; i < end; ++i) {
       fn(i, acc);
     }
     return acc;
   } else {
     parallel_task_queue threads(thread_pool::get_instance());
 
-    size_t nlen = end - begin;  // total rangeS
-    double split_size =
-        (double)nlen / nworkers;  // size of range each worker gets
+    size_t nlen = end - begin; // total rangeS
+    double split_size = (double)nlen / nworkers; // size of range each worker gets
 
     std::vector<ReduceType> acc(nworkers, base);
-    for (size_t i = 0; i < nworkers; ++i) {
-      size_t worker_begin =
-          begin + split_size * i;  // beginning of this worker's range
-      size_t worker_end =
-          begin + split_size * (i + 1);  // end of this worker's range
+    for (size_t i = 0;i < nworkers; ++i) {
+      size_t worker_begin = begin + split_size * i; // beginning of this worker's range
+      size_t worker_end = begin + split_size * (i + 1); // end of this worker's range
       if (i == nworkers - 1) worker_end = end;
-      threads.launch(
-          [&fn, &acc, worker_begin, worker_end, i]() {
-            size_t worker_iter = worker_begin;
-            while (worker_iter < worker_end) {
-              fn(worker_iter, acc[i]);
-              ++worker_iter;
-            }
-          },
-          i);
+      threads.launch([&fn, &acc, worker_begin, worker_end, i]() {
+                       size_t worker_iter = worker_begin;
+                       while (worker_iter < worker_end) {
+                         fn(worker_iter, acc[i]);
+                         ++worker_iter;
+                       }
+                     }, i);
     }
     threads.join();
     ReduceType ret = base;
@@ -214,11 +226,11 @@ ReduceType fold_reduce(size_t begin, size_t end, const FunctionType& fn,
  *           argument which is a current index.
  */
 template <typename RandomAccessIterator, typename FunctionType>
-inline void parallel_for(
-    RandomAccessIterator iter_begin, RandomAccessIterator iter_end,
-    const FunctionType& fn,
-    std::random_access_iterator_tag = typename std::iterator_traits<
-        RandomAccessIterator>::iterator_category()) {
+inline void parallel_for(RandomAccessIterator iter_begin,
+                         RandomAccessIterator iter_end,
+                         const FunctionType& fn,
+                         std::random_access_iterator_tag = typename std::iterator_traits<RandomAccessIterator>::iterator_category()) {
+
   size_t nworkers = thread_pool::get_instance().size();
 
   if (thread::get_tls_data().is_in_thread() || nworkers <= 1) {
@@ -230,28 +242,29 @@ inline void parallel_for(
   } else {
     parallel_task_queue threads(thread_pool::get_instance());
 
-    size_t nlen = std::distance(iter_begin, iter_end);  // number of elements
+    size_t nlen = std::distance(iter_begin, iter_end); // number of elements
 
-    double split_size =
-        (double)nlen / nworkers;  // number of elements per worker
+    double split_size = (double)nlen / nworkers; // number of elements per worker
 
-    for (size_t i = 0; i < nworkers; ++i) {
-      size_t worker_begin = split_size * i;      // index this worker starts at
-      size_t worker_end = split_size * (i + 1);  // index this worker ends at
+    for (size_t i = 0;i < nworkers; ++i) {
+      size_t worker_begin = split_size * i;    // index this worker starts at
+      size_t worker_end = split_size * (i + 1); // index this worker ends at
       if (i == nworkers - 1) worker_end = nlen;
-      threads.launch([&fn, worker_begin, worker_end, &iter_begin]() {
-        RandomAccessIterator my_begin = iter_begin + worker_begin;
-        RandomAccessIterator my_end = iter_begin + worker_end;
-        while (my_begin != my_end) {
-          fn(*my_begin);
-          ++my_begin;
-        }
-      });
+      threads.launch(
+          [&fn, worker_begin, worker_end, &iter_begin]() {
+            RandomAccessIterator my_begin = iter_begin + worker_begin;
+            RandomAccessIterator my_end = iter_begin + worker_end;
+            while (my_begin != my_end) {
+              fn(*my_begin);
+              ++my_begin;
+            }
+          } );
     }
     threads.join();
   }
 }
 
-};  // namespace turi
+
+};
 
 #endif
