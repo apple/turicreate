b6be9404a105dccaee8529fde51e6094d8928d9a
Guihao Liang <guihao_liang@apple.com>
2020-03-26 16:33:08 -0700

    put back the s3 url sanitize test (#3070)
    * add sanitize test again
    
    * fix the extra bug; should not discard any group delimiter

--- test/fileio/sanitize_url_test.cxx
+++ test/fileio/sanitize_url_test.cxx
@@ -1,27 +1,40 @@
 #define BOOST_TEST_MODULE
 #include <boost/test/unit_test.hpp>
-#include <core/util/test_macros.hpp>
 #include <core/storage/fileio/sanitize_url.hpp>
+#include <core/util/test_macros.hpp>
 
 using namespace turi;
 
 struct sanitize_url_test {
  public:
   void test_sanitize_url(void) {
-    TS_ASSERT_EQUALS(sanitize_url("http://www.google.com"), "http://www.google.com");
-    TS_ASSERT_EQUALS(sanitize_url("file://www.google.com"), "file://www.google.com");
-    TS_ASSERT_EQUALS(sanitize_url("hdfs://hello:world@www.google.com"), "hdfs://hello:world@www.google.com");
+    TS_ASSERT_EQUALS(sanitize_url("http://www.google.com"),
+                     "http://www.google.com");
+    TS_ASSERT_EQUALS(sanitize_url("file://www.google.com"),
+                     "file://www.google.com");
+    TS_ASSERT_EQUALS(sanitize_url("hdfs://hello:world@www.google.com"),
+                     "hdfs://hello:world@www.google.com");
     TS_ASSERT_EQUALS(sanitize_url("s3://aa:pika/chu"), "s3://pika/chu");
     TS_ASSERT_EQUALS(sanitize_url("s3://aa:bb:pika/chu"), "s3://pika/chu");
-    TS_ASSERT_EQUALS(sanitize_url("s3://aa:bb:s3.amazonaws.com/pika/chu"), "s3://s3.amazonaws.com/pika/chu");
+
+    // with extra token separators
+    // since s3 is not like the UNIX director that redundant '/' will
+    // be removed.
+    TS_ASSERT_EQUALS(sanitize_url("s3://aa:bb:pika/chu//"), "s3://pika/chu//");
+    TS_ASSERT_EQUALS(sanitize_url("s3://aa:bb:s3.amazonaws.com/pika/chu"),
+                     "s3://s3.amazonaws.com/pika/chu");
     TS_ASSERT_EQUALS(sanitize_url("s3://a/a:bb:cc:pika/chu"), "s3://pika/chu");
-    TS_ASSERT_EQUALS(sanitize_url("s3://a/a:bb:cc:s3.amazonaws.com/pika/chu"), "s3://s3.amazonaws.com/pika/chu");
+    TS_ASSERT_EQUALS(sanitize_url("s3://a/a:bb:cc:s3.amazonaws.com/pika/chu"),
+                     "s3://s3.amazonaws.com/pika/chu");
     TS_ASSERT_EQUALS(sanitize_url("s3://a/a:b/b:cc:pika/chu"), "s3://pika/chu");
-    TS_ASSERT_EQUALS(sanitize_url("s3://a/a:b/b:cc:s3.amazonaws.com/pika/chu"), "s3://s3.amazonaws.com/pika/chu");
+    TS_ASSERT_EQUALS(sanitize_url("s3://a/a:b/b:cc:s3.amazonaws.com/pika/chu"),
+                     "s3://s3.amazonaws.com/pika/chu");
     TS_ASSERT_EQUALS(sanitize_url("s3://:pika/chu"), "s3://pika/chu");
-    TS_ASSERT_EQUALS(sanitize_url("s3://:s3.amazonaws.com/pika/chu"), "s3://s3.amazonaws.com/pika/chu");
+    TS_ASSERT_EQUALS(sanitize_url("s3://:s3.amazonaws.com/pika/chu"),
+                     "s3://s3.amazonaws.com/pika/chu");
     TS_ASSERT_EQUALS(sanitize_url("s3://:::pika/chu"), "s3://pika/chu");
-    TS_ASSERT_EQUALS(sanitize_url("s3://:::s3.amazonaws.com/pika/chu"), "s3://s3.amazonaws.com/pika/chu");
+    TS_ASSERT_EQUALS(sanitize_url("s3://:::s3.amazonaws.com/pika/chu"),
+                     "s3://s3.amazonaws.com/pika/chu");
   }
 };
 
