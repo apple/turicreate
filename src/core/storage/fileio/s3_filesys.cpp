80727bea51043db3dd6a6ff71c4948408a10a074
Guihao Liang <guihao_liang@apple.com>
2020-04-09 16:22:37 -0700

    fix SArray upload issue (#3106)
    * force upload empty files

--- src/core/storage/fileio/s3_filesys.cpp
+++ src/core/storage/fileio/s3_filesys.cpp
@@ -133,8 +133,7 @@ void WriteStream::Write(const void *ptr, size_t size) {
 }
 
 void WriteStream::Upload(bool force_upload) {
-  if (buffer_.empty()) return;
-
+  // empty file needs to be force uploaded. objects.bin can be 0 bytes.
   if (!force_upload && buffer_.size() < max_buffer_size_) {
     return;
   }
