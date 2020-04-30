ef0a5090180c32653aca2428aaaf56b6b07e4c87
Toby Roseman <troseman@apple.com>
2020-04-30 14:41:54 -0700

    Fix TensorFlow object detector evaluate always returning zero accuracy #3148

--- src/python/turicreate/toolkits/object_detector/_tf_image_augmenter.py
+++ src/python/turicreate/toolkits/object_detector/_tf_image_augmenter.py
@@ -566,17 +566,14 @@ class DataAugmenter(object):
             feed_dict = dict()
 
             # Populate feed_dict with images and annotations
-            graph_op = self.resize_op_batch[0 : len(images)]
-            for i in range(0, len(images)):
+            graph_op = self.resize_op_batch[0:len(images)]
+            for i in range(len(images)):
                 feed_dict[self.img_tf[i]] = _utils.convert_shared_float_array_to_numpy(
                     images[i]
                 )
-                if self.resize_only:
-                    feed_dict[self.ann_tf[i]] = self.batch_size * [np.zeros(6)]
-                else:
-                    feed_dict[
-                        self.ann_tf[i]
-                    ] = _utils.convert_shared_float_array_to_numpy(annotations[i])
+                feed_dict[
+                    self.ann_tf[i]
+                ] = _utils.convert_shared_float_array_to_numpy(annotations[i])
 
             # Populate feed_dict with random seed and random alpha values, used
             # to sample image perturbations. We don't use TensorFlow's built-in
