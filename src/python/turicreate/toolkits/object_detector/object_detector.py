89fd875fa5ebd6a1206fcb60818c9666b72d3aeb
Toby Roseman <troseman@apple.com>
2020-05-04 12:02:07 -0700

    Remove _seconds_as_string(..). Defined twice but never used.

--- src/python/turicreate/toolkits/object_detector/object_detector.py
+++ src/python/turicreate/toolkits/object_detector/object_detector.py
@@ -58,25 +58,6 @@ def _get_mps_od_net(
     return network
 
 
-# Standard lib functions would be great here, but the formatting options of
-# timedelta are not great
-def _seconds_as_string(seconds):
-    """
-    Returns seconds as a human-friendly string, e.g. '1d 4h 47m 41s'
-    """
-    TIME_UNITS = [("s", 60), ("m", 60), ("h", 24), ("d", None)]
-    unit_strings = []
-    cur = max(int(seconds), 1)
-    for suffix, size in TIME_UNITS:
-        if size is not None:
-            cur, rest = divmod(cur, size)
-        else:
-            rest = cur
-        if rest > 0:
-            unit_strings.insert(0, "%d%s" % (rest, suffix))
-    return " ".join(unit_strings)
-
-
 def _raise_error_if_not_detection_sframe(
     dataset, feature, annotations, require_annotations
 ):
