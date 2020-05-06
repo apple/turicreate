67216b54d00a0c0e4da3012787f56c8b0ca0136f
Neeraj Komuravalli <komunee2710@gmail.com>
2020-05-06 10:20:30 +0530

    Adding get_deep_features module to extract features and adding more two support functions (#3126)

--- src/python/turicreate/toolkits/image_analysis/image_analysis.py
+++ src/python/turicreate/toolkits/image_analysis/image_analysis.py
@@ -6,8 +6,20 @@
 from __future__ import print_function as _
 from __future__ import division as _
 from __future__ import absolute_import as _
-from ...data_structures.image import Image as _Image
 
+import turicreate as _tc
+
+import turicreate.toolkits._internal_utils as _tkutl
+from .._internal_utils import _mac_ver
+from .. import _pre_trained_models
+from ...data_structures.image import Image as _Image
+from .. import _image_feature_extractor
+
+MODEL_TO_FEATURE_SIZE_MAPPING = {
+    "resnet-50": 2048,
+    "squeezenet_v1.1": 1000,
+    "VisionFeaturePrint_Scene": 2048
+}
 
 def load_images(
     url,
@@ -177,3 +189,111 @@ def resize(image, width, height, channels=None, decode=False, resample="nearest"
         raise ValueError(
             "Cannot call 'resize' on objects that are not either an Image or SArray of Images"
         )
+
+
+def get_deep_features(images, model_name, batch_size=64, verbose=True):
+    """
+    Extracts features from images from a specific model.
+
+    Parameters
+    ----------
+    images : SArray
+        Input data.
+
+    model_name : string
+        string optional
+        Uses a pretrained model to bootstrap an image classifier:
+
+           - "resnet-50" : Uses a pretrained resnet model.
+                           Exported Core ML model will be ~90M.
+
+           - "squeezenet_v1.1" : Uses a pretrained squeezenet model.
+                                 Exported Core ML model will be ~4.7M.
+
+           - "VisionFeaturePrint_Scene": Uses an OS internal feature extractor.
+                                          Only on available on iOS 12.0+,
+                                          macOS 10.14+ and tvOS 12.0+.
+                                          Exported Core ML model will be ~41K.
+
+        Models are downloaded from the internet if not available locally. Once
+        downloaded, the models are cached for future use.
+
+    Returns
+    -------
+    out : SArray
+        Returns an SArray with all the extracted features.
+
+    Examples
+    --------
+
+    """
+
+    # Check model parameter
+    allowed_models = list(_pre_trained_models.IMAGE_MODELS.keys())
+    if _mac_ver() >= (10, 14):
+        allowed_models.append("VisionFeaturePrint_Scene")
+    _tkutl._check_categorical_option_type("model", model_name, allowed_models)
+
+    # Check images parameter
+    if not isinstance(images, _tc.SArray):
+        raise TypeError("Unrecognized type for 'images'. An SArray is expected.")
+    if len(images) == 0:
+        raise _ToolkitError("Unable to extract features on an empty SArray object")
+
+    if batch_size < 1:
+        raise ValueError("'batch_size' must be greater than or equal to 1")
+    
+    # Extract features
+    feature_extractor = _image_feature_extractor._create_feature_extractor(model_name)
+    images_sf = _tc.SFrame({"image":images})
+    return feature_extractor.extract_features(images_sf, "image", verbose=verbose, 
+        batch_size=batch_size)
+
+
+def find_only_image_extracted_features_column(sframe, model_name):
+    """
+    Finds the only column in `sframe` with a type of array.array and has
+    the length same as the last layer of the model in use.
+    If there are zero or more than one image columns, an exception will
+    be raised.
+    """
+    from array import array
+
+    feature_column = _tkutl._find_only_column_of_type(sframe, target_type=array, type_name="array", col_name="extracted_feature")
+    if is_image_deep_feature_sarray(sframe[feature_column], model_name):
+        return feature_column
+    else:
+        raise ToolkitError(
+            'No "{col_name}" column specified and no column with expected type "{type_name}" is found.'.format(
+                col_name=feature_column, type_name="array"
+            )
+            + ' "datasets" consists of columns with types: '
+            + ", ".join([x.__name__ for x in sframe.column_types()])
+            + "."
+        )
+
+
+def is_image_deep_feature_sarray(feature_sarray, model_name):
+    """
+    Finds if the given `SArray` has extracted features for a given model_name.
+    """
+    from array import array
+
+    if feature_sarray.dtype != array:
+        return False
+    if type(feature_sarray[0]) != array:
+        return False
+    if len(feature_sarray[0]) != MODEL_TO_FEATURE_SIZE_MAPPING[model_name]:
+        return False
+    return True
+
+
+'''
+if __name__ == "__main__":
+    sframe_path = "/Users/neeraj/Work/cacti/experiments/test_exp/s_frame_data"
+    model_name = "resnet-50"
+    feature = "image"
+    dataset = tc.SFrame(sframe_path)
+    extracted_features = get_deep_features(dataset, feature, model_name, batch_size=64, verbose=True, dataset_name="data")
+    extracted_features.save("/Users/neeraj/Downloads/test_sframe")
+'''
