56bcbb32c1c453bd13be1e1228baf4c23f78b24a
Toby Roseman <troseman@apple.com>
2020-08-25 11:02:22 -0700

    Add Python 3.8 classifier tags to setup.py files

--- src/python/setup.py
+++ src/python/setup.py
@@ -136,6 +136,7 @@ if __name__ == "__main__":
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: Implementation :: CPython",
         "Topic :: Scientific/Engineering",
         "Topic :: Scientific/Engineering :: Information Analysis",
