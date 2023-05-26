4669f804d82678ca1c2fbc64e952840a1b36fd1c
Kaunil Dhruv <dhruv.kaunil@gmail.com>
2023-05-26 16:51:35 -0700

    Update install_python_toolchain.sh

--- scripts/install_python_toolchain.sh
+++ scripts/install_python_toolchain.sh
@@ -12,9 +12,14 @@ source deps/env/bin/activate
 PYTHON="${PWD}/deps/env/bin/python"
 PIP="${PYTHON} -m pip"
 
+# python version
 PYTHON_MAJOR_VERSION=$(${PYTHON} -c 'import sys; print(sys.version_info.major)')
 PYTHON_MINOR_VERSION=$(${PYTHON} -c 'import sys; print(sys.version_info.minor)')
 PYTHON_VERSION="python${PYTHON_MAJOR_VERSION}.${PYTHON_MINOR_VERSION}"
+
+# pip version
+PIP_MAJOR_VERSION=$(${PYTHON} -c 'import pip; print(pip.__version__.split(".")[0])')
+PIP_MINOR_VERSION=$(${PYTHON} -c 'import pip; print(pip.__version__.split(".")[1])')
 
 # TODO - not sure why 'm' is necessary here (and not in 2.7)
 # note that PYTHON_VERSION includes the word "python", like "python2.7" or "python3.6"
@@ -34,11 +39,18 @@ function linux_patch_sigfpe_handler {
   fi
 }
 
+function get_pip_version{
+  pip_version=$(pip -V).trim()
+  echo $pip_version
+}
+
 $PIP install --upgrade "pip"
+
+DEP_RESOLVER=
 if [[ "$USE_MINIMAL" -eq 1  ]]; then
-  $PIP install -r scripts/requirements-minimal.txt --prefer-binary  --use-feature=2020-resolver
+  $PIP install -r scripts/requirements-minimal.txt --prefer-binary
 else
-  $PIP install -r scripts/requirements.txt --prefer-binary  --use-feature=2020-resolver
+  $PIP install -r scripts/requirements.txt --prefer-binary
 fi
 
 # install pre-commit hooks for git
